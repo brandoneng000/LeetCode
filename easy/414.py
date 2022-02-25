314ffcebad50fe950423cad69f7fbd563f744024
Brandon Eng <brandoneng000@gmail.com>
2022-02-24 21:58:57 -0800

    Finished third maximum number

---
+++ easy/414.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        if len(nums) < 3:
+            return max(nums)
+        num_set = set(nums)
+
+        if len(num_set) < 3:
+            return max(num_set)
+
+        num_set.remove(max(num_set))
+        num_set.remove(max(num_set))
+        return max(num_set)
+
+def main():
+    sol = Solution()
+    print(sol.thirdMax([3,2,1]))
+    print(sol.thirdMax([2,1]))
+    print(sol.thirdMax([2,2,3,1]))
+    print(sol.thirdMax([1,1,2]))
+
+if __name__ == '__main__':
+    main()
