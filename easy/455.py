aa9c60f802be84ef156c95067731dfdbd7e49c30
Brandon Eng <brandoneng000@gmail.com>
2022-02-24 22:41:07 -0800

    Updated assign cookies

--- easy/455.py
+++ easy/455.py
@@ -2,22 +2,38 @@ from typing import List
 
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        if s:
+            s.sort()
+        else:
+            return 0
+
+        g.sort()
         content = 0
 
-        while len(s) > 0 and len(g) > 0:
-            if min(g) > max(s):
-                break
-            greediest = max(g)
-            cookie = max(s)
-
-            if greediest <= cookie:
-                s.remove(cookie)
-                g.remove(greediest)
+        for cookie in s:
+            if g[content] <= cookie:
                 content += 1
-            elif greediest > cookie:
-                g.remove(greediest)
-            
+                if content == len(g):
+                    break
+        
         return content
+        
+        # content = 0
+
+        # while len(s) > 0 and len(g) > 0:
+        #     if min(g) > max(s):
+        #         break
+        #     greediest = max(g)
+        #     cookie = max(s)
+
+        #     if greediest <= cookie:
+        #         s.remove(cookie)
+        #         g.remove(greediest)
+        #         content += 1
+        #     elif greediest > cookie:
+        #         g.remove(greediest)
+            
+        # return content
         
 
 def main():
