33385ab534dd2bc00b726f3367979465a97764e0
Brandon Eng <brandoneng000@gmail.com>
2022-03-01 20:07:01 -0800

    Finished reverse string

---
+++ easy/344.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        size = len(s) // 2
+        index = 0
+        
+        while index < size:
+            temp = s[index]
+            s[index] = s[len(s) - index - 1]
+            s[len(s) - index - 1] = temp
+            index += 1
+
+
+def main():
+    sol = Solution()
+    s = ["h","e","l","l","o"]
+    sol.reverseString(s)
+    print(s)
+    s = ["H","a","n","n","a","h"]
+    sol.reverseString(s)
+    print(s)
+
+if __name__ == '__main__':
+    main()
