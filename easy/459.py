9f5329397f2e47f42a8d8d80a2427093d39434fb
Brandon Eng <brandoneng000@gmail.com>
2022-03-03 17:26:34 -0800

    Finished repeated substring pattern

---
+++ easy/459.py
@@ -0,0 +1,27 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        sub_string = ""
+
+        for letter in s:
+            sub_string += letter
+            if len(sub_string) > len(s) / 2:
+                return False
+            temp = sub_string
+            while(len(sub_string) < len(s)):
+                sub_string += temp
+            if sub_string == s:
+                return True
+            sub_string = temp
+        
+        return False
+        
+def main():
+    sol = Solution()
+    print(sol.repeatedSubstringPattern("babbabbabbabbab"))
+    print(sol.repeatedSubstringPattern("abac"))
+    print(sol.repeatedSubstringPattern("abab"))
+    print(sol.repeatedSubstringPattern("aba"))
+    print(sol.repeatedSubstringPattern("abcabcabcabc"))
+
+if __name__ == '__main__':
+    main()
