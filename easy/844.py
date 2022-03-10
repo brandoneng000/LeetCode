8327d66e8eb45fa1bc8b3043d3b90c88b9383559
Brandon Eng <brandoneng000@gmail.com>
2022-03-09 16:46:24 -0800

    Finished backspace string compare

---
+++ easy/844.py
@@ -0,0 +1,28 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        new_s = ""
+        new_t = ""
+        
+        for letter in s:
+            if letter == '#' and len(new_s) > 0:
+                new_s = new_s[:-1]
+            elif letter != '#':
+                new_s += letter
+
+        for letter in t:
+            if letter == '#' and len(new_t) > 0:
+                new_t = new_t[:-1]
+            elif letter != '#':
+                new_t += letter
+
+        return new_s == new_t
+
+def main():
+    sol = Solution()
+    print(sol.backspaceCompare("y#fo##f", "y#f#o##f"))
+    print(sol.backspaceCompare("ab#c", "ad#c"))
+    print(sol.backspaceCompare("ab##", "c#d#"))
+    print(sol.backspaceCompare("a#c", "b"))
+
+if __name__ == '__main__':
+    main()
