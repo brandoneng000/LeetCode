d3d5768464b2aa4049b7256012129c1cf4543e53
Brandon Eng <brandoneng000@gmail.com>
2022-03-20 02:38:39 -0700

    Finished linked list cycle

---
+++ easy/141.py
@@ -0,0 +1,25 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+        else:
+            slow = head
+            fast = head.next
+
+        while slow is not fast:
+            slow = slow.next
+            fast = fast.next
+            if not slow or not fast:
+                return False
+            fast = fast.next
+            if not fast:
+                return False
+        
+        return True
