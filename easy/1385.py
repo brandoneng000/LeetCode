d2e682a58764e0443ef008b0cafc32557e7b7754
Brandon Eng <brandoneng000@gmail.com>
2023-02-10 11:19:04 -0800

    Finished 1385

---
+++ easy/1385.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        result = 0
+
+        for num in arr1:
+            index = 0
+            while(index < len(arr2) and abs(num - arr2[index]) > d):
+                index += 1
+            if index == len(arr2):
+                result += 1
+        
+        return result
+
+def main():
+    sol = Solution()
+    print(sol.findTheDistanceValue(arr1 = [4,5,8], arr2 = [10,9,1,8], d = 2))
+    print(sol.findTheDistanceValue(arr1 = [1,4,2,3], arr2 = [-4,-3,6,10,20,30], d = 3))
+    print(sol.findTheDistanceValue(arr1 = [2,1,100,3], arr2 = [-5,-2,10,-3,7], d = 6))
+
+if __name__ == '__main__':
+    main()
