4d08bcb7fd91f5b1a61daa6e52457433ad7866f8
Brandon Eng <brandoneng000@gmail.com>
2023-02-11 22:58:19 -0800

    Finish 1437

---
+++ easy/1437.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        count = 0
+
+        for num in nums:
+            if count <= 0 and num == 1:
+                count = k
+            elif num == 1:
+                return False
+            else:
+                count -= 1
+        
+        return True
+
+def main():
+    sol = Solution()
+    print(sol.kLengthApart(nums = [1,0,0,0,1,0,0,1], k = 2))
+    print(sol.kLengthApart(nums = [1,0,0,1,0,1], k = 2))
+
+if __name__ == '__main__':
+    main()
