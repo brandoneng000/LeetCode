0e717404fce321301de50b3e194475d29801e83f
Brandon Eng <brandoneng000@gmail.com>
2023-02-16 17:27:02 -0800

    Finished 1523

---
+++ easy/1523.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        result = high % 2 == 1 or low % 2 == 1
+        result += (high - low) // 2
+        return result
+
+def main():
+    sol = Solution()
+    print(sol.countOdds(3, 7))
+    print(sol.countOdds(8, 10))
+
+if __name__ == '__main__':
+    main()
