d668f7f711b81ff9475b5c808ba123f0852444f4
Brandon Eng <brandoneng000@gmail.com>
2023-03-08 00:07:03 -0800

    Finished 1266

---
+++ easy/1266.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        # x, y = points.pop(0)
+        # result = 0
+
+        # for point in points:
+        #     large_x = max(point[0], x)
+        #     large_y = max(point[1], y)
+        #     small_x = min(point[0], x)
+        #     small_y = min(point[1], y)
+        #     diag = min(large_x - small_x, large_y - small_y)
+        #     straight = max(large_x - (small_x + diag), large_y - (small_y + diag))
+        #     result += diag + straight
+        #     x, y = point
+        
+        # return result
+        result = 0
+
+        for index in range(1, len(points)):
+            straight = max(abs(points[index - 1][0] - points[index][0]), abs(points[index - 1][1] - points[index][1]))
+            result += straight
+        
+        return result
+
+def main():
+    sol = Solution()
+    print(sol.minTimeToVisitAllPoints([[1,1],[0, 0]]))
+    print(sol.minTimeToVisitAllPoints([[1,1],[3,4],[-1,0]]))
+    print(sol.minTimeToVisitAllPoints([[3,2],[-2,2]]))
+
+if __name__ == '__main__':
+    main()
