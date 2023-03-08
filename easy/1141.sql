bcee55201f0aa9b3ec5d835437fe6e653efbfee1
Brandon Eng <brandoneng000@gmail.com>
2023-03-07 16:07:23 -0800

    Finished 1141

---
+++ easy/1141.sql
@@ -0,0 +1,12 @@
+SELECT
+    activity_date as day,
+    COUNT(DISTINCT user_id) as active_users
+FROM 
+    activity
+WHERE
+    DATEDIFF('2019-07-27', activity_date) < 30
+    AND activity_date <= '2019-07-27'
+GROUP BY
+    activity_date
+ORDER BY
+    activity_date;
