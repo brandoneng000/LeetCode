419bfd399dbd92e1b595636c49bc76867bb0bc6f
Brandon Eng <brandoneng000@gmail.com>
2023-03-08 16:25:40 -0800

    Finished 1331

---
+++ easy/1331.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        arr_ordered = sorted(list(set(arr)))
+        arr_dict = {}
+
+        for index, val in enumerate(arr_ordered, 1):
+            arr_dict[val] = index
+        
+        for index, val in enumerate(arr):
+            arr[index] = arr_dict[val]
+        
+        return arr
+
+def main():
+    sol = Solution()
+    print(sol.arrayRankTransform([40,10,20,30]))
+    print(sol.arrayRankTransform([100,100,100]))
+    print(sol.arrayRankTransform([37,12,28,9,100,56,80,5,12]))
+
+if __name__ == '__main__':
+    main()
