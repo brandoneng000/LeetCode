b32e1447a0e3f302900b18aa6f7644b97efc2f11
Brandon Eng <brandoneng000@gmail.com>
2023-03-22 04:46:07 -0700

    Finished 2169

---
+++ easy/2169.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        if not num1 or not num2:
+            return 0
+        small = min(num1, num2)
+        large = max(num1, num2)
+        res = 0
+
+        while large % small != 0:
+            large -= small
+            small, large =  min(large, small), max(large, small)
+            res += 1
+
+        return res + large // small
+
+
+def main():
+    sol = Solution()
+    print(sol.countOperations(2, 3))
+    print(sol.countOperations(10, 10))
+
+if __name__ == '__main__':
+    main()
