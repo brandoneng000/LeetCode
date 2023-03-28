aaeac28c6fd2cc5fc60ecbb850b0e8bd927fc940
Brandon Eng <brandoneng000@gmail.com>
2023-03-27 21:04:35 -0700

    Finished 2220

---
+++ easy/2220.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        res = 0
+        
+        while start or goal:
+            if start & 1 != goal & 1:
+                res += 1
+            start >>= 1
+            goal >>= 1
+        
+        return res
+
+
+def main():
+    sol = Solution()
+    print(sol.minBitFlips(10, 7))
+    print(sol.minBitFlips(3, 4))
+
+if __name__ == '__main__':
+    main()
