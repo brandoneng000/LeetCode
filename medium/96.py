cf794529dc3a2b6ed9421456a3ecb88c5d8d97ee
Brandon Eng <brandoneng000@gmail.com>
2023-04-11 19:28:38 -0700

    Finished 96

---
+++ medium/96.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        num_tree = [1] * (n + 1)
+
+        for nodes in range(2, n + 1):
+            total = 0
+            for root in range(1, nodes + 1):
+                left = root - 1
+                right = nodes - root
+                total += num_tree[left] * num_tree[right]
+            num_tree[nodes] = total
+
+        return num_tree[n]
+
+def main():
+    sol = Solution()
+    print(sol.numTrees(3))
+    print(sol.numTrees(1))
+
+if __name__ == '__main__':
+    main()
