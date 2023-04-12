38067544b40814c8fe743b27d06e44e930ff4c23
Brandon Eng <brandoneng000@gmail.com>
2023-04-12 05:40:20 -0700

    Finished 97

---
+++ medium/97.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        
+        dp = [False] * (len(s2) + 1)
+        
+        for i in range(len(s1) + 1):
+            for j in range(len(s2) + 1):
+                if i == 0 and j == 0:
+                    dp[j] = True
+                elif i == 0:
+                    dp[j] = dp[j - 1] and s2[j - 1] == s3[i + j - 1]
+                elif j == 0:
+                    dp[j] = dp[j] and s1[i - 1] == s3[i + j - 1]
+                else:
+                    dp[j] = (dp[j] and s1[i - 1] == s3[i + j - 1]) or (dp[j - 1] and s2[j - 1] == s3[i + j - 1])
+
+        return dp[len(s2)]
+
+def main():
+    sol = Solution()
+    print(sol.isInterleave(s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"))
+    print(sol.isInterleave(s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"))
+    print(sol.isInterleave(s1 = "", s2 = "", s3 = ""))
+    print(sol.isInterleave(s1 = "a", s2 = "", s3 = "c"))
+    print(sol.isInterleave(s1 = "", s2 = "a", s3 = "c"))
+
+if __name__ == '__main__':
+    main()
