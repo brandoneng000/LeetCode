db67e4e4a49d94569f9d4d9e142f601381cbcb8f
Brandon Eng <brandoneng000@gmail.com>
2023-04-13 16:08:18 -0700

    Finished 117

---
+++ easy/117.py
@@ -0,0 +1,38 @@
+import collections
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root: return None
+        temp = root
+        queue = collections.deque([temp])
+        children = 1
+        prev = None
+
+        while queue:
+            node = queue.popleft()
+            children -= 1
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+            if not prev:
+                prev = node
+            else:
+                prev.next = node
+                prev = prev.next
+
+            if not children:
+                prev.next = None
+                prev = None
+                children = len(queue)
+        
+        return root
+
