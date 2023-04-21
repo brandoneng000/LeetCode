dd83d4647a735932a0c584758bd868c24055d894
Brandon Eng <brandoneng000@gmail.com>
2023-04-21 00:32:54 -0700

    Finished 559

---
+++ easy/559.py
@@ -0,0 +1,19 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        def dfs(root: 'Node'):
+            if not root:
+                return 0
+            
+            depth = 0
+            for child in root.children:
+                depth = max(depth, dfs(child))
+            
+            return depth + 1
+        
+        return dfs(root)
