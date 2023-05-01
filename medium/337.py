61ad46a68ed49bb1bf386313e81e49f34af2a45a
Brandon Eng <brandoneng000@gmail.com>
2023-04-30 17:50:18 -0700

    Finished 337

---
+++ medium/337.py
@@ -0,0 +1,20 @@
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dfs(root: TreeNode) -> tuple:
+            if not root:
+                return (0, 0)
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            return (root.val + left[1] + right[1], max(left) + max(right))
+
+        return max(dfs(root))
