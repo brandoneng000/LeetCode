3dd5fba7f77035bf4c06c42505503b3fb81e99c0
Brandon Eng <brandoneng000@gmail.com>
2023-05-03 23:55:40 -0700

    Finished 385

---
+++ medium/385.py
@@ -0,0 +1,52 @@
+class NestedInteger:
+   def __init__(self, value=None):
+       """
+       If value is not specified, initializes an empty list.
+       Otherwise initializes a single integer equal to value.
+       """
+
+   def isInteger(self):
+       """
+       @return True if this NestedInteger holds a single integer, rather than a nested list.
+       :rtype bool
+       """
+
+   def add(self, elem):
+       """
+       Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+       :rtype void
+       """
+
+   def setInteger(self, value):
+       """
+       Set this NestedInteger to hold a single integer equal to value.
+       :rtype void
+       """
+
+   def getInteger(self):
+       """
+       @return the single integer that this NestedInteger holds, if it holds a single integer
+       Return None if this NestedInteger holds a nested list
+       :rtype int
+       """
+
+   def getList(self):
+       """
+       @return the nested list that this NestedInteger holds, if it holds a nested list
+       Return None if this NestedInteger holds a single integer
+       :rtype List[NestedInteger]
+       """
+
+class Solution:
+    def deserialize(self, s: str) -> NestedInteger:
+        
+        def generate_nested(nest):
+            if isinstance(nest, int):
+                return NestedInteger(nest)
+            list_nest = NestedInteger()
+
+            for n in nest:
+                list_nest.add(generate_nested(n))
+            return list_nest
+        
+        return generate_nested(eval(s))
