ede9b80ae60ca40b78bcf1b03ffdcab56de09f59
Brandon Eng <brandoneng000@gmail.com>
2023-06-21 02:31:02 -0700

    Finished 877

---
+++ medium/877.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
+
+def main():
+    sol = Solution()
+    print(sol.stoneGame([5,3,4,5]))
+    print(sol.stoneGame([3,7,2,3]))
+
+if __name__ == '__main__':
+    main()
