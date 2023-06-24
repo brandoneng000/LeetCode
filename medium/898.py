441587ac2b7bbe622eda1e240f952c4058c24a2e
Brandon Eng <brandoneng000@gmail.com>
2023-06-23 22:39:06 -0700

    Finished 898

---
+++ medium/898.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def subarrayBitwiseORs(self, arr: List[int]) -> int:
+        res = set(arr)
+        prev = set()
+        prev.add(arr[0])
+        for n in arr[1:]:
+            temp = set()
+            for p in prev:
+                temp.add(n | p)
+                res.add(n | p)
+            prev = temp
+            prev.add(n)
+        
+        return len(res)
+            
+def main():
+    sol = Solution()
+    print(sol.subarrayBitwiseORs([0]))
+    print(sol.subarrayBitwiseORs([1,1,2]))
+    print(sol.subarrayBitwiseORs([1,2,4]))
+
+if __name__ == '__main__':
+    main()
