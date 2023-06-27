51872a3e6d253ba593835a17a9b296c5beb98e79
Brandon Eng <brandoneng000@gmail.com>
2023-06-27 02:13:43 -0700

    Finished 916

---
+++ medium/916.py
@@ -0,0 +1,33 @@
+from typing import List
+import collections
+
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        words2_counter = [collections.Counter(word) for word in words2]
+        res = []
+
+        full_counter = collections.Counter()
+
+        for letter_counter in words2_counter:
+            for letter in letter_counter:
+                full_counter[letter] = max(full_counter[letter], letter_counter[letter])
+
+        for word in words1:
+            state = True
+            word_counter = collections.Counter(word)
+            for letter in full_counter:
+                if word_counter[letter] < full_counter[letter]:
+                    state = False
+            if state:
+                res.append(word)
+
+        return res
+
+def main():
+    sol = Solution()
+    print(sol.wordSubsets(words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["e","o"]))
+    print(sol.wordSubsets(words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["l","e"]))
+    print(sol.wordSubsets(words1 = ["goggle"], words2 = ["lo","eo","oo"]))
+
+if __name__ == '__main__':
+    main()
