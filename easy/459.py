1f128d7cb776c53ca39d7d9ad8d84d2a587df495
Brandon Eng <brandoneng000@gmail.com>
2023-08-21 01:34:37 -0700

    Updated 459

--- easy/459.py
+++ easy/459.py
@@ -1,19 +1,23 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        sub_string = ""
+        dupe = s + s
+        return s in dupe[1:-1]
 
-        for letter in s:
-            sub_string += letter
-            if len(sub_string) > len(s) / 2:
-                return False
-            temp = sub_string
-            while(len(sub_string) < len(s)):
-                sub_string += temp
-            if sub_string == s:
-                return True
-            sub_string = temp
+    # def repeatedSubstringPattern(self, s: str) -> bool:
+    #     sub_string = ""
+
+    #     for letter in s:
+    #         sub_string += letter
+    #         if len(sub_string) > len(s) / 2:
+    #             return False
+    #         temp = sub_string
+    #         while(len(sub_string) < len(s)):
+    #             sub_string += temp
+    #         if sub_string == s:
+    #             return True
+    #         sub_string = temp
         
-        return False
+    #     return False
         
 def main():
     sol = Solution()
