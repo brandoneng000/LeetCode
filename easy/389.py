7175af6dbfef9ef5c951a4d776a68aaf4bf0a219
Brandon Eng <brandoneng000@gmail.com>
2023-09-25 01:35:35 -0700

    Updated 389

--- easy/389.py
+++ easy/389.py
@@ -1,30 +1,37 @@
+import collections
+
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        char = {}
+        res = ord(t[0])
 
-        for letter in t:
-            if letter not in char:
-                char[letter] = 1
-            else:
-                char[letter] += 1
+        for i, j in zip(s, t[1:]):
+            res ^= ord(i)
+            res ^= ord(j)
+
+        return chr(res)
+    
+    # def findTheDifference(self, s: str, t: str) -> str:
+    #     return list((collections.Counter(t) - collections.Counter(s)).keys())[0]
+
+    # def findTheDifference(self, s: str, t: str) -> str:
+    #     char = {}
+
+    #     for letter in t:
+    #         if letter not in char:
+    #             char[letter] = 1
+    #         else:
+    #             char[letter] += 1
         
-        for letter in s:
-            char[letter] -= 1
-            if char[letter] == 0:
-                del char[letter]
+    #     for letter in s:
+    #         char[letter] -= 1
+    #         if char[letter] == 0:
+    #             del char[letter]
 
-        return list(char.keys())[0]
-
-        # char = list(t)
-
-        # for letter in s:
-        #     char.remove(letter)
-
-        # return char[0]
-            
+    #     return list(char.keys())[0]
 
 def main():
     sol = Solution()
+    print(sol.findTheDifference("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "abcdefghijdklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"))
     print(sol.findTheDifference("abcd", "abcde"))
     print(sol.findTheDifference("", "y"))
     print(sol.findTheDifference("a", "aa"))
