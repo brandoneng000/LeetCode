b39fd40f696515ca2936d100e5ee1c1fff670606
Brandon Eng <brandoneng000@gmail.com>
2023-09-29 01:23:08 -0700

    Updated 896

--- easy/896.py
+++ easy/896.py
@@ -2,16 +2,26 @@ from typing import List
 
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        mono_increase = True
-        mono_decrease = True
-
-        for index in range(len(nums) - 1):
-            if nums[index] < nums[index + 1]:
-                mono_decrease = False
-            elif nums[index] > nums[index + 1]:
-                mono_increase = False
+        def helper(nums: List[int]):
+            for i in range(1, len(nums)):
+                if nums[i - 1] > nums[i]:
+                    return False
+            
+            return True
         
-        return mono_increase or mono_decrease
+        return helper(nums) or helper(nums[::-1])
+    
+    # def isMonotonic(self, nums: List[int]) -> bool:
+    #     mono_increase = True
+    #     mono_decrease = True
+
+    #     for index in range(len(nums) - 1):
+    #         if nums[index] < nums[index + 1]:
+    #             mono_decrease = False
+    #         elif nums[index] > nums[index + 1]:
+    #             mono_increase = False
+        
+    #     return mono_increase or mono_decrease
     
 def main():
     sol = Solution()
