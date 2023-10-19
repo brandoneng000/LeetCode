59722dd18dd468310adc1d1b4841aaec24708214
Brandon Eng <brandoneng000@gmail.com>
2023-10-19 04:13:15 -0700

    Updated 844

--- easy/844.py
+++ easy/844.py
@@ -1,21 +1,39 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        new_s = ""
-        new_t = ""
+        s_stack = []
+        t_stack = []
+
+        for c in s:
+            if c == '#' and s_stack:
+                s_stack.pop()
+            elif c != '#':
+                s_stack.append(c)
         
-        for letter in s:
-            if letter == '#' and len(new_s) > 0:
-                new_s = new_s[:-1]
-            elif letter != '#':
-                new_s += letter
+        for c in t:
+            if c == '#' and t_stack:
+                t_stack.pop()
+            elif c != '#':
+                t_stack.append(c)
+        
+        return s_stack == t_stack
 
-        for letter in t:
-            if letter == '#' and len(new_t) > 0:
-                new_t = new_t[:-1]
-            elif letter != '#':
-                new_t += letter
+    # def backspaceCompare(self, s: str, t: str) -> bool:
+    #     new_s = ""
+    #     new_t = ""
+        
+    #     for letter in s:
+    #         if letter == '#' and len(new_s) > 0:
+    #             new_s = new_s[:-1]
+    #         elif letter != '#':
+    #             new_s += letter
 
-        return new_s == new_t
+    #     for letter in t:
+    #         if letter == '#' and len(new_t) > 0:
+    #             new_t = new_t[:-1]
+    #         elif letter != '#':
+    #             new_t += letter
+
+    #     return new_s == new_t
 
 def main():
     sol = Solution()
