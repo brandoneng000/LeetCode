f039aebb0a2e8008054ab49563e2cccee4d87d87
Brandon Eng <brandoneng000@gmail.com>
2023-11-16 01:50:21 -0800

    Finished 1980

---
+++ medium/1980.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        res = []
+
+        for i in range(len(nums)):
+            cur = nums[i][i]
+            res.append('1' if cur == '0' else '0')
+        
+        return "".join(res)
+    
+    # def findDifferentBinaryString(self, nums: List[str]) -> str:
+    #     def gen_binary(cur: List[str]):
+    #         if len(cur) == bits:
+    #             binaries.append("".join(cur))
+    #             return
+
+    #         for bit in ['0', '1']:
+    #             cur.append(bit)
+    #             gen_binary(cur)
+    #             cur.remove(bit)
+        
+    #     bits = len(nums[0])
+    #     binaries = []
+    #     gen_binary([])
+
+    #     return list(set(binaries) - set(nums))[0]
+        
+def main():
+    sol = Solution()
+    print(sol.findDifferentBinaryString(["01","10"]))
+    print(sol.findDifferentBinaryString(["00","01"]))
+    print(sol.findDifferentBinaryString(["111","011","001"]))
+
+if __name__ == '__main__':
+    main()
