10d4613bdba0e25e33bbcbd68370709acc7f1a12
Brandon Eng <brandoneng000@gmail.com>
2024-01-05 09:11:42 -0800

    Updated 300

--- medium/300.py
+++ medium/300.py
@@ -1,45 +1,59 @@
 from typing import List
+from bisect import bisect_left
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # nums_dict = {}
-        # res = []
-        # arr = []
+        sub = []
 
-        # for index, num in enumerate(nums):
-        #     nums_dict[(num, index)] = nums_dict.get(num, [])
-        #     for n in nums_dict:
-        #         if n[0] < num:
-        #             nums_dict[n].append((num, index))
+        for num in nums:
+            if len(sub) == 0 or sub[-1] < num:
+                sub.append(num)
+            else:
+                index = bisect_left(sub, num)
+                sub[index] = num
         
-        # def recursive(nums_dict, arr: List[int]):
-        #     if not nums_dict[arr[-1]]:
-        #         return arr.copy()
+        return len(sub)
+
+    # def lengthOfLIS(self, nums: List[int]) -> int:
+    #     nums_dict = {}
+    #     res = []
+    #     arr = []
+
+    #     for index, num in enumerate(nums):
+    #         nums_dict[(num, index)] = nums_dict.get(num, [])
+    #         for n in nums_dict:
+    #             if n[0] < num:
+    #                 nums_dict[n].append((num, index))
+        
+    #     def recursive(nums_dict, arr: List[int]):
+    #         if not nums_dict[arr[-1]]:
+    #             return arr.copy()
             
-        #     longest = []
-        #     for n in nums_dict[arr[-1]]:
-        #         arr.append(n)
-        #         longest = max(longest, recursive(nums_dict, arr), key=len)
-        #         arr.pop()
+    #         longest = []
+    #         for n in nums_dict[arr[-1]]:
+    #             arr.append(n)
+    #             longest = max(longest, recursive(nums_dict, arr), key=len)
+    #             arr.pop()
             
-        #     return longest
+    #         return longest
         
-        # for index, num in enumerate(nums):
-        #     arr.append((num, index))
-        #     res = max(res, recursive(nums_dict, arr), key=len)
-        #     arr.pop()
+    #     for index, num in enumerate(nums):
+    #         arr.append((num, index))
+    #         res = max(res, recursive(nums_dict, arr), key=len)
+    #         arr.pop()
         
-        # return len(res)
+    #     return len(res)
 
-        # cache = {}
-        # for i in range(len(nums) - 1, -1, -1):
-        #     cache[i] = 1
-        #     for j in range(i, len(nums)):
-        #         if nums[i] < nums[j]:
-        #             cache[i] = max(cache[j] + 1, cache[i])
+    #     cache = {}
+    #     for i in range(len(nums) - 1, -1, -1):
+    #         cache[i] = 1
+    #         for j in range(i, len(nums)):
+    #             if nums[i] < nums[j]:
+    #                 cache[i] = max(cache[j] + 1, cache[i])
 
-        # return max(cache.values())
+    #     return max(cache.values())
 
+    def lengthOfLIS(self, nums: List[int]) -> int:
         cache = [1] * len(nums)
         for i in range(len(nums) - 1, -1, -1):
             for j in range(i, len(nums)):
