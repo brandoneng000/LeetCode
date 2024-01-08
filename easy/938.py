1c2a587c034419ad7b4e37310ce1302ccea68c48
Brandon Eng <brandoneng000@gmail.com>
2024-01-08 06:11:19 -0800

    Updated 938

--- easy/938.py
+++ easy/938.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from collections import deque
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -8,32 +9,54 @@ class TreeNode:
         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        # cur_node = root
-        # stack = []
-        # result = 0
+        q = deque([root])
+        res = 0
 
-        # while cur_node or stack:
-        #     if cur_node:
-        #         stack.append(cur_node)
-        #         cur_node = cur_node.left
-        #     elif stack:
-        #         cur_node = stack.pop()
-        #         if low <= cur_node.val <= high:
-        #             result += cur_node.val
-        #         cur_node = cur_node.right
+        while q:
+            node = q.popleft()
+            if not node:
+                continue
 
-        # return result
+            if low <= node.val <= high:
+                print(node.val)
+                res += node.val
 
-        stack = [root]
-        result = 0
-        while stack:
-            cur_node = stack.pop()
-            if cur_node:
-                if low <= cur_node.val <= high:
-                    result += cur_node.val
-                if low < cur_node.val:
-                    stack.append(cur_node.left)
-                if cur_node.val < high:
-                    stack.append(cur_node.right)
+            if node.val > low:
+                q.append(node.left)
+            if node.val < high:
+                q.append(node.right)
         
-        return result
+        return res
+
+
+    # def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+    #     cur_node = root
+    #     stack = []
+    #     result = 0
+
+    #     while cur_node or stack:
+    #         if cur_node:
+    #             stack.append(cur_node)
+    #             cur_node = cur_node.left
+    #         elif stack:
+    #             cur_node = stack.pop()
+    #             if low <= cur_node.val <= high:
+    #                 result += cur_node.val
+    #             cur_node = cur_node.right
+
+    #     return result
+
+    # def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+    #     stack = [root]
+    #     result = 0
+    #     while stack:
+    #         cur_node = stack.pop()
+    #         if cur_node:
+    #             if low <= cur_node.val <= high:
+    #                 result += cur_node.val
+    #             if low < cur_node.val:
+    #                 stack.append(cur_node.left)
+    #             if cur_node.val < high:
+    #                 stack.append(cur_node.right)
+        
+    #     return result
