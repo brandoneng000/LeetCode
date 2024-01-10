9299bb35cbbbe0c7f9cb01c24814f4a1935062b0
Brandon Eng <brandoneng000@gmail.com>
2024-01-10 05:26:13 -0800

    Finished 1813

---
+++ medium/1813.py
@@ -0,0 +1,34 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        if len(sentence1) < len(sentence2):
+            sentence1, sentence2 = sentence2, sentence1
+        
+        s1, s2 = sentence1.split(), sentence2.split()
+        n, m = len(s1), len(s2)
+        j = 0
+
+        for i in range(n):
+            if j < m and s1[i] == s2[j]:
+                j += 1
+            else:
+                break
+        
+        n -= 1
+        for j in range(m - 1, j - 1, -1):
+            if s1[n] == s2[j]:
+                n -= 1
+            else:
+                break
+        else:
+            return True
+
+        return False
+        
+def main():
+    sol = Solution()
+    print(sol.areSentencesSimilar(sentence1 = "My name is Haley", sentence2 = "My Haley"))
+    print(sol.areSentencesSimilar(sentence1 = "of", sentence2 = "A lot of words"))
+    print(sol.areSentencesSimilar(sentence1 = "Eating right now", sentence2 = "Eating"))
+
+if __name__ == '__main__':
+    main()
