2566e90d5881294c470ac159a56114c43fec7dad
Brandon Eng <brandoneng000@gmail.com>
2024-01-12 03:04:06 -0800

    Updated 1704

--- easy/1704.py
+++ easy/1704.py
@@ -1,18 +1,32 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        def count_vowel(s: str) -> dict:
-            count = 0
-            
-            for letter in s:
-                if letter in 'aeiouAEIOU':
-                    count += 1
-            
-            return count
-        
-        s1 = s[: len(s) // 2]
-        s2 = s[len(s) // 2:]
+        n = len(s)
+        vowels = set('aeiouAEIOU')
+        count_first = count_second = 0
 
-        return count_vowel(s1) == count_vowel(s2)
+        for i in range(n // 2):
+            count_first += (s[i] in vowels)
+        
+        for i in range(n // 2, n):
+            count_second += (s[i] in vowels)
+        
+        return count_first == count_second
+
+
+    # def halvesAreAlike(self, s: str) -> bool:
+    #     def count_vowel(s: str) -> dict:
+    #         count = 0
+            
+    #         for letter in s:
+    #             if letter in 'aeiouAEIOU':
+    #                 count += 1
+            
+    #         return count
+        
+    #     s1 = s[: len(s) // 2]
+    #     s2 = s[len(s) // 2:]
+
+    #     return count_vowel(s1) == count_vowel(s2)
 
 def main():
     sol = Solution()
