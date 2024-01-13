2ae30fd650a2715fbf36ac0041c601c90b9ab050
Brandon Eng <brandoneng000@gmail.com>
2024-01-13 03:37:19 -0800

    Updated 1347

--- medium/1347.py
+++ medium/1347.py
@@ -2,14 +2,30 @@ import collections
 
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        s_count = collections.Counter(s)
-        t_count = collections.Counter(t)
+        s_count = [0] * 26
+        t_count = [0] * 26
         res = 0
 
-        for letter in s_count:
-            res += max(0, s_count[letter] - t_count.get(letter, 0))
+        for c in s:
+            s_count[ord(c) - 97] += 1
+        
+        for c in t:
+            t_count[ord(c) - 97] += 1
+        
+        for i in range(26):
+            res += max(0, s_count[i] - t_count[i])
         
         return res
+
+    # def minSteps(self, s: str, t: str) -> int:
+    #     s_count = collections.Counter(s)
+    #     t_count = collections.Counter(t)
+    #     res = 0
+
+    #     for letter in s_count:
+    #         res += max(0, s_count[letter] - t_count.get(letter, 0))
+        
+    #     return res
 
 def main():
     sol = Solution()
