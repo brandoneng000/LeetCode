87b2221d14f09fc52ca7280743f9373804d63256
Brandon Eng <brandoneng000@gmail.com>
2024-01-19 01:59:40 -0800

    Updated 931

--- medium/931.py
+++ medium/931.py
@@ -2,17 +2,36 @@ from typing import List
 
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        for i in range(1, len(matrix)):
-            left = float('inf')
-            right = float('inf')
-            for j in range(len(matrix)):
-                if j > 0:
-                    left = matrix[i - 1][j - 1] + matrix[i][j]
-                if j < len(matrix[0]) - 1:
-                    right = matrix[i - 1][j + 1] + matrix[i][j]
-                matrix[i][j] = min(matrix[i - 1][j] + matrix[i][j], left, right)
+        n = len(matrix)
+        cur = matrix[0]
 
-        return min(matrix[-1])
+        for i in range(1, len(matrix)):
+            next = matrix[i].copy()
+            left = right = float('inf')
+
+            for j in range(n):
+                if j > 0:
+                    left = cur[j - 1] + next[j]
+                if j < n - 1:
+                    right = cur[j + 1] + next[j]
+                next[j] = min(cur[j] + next[j], left, right)
+            cur = next
+        
+        return min(cur)
+        
+
+    # def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+    #     for i in range(1, len(matrix)):
+    #         left = float('inf')
+    #         right = float('inf')
+    #         for j in range(len(matrix)):
+    #             if j > 0:
+    #                 left = matrix[i - 1][j - 1] + matrix[i][j]
+    #             if j < len(matrix[0]) - 1:
+    #                 right = matrix[i - 1][j + 1] + matrix[i][j]
+    #             matrix[i][j] = min(matrix[i - 1][j] + matrix[i][j], left, right)
+
+    #     return min(matrix[-1])
 
 def main():
     sol = Solution()
