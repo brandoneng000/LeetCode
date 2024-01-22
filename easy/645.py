32b525bc06c4929c12c1e5d29bcadebfb235ce1c
Brandon Eng <brandoneng000@gmail.com>
2024-01-22 00:08:31 -0800

    Updated 645

--- easy/645.py
+++ easy/645.py
@@ -2,17 +2,23 @@ from typing import List
 
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        from collections import Counter
-        def sum_of_all(n: int) -> int:
-            return (n * (n + 1)) // 2
-
-        nums_dict = Counter(nums)
+        n = len(nums)
+        missing = (set(range(1, n + 1)) - set(nums)).pop()
         
-        for num in nums_dict:
-            if nums_dict[num] == 2:
-                dupe = num
+        return [sum(nums) - (sum(range(1, n + 1)) - missing), missing]
 
-        return [dupe, dupe + (sum_of_all(len(nums)) - sum(nums))]
+    # def findErrorNums(self, nums: List[int]) -> List[int]:
+    #     from collections import Counter
+    #     def sum_of_all(n: int) -> int:
+    #         return (n * (n + 1)) // 2
+
+    #     nums_dict = Counter(nums)
+        
+    #     for num in nums_dict:
+    #         if nums_dict[num] == 2:
+    #             dupe = num
+
+    #     return [dupe, dupe + (sum_of_all(len(nums)) - sum(nums))]
 
 
 def main():
