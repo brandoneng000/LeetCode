53592bf2f8b1c7298995244e31a66169bfef7415
Brandon Eng <brandoneng000@gmail.com>
2024-01-30 01:39:36 -0800

    Updated 232

--- easy/232.py
+++ easy/232.py
@@ -13,13 +13,42 @@ class MyQueue:
 
     def peek(self) -> int:
         if not self.out_stack:
-            while self.in_stack:
-                self.out_stack.append(self.in_stack.pop())
+            self.out_stack = self.in_stack[::-1]
+            self.in_stack.clear()
         return self.out_stack[-1]
-
 
     def empty(self) -> bool:
         return not self.in_stack and not self.out_stack
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+
+# class MyQueue:
+
+#     def __init__(self):
+#         self.in_stack = []
+#         self.out_stack = []
+
+#     def push(self, x: int) -> None:
+#         self.in_stack.append(x)
+
+#     def pop(self) -> int:
+#         self.peek()
+#         return self.out_stack.pop()
+
+#     def peek(self) -> int:
+#         if not self.out_stack:
+#             while self.in_stack:
+#                 self.out_stack.append(self.in_stack.pop())
+#         return self.out_stack[-1]
+
+
+#     def empty(self) -> bool:
+#         return not self.in_stack and not self.out_stack
         
 
 
