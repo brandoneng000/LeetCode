4dc38124c699e213cda34e52c5720367a211722c
Brandon Eng <brandoneng000@gmail.com>
2024-01-31 04:16:24 -0800

    Finished 1910

---
+++ medium/1910.py
@@ -0,0 +1,22 @@
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        part_list = list(part)
+        stack = []
+
+        for letter in s:
+            stack.append(letter)
+
+            if stack[-len(part):] == part_list:
+                for _ in range(len(part)):
+                    stack.pop()
+        
+        return ''.join(stack)
+        
+def main():
+    sol = Solution()
+    print(sol.removeOccurrences(s = "aabababa", part = "aba"))
+    print(sol.removeOccurrences(s = "daabcbaabcbc", part = "abc"))
+    print(sol.removeOccurrences(s = "axxxxyyyyb", part = "xy"))
+
+if __name__ == '__main__':
+    main()
