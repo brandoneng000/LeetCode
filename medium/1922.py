11771934930dc0fc7739bd705e75c3064470f438
Brandon Eng <brandoneng000@gmail.com>
2024-02-03 00:56:48 -0800

    Finished 1922

---
+++ medium/1922.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        mod = 1000000007
+        res = 5 ** (n % 2)
+        x = 20
+        i = n // 2
+
+        while i > 0:
+            if i % 2 == 1:
+                res = res * x % mod
+            x = x * x % mod
+            i //= 2
+        
+        return res
+        
+def main():
+    sol = Solution()
+    print(sol.countGoodNumbers(1))
+    print(sol.countGoodNumbers(4))
+    print(sol.countGoodNumbers(50))
+
+if __name__ == '__main__':
+    main()
