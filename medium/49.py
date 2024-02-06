eb5591ed362019e20837a91b671a986a8bb54cfe
Brandon Eng <brandoneng000@gmail.com>
2024-02-06 04:00:08 -0800

    Updated 49

--- medium/49.py
+++ medium/49.py
@@ -1,23 +1,35 @@
 from typing import List
+from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = {}
+        anagrams = defaultdict(list)
 
         for word in strs:
             letters = "".join(sorted(word))
-            # if letters in anagrams:
-            #     anagrams[letters].append(word)
-            # else:
-            #     anagrams[letters] = [word]
-            anagrams[letters] = anagrams.get(letters, []) + [word]
+            anagrams[letters].append(word)
         
-        return list(anagrams.values())
+        return [anagrams[letters] for letters in anagrams]
+
+    # def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    #     anagrams = {}
+
+    #     for word in strs:
+    #         letters = "".join(sorted(word))
+    #         # if letters in anagrams:
+    #         #     anagrams[letters].append(word)
+    #         # else:
+    #         #     anagrams[letters] = [word]
+    #         anagrams[letters] = anagrams.get(letters, []) + [word]
+        
+    #     return list(anagrams.values())
 
         
 def main():
     sol = Solution()
     print(sol.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+    print(sol.groupAnagrams([""]))
+    print(sol.groupAnagrams(["a"]))
 
 if __name__ == '__main__':
     main()
