c8efd66534076af204cb5cdf5646bf3946dd6162
Brandon Eng <brandoneng000@gmail.com>
2024-02-22 07:09:13 -0800

    Updated 997

--- easy/997.py
+++ easy/997.py
@@ -1,21 +1,37 @@
 from typing import List
+from collections import Counter
 
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        from collections import defaultdict
-        town = defaultdict(list)
-        judge_trust = set([ppl for ppl in range(1, n + 1)])
+        town = Counter()
+        judge = set(range(1, n + 1))
 
-        for t in trust:
-            town[t[1]].append(t[0])
-            if t[0] in judge_trust:
-                judge_trust.remove(t[0])
-            
-        for judge in judge_trust:
-            if len(town[judge]) == n - 1:
-                return judge
+        for a, b in trust:
+            town[b] += 1
+            judge.discard(a)
 
+        for j in judge:
+            if town[j] == n - 1:
+                return j
+        
         return -1
+
+
+    # def findJudge(self, n: int, trust: List[List[int]]) -> int:
+    #     from collections import defaultdict
+    #     town = defaultdict(list)
+    #     judge_trust = set([ppl for ppl in range(1, n + 1)])
+
+    #     for t in trust:
+    #         town[t[1]].append(t[0])
+    #         if t[0] in judge_trust:
+    #             judge_trust.remove(t[0])
+            
+    #     for judge in judge_trust:
+    #         if len(town[judge]) == n - 1:
+    #             return judge
+
+    #     return -1
 
 def main():
     sol = Solution()
