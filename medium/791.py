98726173930e6d87f34083f468ae246cba12f9f0
Brandon Eng <brandoneng000@gmail.com>
2024-03-11 01:57:29 -0700

    Updated 791

--- medium/791.py
+++ medium/791.py
@@ -1,18 +1,35 @@
+from string import ascii_lowercase
 import collections
 
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        count_s = collections.Counter(s)
-        res = []
-        for c in order:
-            if c in count_s:
-                res.extend([c] * count_s[c])
-                count_s.pop(c)
+        count_alphabet = {}
 
-        for c in count_s:
-            res.extend([c] * count_s[c])
+        for letter in order:
+            count_alphabet[letter] = 0
         
-        return "".join(res)
+        for letter in ascii_lowercase:
+            if letter in count_alphabet:
+                continue
+            count_alphabet[letter] = 0
+        
+        for letter in s:
+            count_alphabet[letter] += 1
+        
+        return "".join((letter * count_alphabet[letter]) for letter in count_alphabet)
+
+    # def customSortString(self, order: str, s: str) -> str:
+    #     count_s = collections.Counter(s)
+    #     res = []
+    #     for c in order:
+    #         if c in count_s:
+    #             res.extend([c] * count_s[c])
+    #             count_s.pop(c)
+
+    #     for c in count_s:
+    #         res.extend([c] * count_s[c])
+        
+    #     return "".join(res)
 
     # def customSortString(self, order: str, s: str) -> str:
     #     custom_order = { c: i for i, c in enumerate(order) }
