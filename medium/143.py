3fca7ca97d3f3350d2737776bbf36f6562668311
Brandon Eng <brandoneng000@gmail.com>
2024-03-23 02:40:02 -0700

    Updated 143

--- medium/143.py
+++ medium/143.py
@@ -10,22 +10,49 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        array = []
-        temp = head
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
         
-        while temp:
-            array.append(temp)
-            temp = temp.next
+        prev, cur = None, slow.next
+        while cur:
+            next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
         
-        alternate = True
-        head = temp = array.pop(0)
-        while array:
-            if alternate:
-                node = array.pop()
-            else:
-                node = array.pop(0)
-            temp.next = node
-            temp = temp.next
-            alternate = not alternate
+        slow.next = None
+        head1, head2 = head, prev
+
+        while head2:
+            next = head1.next
+            head1.next = head2
+            head1 = head2
+            head2 = next
         
-        return head
+
+    # def reorderList(self, head: Optional[ListNode]) -> None:
+    #     """
+    #     Do not return anything, modify head in-place instead.
+    #     """
+    #     array = []
+    #     temp = head
+        
+    #     while temp:
+    #         array.append(temp)
+    #         temp = temp.next
+        
+    #     alternate = True
+    #     head = temp = array.pop(0)
+    #     while array:
+    #         if alternate:
+    #             node = array.pop()
+    #         else:
+    #             node = array.pop(0)
+    #         temp.next = node
+    #         temp = temp.next
+    #         alternate = not alternate
+        
+    #     return head
