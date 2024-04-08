c396e3e34d9054d43c07d232ccfed145877331f5
Brandon Eng <brandoneng000@gmail.com>
2024-04-08 02:52:36 -0700

    Updated 1700

--- easy/1700.py
+++ easy/1700.py
@@ -3,28 +3,42 @@ import collections
 
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        # count = 0
-        
-        # while students:
-        #     if students[0] != sandwiches[0]:
-        #         students.append(students.pop(0))
-        #         count += 1
-        #         if count == len(students):
-        #             return count
-        #     else:
-        #         students.pop(0)
-        #         sandwiches.pop(0)
-        #         count = 0
-        
-        # return count
-        students = collections.Counter(students)
+        n = len(students)
+        orders = collections.Counter(students)
+        i = 0
 
-        for sandwich in sandwiches:
-            if students[sandwich] == 0:
-                break
-            students[sandwich] -= 1
+        while i < n and orders[sandwiches[i]]:
+            orders[sandwiches[i]] -= 1
+            i += 1
         
-        return students[0] + students[1]
+        return n - i
+        
+
+    # def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+    #     count = 0
+        
+    #     while students:
+    #         if students[0] != sandwiches[0]:
+    #             students.append(students.pop(0))
+    #             count += 1
+    #             if count == len(students):
+    #                 return count
+    #         else:
+    #             students.pop(0)
+    #             sandwiches.pop(0)
+    #             count = 0
+        
+    #     return count
+    
+    # def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+    #     students = collections.Counter(students)
+
+    #     for sandwich in sandwiches:
+    #         if students[sandwich] == 0:
+    #             break
+    #         students[sandwich] -= 1
+        
+    #     return students[0] + students[1]
         
 
 def main():
