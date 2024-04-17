a441fe7d502f7efad5854f7dde393365857d9d52
Brandon Eng <brandoneng000@gmail.com>
2024-04-17 01:56:53 -0700

    Updated 988

--- medium/988.py
+++ medium/988.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from collections import deque
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -8,18 +9,42 @@ class TreeNode:
         self.right = right
 class Solution:
     def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
-        def dfs(root: TreeNode):
-            if not root:
-                return []
-            
-            letter = chr(root.val + ord('a'))
-            
-            if not root.left and not root.right:
-                return [letter]
-            
-            left = dfs(root.left)
-            right = dfs(root.right)
+        q = deque([["", root]])
+        res = None
 
-            return [letters + letter for letters in left + right]
+        while q:
+            size = len(q)
+
+            for _ in range(size):
+                cur, node = q.popleft()
+                cur = chr(node.val + ord('a')) + cur
+
+                if not node.left and not node.right:
+                    if not res:
+                        res = cur
+                    res = min(res, cur)
+                
+                if node.left:
+                    q.append([cur, node.left])
+                if node.right:
+                    q.append([cur, node.right])
         
-        return min(dfs(root))
+        return res
+
+
+    # def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+    #     def dfs(root: TreeNode):
+    #         if not root:
+    #             return []
+            
+    #         letter = chr(root.val + ord('a'))
+            
+    #         if not root.left and not root.right:
+    #             return [letter]
+            
+    #         left = dfs(root.left)
+    #         right = dfs(root.right)
+
+    #         return [letters + letter for letters in left + right]
+        
+    #     return min(dfs(root))
