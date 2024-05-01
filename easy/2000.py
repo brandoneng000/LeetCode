02afa2a5310fa58e5e6e30ad0b3f6e25e9833715
Brandon Eng <brandoneng000@gmail.com>
2024-05-01 02:12:02 -0700

    Updated 2000

--- easy/2000.py
+++ easy/2000.py
@@ -1,14 +1,25 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        # if ch not in word:
-        #     return word
-        # prefix = list(word[:word.index(ch) + 1])
-        # prefix.reverse()
-        # return "".join(prefix) + word[word.index(ch) + 1:]
-        if ch not in word:
-            return word
+        n = len(word)
+
+        for i in range(n):
+            if word[i] == ch:
+                return word[:i + 1][::-1] + word[i + 1:]
         
-        return word[:word.find(ch) + 1][::-1] + word[word.find(ch) + 1:]
+        return word
+        
+    # def reversePrefix(self, word: str, ch: str) -> str:
+    #     if ch not in word:
+    #         return word
+    #     prefix = list(word[:word.index(ch) + 1])
+    #     prefix.reverse()
+    #     return "".join(prefix) + word[word.index(ch) + 1:]
+
+    # def reversePrefix(self, word: str, ch: str) -> str:
+    #     if ch not in word:
+    #         return word
+        
+    #     return word[:word.find(ch) + 1][::-1] + word[word.find(ch) + 1:]
 
 def main():
     sol = Solution()
