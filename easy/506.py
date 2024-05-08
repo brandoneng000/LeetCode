50741748d6e00bd303d9e376a4ac480af57e3436
Brandon Eng <brandoneng000@gmail.com>
2024-05-08 04:30:53 -0700

    Updated 506

--- easy/506.py
+++ easy/506.py
@@ -1,27 +1,51 @@
 from typing import List
+from heapq import heappush, heappop
 
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
-        first = "Gold Medal"
-        second = "Silver Medal"
-        third = "Bronze Medal"
-        ranks = {}
-        scores = sorted(score, reverse=True)
+        n = len(score)
+        res = [""] * n
+        medals = { 1: "Gold Medal", 2: "Silver Medal", 3: "Bronze Medal" }
+        heap = []
+        count = 1
 
-        for index, s in enumerate(scores):
-            if index == 0:
-                ranks[s] = first
-            elif index == 1:
-                ranks[s] = second
-            elif index == 2:
-                ranks[s] = third
+        for i in range(n):
+            heappush(heap, (-score[i], i))
+
+        while heap:
+            s, i = heappop(heap)
+
+            if count in medals:
+                res[i] = medals[count]
             else:
-                ranks[s] = str(index + 1)
+                res[i] = str(count)
+
+            count += 1
+
+        return res
+
+
+    # def findRelativeRanks(self, score: List[int]) -> List[str]:
+    #     first = "Gold Medal"
+    #     second = "Silver Medal"
+    #     third = "Bronze Medal"
+    #     ranks = {}
+    #     scores = sorted(score, reverse=True)
+
+    #     for index, s in enumerate(scores):
+    #         if index == 0:
+    #             ranks[s] = first
+    #         elif index == 1:
+    #             ranks[s] = second
+    #         elif index == 2:
+    #             ranks[s] = third
+    #         else:
+    #             ranks[s] = str(index + 1)
         
-        results = []
-        for s in score:
-            results.append(ranks[s])
-        return results
+    #     results = []
+    #     for s in score:
+    #         results.append(ranks[s])
+    #     return results
 
 def main():
     sol = Solution()
