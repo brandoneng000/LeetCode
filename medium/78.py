2682652c04293870475a0eb19d19a09ac4213245
Brandon Eng <brandoneng000@gmail.com>
2024-05-21 02:08:06 -0700

    Updated 78

--- medium/78.py
+++ medium/78.py
@@ -2,21 +2,35 @@ from typing import List
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        def helper(cur: List[int], index: int):
+            res.append(cur.copy())
 
-        def combination(combo: List[int], cur_num: int, size: int):
-            if len(combo) == size:
-                result.append(combo.copy())
+            for i in range(index, n):
+                cur.append(nums[i])
+                helper(cur, i + 1)
+                cur.pop()
+
+        n = len(nums)
+        res = []
+        helper([], 0)
+        return res
+        
+    # def subsets(self, nums: List[int]) -> List[List[int]]:
+    #     result = []
+
+    #     def combination(combo: List[int], cur_num: int, size: int):
+    #         if len(combo) == size:
+    #             result.append(combo.copy())
             
-            for index in range(cur_num + 1, len(nums)):
-                combo.append(nums[index])
-                combination(combo, index, size)
-                combo.pop()
+    #         for index in range(cur_num + 1, len(nums)):
+    #             combo.append(nums[index])
+    #             combination(combo, index, size)
+    #             combo.pop()
         
-        for size in range(len(nums) + 1):
-            combination([], -1, size)
+    #     for size in range(len(nums) + 1):
+    #         combination([], -1, size)
         
-        return result
+    #     return result
 
 def main():
     sol = Solution()
