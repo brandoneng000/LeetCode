f8253a1810d8dc604aefcb4edf6099ba5ee926a1
Brandon Eng <brandoneng000@gmail.com>
2024-05-22 02:36:48 -0700

    Finished 2396

---
+++ medium/2396.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        return False
+    
+def main():
+    sol = Solution()
+    print(sol.isStrictlyPalindromic(9))
+    print(sol.isStrictlyPalindromic(4))
+
+if __name__ == '__main__':
+    main()
