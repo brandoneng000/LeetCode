74a51b3e9d692d8491908ba0473326f72c07d3e8
Brandon Eng <brandoneng000@gmail.com>
2024-06-02 01:29:00 -0700

    Updated 344

--- easy/344.py
+++ easy/344.py
@@ -2,17 +2,23 @@ from typing import List
 
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        size = len(s) // 2
-        index = 0
+        n = len(s)
+
+        for i in range(n // 2):
+            s[i], s[n - i - 1] = s[n - i - 1], s[i]
+
+    # def reverseString(self, s: List[str]) -> None:
+    #     """
+    #     Do not return anything, modify s in-place instead.
+    #     """
+    #     size = len(s) // 2
+    #     index = 0
         
-        while index < size:
-            temp = s[index]
-            s[index] = s[len(s) - index - 1]
-            s[len(s) - index - 1] = temp
-            index += 1
+    #     while index < size:
+    #         temp = s[index]
+    #         s[index] = s[len(s) - index - 1]
+    #         s[len(s) - index - 1] = temp
+    #         index += 1
 
 
 def main():
