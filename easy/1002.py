ef81dc02192ed1d18d45b6ad54ab8abe3b7d9b7f
Brandon Eng <brandoneng000@gmail.com>
2024-06-05 01:59:11 -0700

    Updated 1002

--- easy/1002.py
+++ easy/1002.py
@@ -1,31 +1,56 @@
 from typing import List
+from collections import Counter
+from string import ascii_lowercase
 
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        dictionary = {}
-        result = []
-        
-        for letter in words[0]:
-            if letter not in dictionary:
-                dictionary[letter] = 1
-            else:
-                dictionary[letter] += 1
+        res = Counter(words[0])
 
-        for index in range(1, len(words)):
-            for letter in dictionary:
-                if letter not in words[index]:
-                    dictionary[letter] = 0
-                elif dictionary[letter] > words[index].count(letter):
-                    dictionary[letter] = words[index].count(letter)
+        for word in words:
+            res &= Counter(word)
         
-        for letter in dictionary:
-            result += [letter] * dictionary[letter]
+        return list(res.elements())
+        
+    # def commonChars(self, words: List[str]) -> List[str]:
+    #     res = Counter(ascii_lowercase)
 
-        return result
+    #     for letter in res:
+    #         res[letter] = 100
+        
+    #     for word in words:
+    #         count = Counter(word)
+    #         for letter in res:
+    #             res[letter] = min(res[letter], count[letter])
+
+    #     return list(res.elements())
+
+
+    # def commonChars(self, words: List[str]) -> List[str]:
+    #     dictionary = {}
+    #     result = []
+        
+    #     for letter in words[0]:
+    #         if letter not in dictionary:
+    #             dictionary[letter] = 1
+    #         else:
+    #             dictionary[letter] += 1
+
+    #     for index in range(1, len(words)):
+    #         for letter in dictionary:
+    #             if letter not in words[index]:
+    #                 dictionary[letter] = 0
+    #             elif dictionary[letter] > words[index].count(letter):
+    #                 dictionary[letter] = words[index].count(letter)
+        
+    #     for letter in dictionary:
+    #         result += [letter] * dictionary[letter]
+
+    #     return result
                     
 def main():
     sol = Solution()
     print(sol.commonChars(["bella", "label", "roller"]))
+    print(sol.commonChars(["cool","lock","cook"]))
     print(sol.commonChars(["lllllllllllllllllllllllll", "l"]))
 
 if __name__ == '__main__':
