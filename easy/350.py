b60fab6278b4d00e0ff174ec620adb99377f090b
Brandon Eng <brandoneng000@gmail.com>
2024-07-02 01:23:44 -0700

    Updated 350

--- easy/350.py
+++ easy/350.py
@@ -1,17 +1,30 @@
 from typing import List
+from collections import Counter
 
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1_count = Counter(nums1)
+        nums2_count = Counter(nums2)
         res = []
-        short = nums1 if len(nums1) <= len(nums2) else nums2
-        long = nums1 if len(nums1) > len(nums2) else nums2
 
-        for num in short:
-            if num in long:
-                res.append(num)
-                long.remove(num)
+        for num in nums1_count:
+            if num in nums2_count:
+                res.extend([num] * min(nums1_count[num], nums2_count[num]))
         
         return res
+
+
+    # def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    #     res = []
+    #     short = nums1 if len(nums1) <= len(nums2) else nums2
+    #     long = nums1 if len(nums1) > len(nums2) else nums2
+
+    #     for num in short:
+    #         if num in long:
+    #             res.append(num)
+    #             long.remove(num)
+        
+    #     return res
         
 def main():
     sol = Solution()
