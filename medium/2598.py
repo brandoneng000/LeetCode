1d7fb57e8cec5e3bbc736983f2bd9879b578f3f5
Brandon Eng <brandoneng000@gmail.com>
2024-07-09 01:42:49 -0700

    Finished 2598

---
+++ medium/2598.py
@@ -0,0 +1,21 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def findSmallestInteger(self, nums: List[int], value: int) -> int:
+        count = Counter([num % value for num in nums])
+        res = 0
+
+        while count[res % value]:
+            count[res % value] -= 1
+            res += 1
+        
+        return res
+
+def main():
+    sol = Solution()
+    print(sol.findSmallestInteger(nums = [1,-10,7,13,6,8], value = 5))
+    print(sol.findSmallestInteger(nums = [1,-10,7,13,6,8], value = 7))
+
+if __name__ == '__main__':
+    main()
