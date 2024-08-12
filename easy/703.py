c6b06a59837e1b4f314f3d31eed4a3ccbbdf2949
Brandon Eng <brandoneng000@gmail.com>
2024-08-12 02:34:37 -0700

    Updated 703

--- easy/703.py
+++ easy/703.py
@@ -1,17 +1,39 @@
 from typing import List
+from heapq import heappush, heappushpop
 
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
+        self.heap = []
         self.k = k
-        self.nums = sorted(nums)
+
+        for i in range(len(nums)):
+            if k:
+                heappush(self.heap, nums[i])
+                k -= 1
+            else:
+                heappushpop(self.heap, nums[i])
+        
         
 
     def add(self, val: int) -> int:
-        self.nums.append(val)
-        self.nums.sort()
-        return self.nums[-self.k]
+        if len(self.heap) < self.k:
+            heappush(self.heap, val)
+        else:
+            heappushpop(self.heap, val)
+        return self.heap[0]
 
+# class KthLargest:
+
+#     def __init__(self, k: int, nums: List[int]):
+#         self.k = k
+#         self.nums = sorted(nums)
+        
+
+#     def add(self, val: int) -> int:
+#         self.nums.append(val)
+#         self.nums.sort()
+#         return self.nums[-self.k]
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
