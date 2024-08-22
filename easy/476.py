9b10aad95f3c4920b3718cbfc36509c4a89a89c8
Brandon Eng <brandoneng000@gmail.com>
2024-08-22 06:18:52 -0700

    Updated 476

--- easy/476.py
+++ easy/476.py
@@ -1,15 +1,24 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        temp = num
-        mask = 0
-        while temp:
-            temp //= 2
-            mask = (mask << 1) + 1
+        cur = 1
 
-        return num ^ mask
+        while cur <= num:
+            cur *= 2
+
+        return num ^ (cur - 1)
+
+    # def findComplement(self, num: int) -> int:
+    #     temp = num
+    #     mask = 0
+    #     while temp:
+    #         temp //= 2
+    #         mask = (mask << 1) + 1
+
+    #     return num ^ mask
 
 def main():
     sol = Solution()
+    print(sol.findComplement(2))
     print(sol.findComplement(5))
     print(sol.findComplement(1))
 
