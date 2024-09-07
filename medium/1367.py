107dec013b70b654c7ea1e5e297dbea72937e294
Brandon Eng <brandoneng000@gmail.com>
2024-09-07 06:35:28 -0700

    Updated 1367

--- medium/1367.py
+++ medium/1367.py
@@ -1,4 +1,5 @@
 from typing import Optional, List
+from collections import deque
 
 # Definition for singly-linked list.
 class ListNode:
@@ -13,19 +14,46 @@ class TreeNode:
         self.right = right
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-        def dfs(head: ListNode, root: TreeNode):
-            if not head:
-                return True
-            if not root:
-                return False
+        q = deque([(head, root)])
+
+        while q:
+            linked_list_node, tree_node = q.popleft()
+
+            if linked_list_node.val == tree_node.val:
+                if not linked_list_node.next:
+                    return True
+                
+                if tree_node.left:
+                    q.append((linked_list_node.next, tree_node.left))
+                
+                if tree_node.right:
+                    q.append((linked_list_node.next, tree_node.right))
             
-            return root.val == head.val and (dfs(head.next, root.left) or dfs(head.next, root.right))
+            if linked_list_node != head:
+                continue
+            
+            if tree_node.left:
+                q.append((head, tree_node.left))
+            
+            if tree_node.right:
+                q.append((head, tree_node.right))
         
-        if not head:
-            return True
-        if not root:
-            return False
+        return False
+
+    # def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+    #     def dfs(head: ListNode, root: TreeNode):
+    #         if not head:
+    #             return True
+    #         if not root:
+    #             return False
+            
+    #         return root.val == head.val and (dfs(head.next, root.left) or dfs(head.next, root.right))
         
-        return dfs(head, root) or self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
+    #     if not head:
+    #         return True
+    #     if not root:
+    #         return False
+        
+    #     return dfs(head, root) or self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
 
         
