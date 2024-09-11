c0d863b7ec7995071c131a21553fdd9b096fdfdb
Brandon Eng <brandoneng000@gmail.com>
2024-09-11 01:49:27 -0700

    Updated 2220

--- easy/2220.py
+++ easy/2220.py
@@ -1,14 +1,20 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        res = 0
+        return (start ^ goal).bit_count()
+
+    # def minBitFlips(self, start: int, goal: int) -> int:
+    #     return bin(start ^ goal).count('1')
         
-        while start or goal:
-            if start & 1 != goal & 1:
-                res += 1
-            start >>= 1
-            goal >>= 1
+    # def minBitFlips(self, start: int, goal: int) -> int:
+    #     res = 0
         
-        return res
+    #     while start or goal:
+    #         if start & 1 != goal & 1:
+    #             res += 1
+    #         start >>= 1
+    #         goal >>= 1
+        
+    #     return res
 
 
 def main():
