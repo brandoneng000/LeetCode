aac5597cac9020e320e66d102780951bd67307f8
Brandon Eng <brandoneng000@gmail.com>
2024-09-12 03:41:40 -0700

    Updated 1684

--- easy/1684.py
+++ easy/1684.py
@@ -2,16 +2,22 @@ from typing import List
 
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        res = 0
+        allowed_set = set(allowed)
 
-        for word in words:
-            for letter in allowed:
-                word = word.replace(letter, "")
+        return sum(not (set(word) - allowed_set) for word in words)
+
+
+    # def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+    #     res = 0
+
+    #     for word in words:
+    #         for letter in allowed:
+    #             word = word.replace(letter, "")
             
-            if not len(word):
-                res += 1
+    #         if not len(word):
+    #             res += 1
 
-        return res
+    #     return res
 
 def main():
     sol = Solution()
