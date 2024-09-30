2c782f6f5dd2c20b8edfebbc12fbf85e87ca6dae
Brandon Eng <brandoneng000@gmail.com>
2024-09-30 03:05:27 -0700

    Updated 1381

--- medium/1381.py
+++ medium/1381.py
@@ -1,3 +1,27 @@
+# class CustomStack:
+
+#     def __init__(self, maxSize: int):
+#         self.size = maxSize
+#         self.stack = []
+#         self.inc = { i: 0 for i in range(maxSize) }
+
+#     def push(self, x: int) -> None:
+#         if self.size == len(self.stack):
+#             return
+        
+#         self.stack.append(x)
+
+#     def pop(self) -> int:
+#         if self.stack:
+#             temp = self.inc[len(self.stack) - 1]
+#             self.inc[len(self.stack) - 1] = 0
+#             return self.stack.pop() + temp
+#         return -1
+
+#     def increment(self, k: int, val: int) -> None:
+#         for i in range(min(k, len(self.stack))):
+#             self.inc[i] += val
+
 class CustomStack:
 
     def __init__(self, maxSize: int):
