5fb9c5c204517b9d52c865ffd35f49258566fbed
Brandon Eng <brandoneng000@gmail.com>
2024-10-06 03:27:52 -0700

    Updated 1813

--- medium/1813.py
+++ medium/1813.py
@@ -1,28 +1,47 @@
+from collections import deque
+
 class Solution:
     def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
-        if len(sentence1) < len(sentence2):
+        if len(sentence1) > len(sentence2):
             sentence1, sentence2 = sentence2, sentence1
         
-        s1, s2 = sentence1.split(), sentence2.split()
-        n, m = len(s1), len(s2)
-        j = 0
+        s1, s2 = deque(sentence1.split()), deque(sentence2.split())
 
-        for i in range(n):
-            if j < m and s1[i] == s2[j]:
-                j += 1
-            else:
-                break
+        while s1 and s2 and s1[0] == s2[0]:
+            s1.popleft()
+            s2.popleft()
+
+        while s1 and s2 and s1[-1] == s2[-1]:
+            s1.pop()
+            s2.pop()
         
-        n -= 1
-        for j in range(m - 1, j - 1, -1):
-            if s1[n] == s2[j]:
-                n -= 1
-            else:
-                break
-        else:
-            return True
+        return len(s1) == 0
 
-        return False
+
+    # def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+    #     if len(sentence1) < len(sentence2):
+    #         sentence1, sentence2 = sentence2, sentence1
+        
+    #     s1, s2 = sentence1.split(), sentence2.split()
+    #     n, m = len(s1), len(s2)
+    #     j = 0
+
+    #     for i in range(n):
+    #         if j < m and s1[i] == s2[j]:
+    #             j += 1
+    #         else:
+    #             break
+        
+    #     n -= 1
+    #     for j in range(m - 1, j - 1, -1):
+    #         if s1[n] == s2[j]:
+    #             n -= 1
+    #         else:
+    #             break
+    #     else:
+    #         return True
+
+    #     return False
         
 def main():
     sol = Solution()
