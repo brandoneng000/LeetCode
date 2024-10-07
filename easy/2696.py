5742f3277c466ddc480b6b84c300229d7542d85e
Brandon Eng <brandoneng000@gmail.com>
2024-10-07 02:41:46 -0700

    Updated 2696

--- easy/2696.py
+++ easy/2696.py
@@ -1,14 +1,21 @@
 class Solution:
     def minLength(self, s: str) -> int:
-        stack = []
-
-        for letter in s:
-            stack.append(letter)
-            if stack[-2:] == ['A', 'B'] or stack[-2:] == ['C', 'D']:
-                stack.pop()
-                stack.pop()
+        while 'AB' in s or 'CD' in s:
+            s = s.replace('AB', '')
+            s = s.replace('CD', '')
         
-        return len(stack)
+        return len(s)
+
+    # def minLength(self, s: str) -> int:
+    #     stack = []
+
+    #     for letter in s:
+    #         stack.append(letter)
+    #         if stack[-2:] == ['A', 'B'] or stack[-2:] == ['C', 'D']:
+    #             stack.pop()
+    #             stack.pop()
+        
+    #     return len(stack)
         
 def main():
     sol = Solution()
