f5cf73ee3cc88e0fdff76f7d46b65c3cd2475723
Brandon Eng <brandoneng000@gmail.com>
2024-10-09 06:25:33 -0700

    Updated 921

--- medium/921.py
+++ medium/921.py
@@ -1,18 +1,44 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        stack = []
+        open_brackets = 0
+        additional_brackets = 0
 
         for p in s:
-            if p == "(":
-                stack.append(p)
-            elif not stack and p == ")":
-                stack.append(")")
-            elif stack[-1] == "(" and p == ")":
-                stack.pop()
+            if p == '(':
+                open_brackets += 1
             else:
-                stack.append(p)
+                if open_brackets > 0:
+                    open_brackets -= 1
+                else:
+                    additional_brackets += 1
+            
+        return open_brackets + additional_brackets
+
+    # def minAddToMakeValid(self, s: str) -> int:
+    #     stack = []
+
+    #     for p in s:
+    #         if stack and stack[-1] == '(' and p == ')':
+    #             stack.pop()
+    #         else:
+    #             stack.append(p)
         
-        return len(stack)
+    #     return len(stack)
+
+    # def minAddToMakeValid(self, s: str) -> int:
+    #     stack = []
+
+    #     for p in s:
+    #         if p == "(":
+    #             stack.append(p)
+    #         elif not stack and p == ")":
+    #             stack.append(")")
+    #         elif stack[-1] == "(" and p == ")":
+    #             stack.pop()
+    #         else:
+    #             stack.append(p)
+        
+    #     return len(stack)
 
 def main():
     sol = Solution()
