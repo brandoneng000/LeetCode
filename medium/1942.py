dca23af8fa5b196e559218ebb3c3b59b8515f368
Brandon Eng <brandoneng000@gmail.com>
2024-10-11 02:31:19 -0700

    Updated 1942

--- medium/1942.py
+++ medium/1942.py
@@ -3,24 +3,41 @@ from heapq import heappush, heappop
 
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
-        n = len(times)
-        friends = sorted((times[i][0], times[i][1], i) for i in range(n))
-        seats = [False] * n
-        heap = []
+        order = sorted(range(len(times)), key=lambda x: times[x][0])
+        seats_empty, seats_taken = list(range(len(times))), []
 
-        for arrival, leave, index in friends:
-            while heap and heap[0][0] <= arrival:
-                leaving, _, s = heappop(heap)
-                seats[s] = False
+        for i in order:
+            arrival, leave = times[i]
+
+            while seats_taken and seats_taken[0][0] <= arrival:
+                heappush(seats_empty, heappop(seats_taken)[1])
             
-            for i in range(n):
-                if not seats[i]:
-                    if index == targetFriend:
-                        return i
+            seat = heappop(seats_empty)
+            
+            if i == targetFriend:
+                return seat
+        
+            heappush(seats_taken, (leave, seat))
+
+    # def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+    #     n = len(times)
+    #     friends = sorted((times[i][0], times[i][1], i) for i in range(n))
+    #     seats = [False] * n
+    #     heap = []
+
+    #     for arrival, leave, index in friends:
+    #         while heap and heap[0][0] <= arrival:
+    #             leaving, _, s = heappop(heap)
+    #             seats[s] = False
+            
+    #         for i in range(n):
+    #             if not seats[i]:
+    #                 if index == targetFriend:
+    #                     return i
                     
-                    seats[i] = True
-                    heappush(heap, (leave, index, i))
-                    break
+    #                 seats[i] = True
+    #                 heappush(heap, (leave, index, i))
+    #                 break
                 
 
         
