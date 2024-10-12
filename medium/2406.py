739ca2252eb26fa593eb5c8535bc815b80bdbb3c
Brandon Eng <brandoneng000@gmail.com>
2024-10-12 02:29:08 -0700

    Updated 2406

--- medium/2406.py
+++ medium/2406.py
@@ -2,18 +2,33 @@ from typing import List
 
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
-        n = max(right for left, right in intervals)
-        prefix = [0] * (n + 2)
+        groups = []
+        res = cur = 0
 
         for left, right in intervals:
-            prefix[left] += 1
-            prefix[right + 1] -= 1
+            groups.append([left, 1])
+            groups.append([right + 1, -1])
 
-        for i in range(1, n + 1):
-            prefix[i] += prefix[i - 1]
+        for time, diff in sorted(groups):
+            cur += diff
+            res = max(res, cur)
+
+        return res
+
+
+    # def minGroups(self, intervals: List[List[int]]) -> int:
+    #     n = max(right for left, right in intervals)
+    #     prefix = [0] * (n + 2)
+
+    #     for left, right in intervals:
+    #         prefix[left] += 1
+    #         prefix[right + 1] -= 1
+
+    #     for i in range(1, n + 1):
+    #         prefix[i] += prefix[i - 1]
         
-        prefix.pop()
-        return max(prefix)
+    #     prefix.pop()
+    #     return max(prefix)
         
 def main():
     sol = Solution()
