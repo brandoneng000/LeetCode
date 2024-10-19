4ea2ea216cba4bd081e09b4c43b8d619b107bd03
Brandon Eng <brandoneng000@gmail.com>
2024-10-19 06:55:00 -0700

    Updated 1545

--- medium/1545.py
+++ medium/1545.py
@@ -1,15 +1,32 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        s = '0'
+        flip = 0
+        l = 2 ** n - 1
 
-        for _ in range(n):
-            invert = "".join('0' if bit == '1' else '1' for bit in s[::-1])
-            s = s + '1' + invert
+        while k > 1:
+            if k == l // 2 + 1:
+                return str(1 ^ flip)
 
-            if len(s) > k + 1:
-                break
+            if k > l // 2:
+                k = l + 1 - k
+                flip = 1 - flip
+            
+            l //= 2
         
-        return s[k - 1]
+        return str(flip)
+
+
+    # def findKthBit(self, n: int, k: int) -> str:
+    #     s = '0'
+
+    #     for _ in range(n):
+    #         invert = "".join('0' if bit == '1' else '1' for bit in s[::-1])
+    #         s = s + '1' + invert
+
+    #         if len(s) > k + 1:
+    #             break
+        
+    #     return s[k - 1]
         
 def main():
     sol = Solution()
