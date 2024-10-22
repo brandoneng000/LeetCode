59ea5d1aea53ae004a0505ea297e69cec7e0c1a2
Brandon Eng <brandoneng000@gmail.com>
2024-10-22 06:56:17 -0700

    Updated 2583

--- medium/2583.py
+++ medium/2583.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from collections import deque
+from collections import deque, Counter
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -9,20 +9,38 @@ class TreeNode:
         self.right = right
 class Solution:
     def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
-        q = deque([root])
-        nums = []
+        def dfs(root: TreeNode, level: int):
+            if not root:
+                return
+            
+            self.levels = max(self.levels, level)
+            level_sum[level] += root.val
+            dfs(root.left, level + 1)
+            dfs(root.right, level + 1)
 
-        while q:
-            cur = 0
-            size = len(q)
-
-            for _ in range(size):
-                node = q.popleft()
-                cur += node.val
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            nums.append(cur)
+        level_sum = Counter()
+        self.levels = 0
+        dfs(root, 1)
         
-        return sorted(nums, reverse=True)[k - 1] if len(nums) >= k else -1
+        return level_sum.most_common()[k - 1][1] if self.levels >= k else -1
+
+
+
+    # def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+    #     q = deque([root])
+    #     nums = []
+
+    #     while q:
+    #         cur = 0
+    #         size = len(q)
+
+    #         for _ in range(size):
+    #             node = q.popleft()
+    #             cur += node.val
+    #             if node.left:
+    #                 q.append(node.left)
+    #             if node.right:
+    #                 q.append(node.right)
+    #         nums.append(cur)
+        
+    #     return sorted(nums, reverse=True)[k - 1] if len(nums) >= k else -1
