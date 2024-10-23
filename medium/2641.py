8ea1b9a9006935614ba3a03428203846cf4c24dd
Brandon Eng <brandoneng000@gmail.com>
2024-10-23 02:47:23 -0700

    Updated 2641

--- medium/2641.py
+++ medium/2641.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from collections import deque
+from collections import deque, defaultdict
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -9,26 +9,54 @@ class TreeNode:
         self.right = right
 class Solution:
     def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        cur_level_sum = root.val
-        q = deque([root])
-
-        while q:
-            next_level = []
-            next_level_sum = 0
-
-            for node in q:
-                node.val = cur_level_sum - node.val
-                
-                for child in node.left, node.right:
-                    if child:
-                        next_level.append(child)
-                        next_level_sum += child.val
-                
-                if node.left and node.right:
-                    total = node.left.val + node.right.val
-                    node.left.val = node.right.val = total
+        def dfs_calc(root: TreeNode, parent: TreeNode, level: int):
+            if not root:
+                return
             
-            q = next_level
-            cur_level_sum = next_level_sum
+            parent_sums[parent] += root.val
+            level_sums[level] += root.val
+            dfs_calc(root.left, root, level + 1)
+            dfs_calc(root.right, root, level + 1)
+
+        def dfs_replace(root: TreeNode, parent: TreeNode, level: int):
+            if not root:
+                return
+            
+            dfs_replace(root.left, root, level + 1)
+            dfs_replace(root.right, root, level + 1)
+
+            root.val = level_sums[level] - parent_sums[parent]
         
+        level_sums = defaultdict(int)
+        parent_sums = defaultdict(int)
+        temp = TreeNode()
+        dfs_calc(root, temp, 0)
+        dfs_replace(root, temp, 0)
+
         return root
+
+
+    # def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    #     cur_level_sum = root.val
+    #     q = deque([root])
+
+    #     while q:
+    #         next_level = []
+    #         next_level_sum = 0
+
+    #         for node in q:
+    #             node.val = cur_level_sum - node.val
+                
+    #             for child in node.left, node.right:
+    #                 if child:
+    #                     next_level.append(child)
+    #                     next_level_sum += child.val
+                
+    #             if node.left and node.right:
+    #                 total = node.left.val + node.right.val
+    #                 node.left.val = node.right.val = total
+            
+    #         q = next_level
+    #         cur_level_sum = next_level_sum
+        
+    #     return root
