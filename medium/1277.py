4a5f5d3abc826a1ad35bf7d79113fc5cdab02609
Brandon Eng <brandoneng000@gmail.com>
2024-10-27 02:49:30 -0700

    Updated 1277

--- medium/1277.py
+++ medium/1277.py
@@ -2,11 +2,24 @@ from typing import List
 
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
-        for i in range(1, len(matrix)):
-            for j in range(1, len(matrix[0])):
-                matrix[i][j] *= min(matrix[i - 1][j], matrix[i][j - 1], matrix[i - 1][j - 1]) + 1
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
+        res = 0
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j]:
+                    dp[i + 1][j + 1] = min(dp[i][j + 1], dp[i + 1][j], dp[i][j]) + 1
+                    res += dp[i + 1][j + 1]
         
-        return sum(sum(row) for row in matrix)
+        return res
+
+    # def countSquares(self, matrix: List[List[int]]) -> int:
+    #     for i in range(1, len(matrix)):
+    #         for j in range(1, len(matrix[0])):
+    #             matrix[i][j] *= min(matrix[i - 1][j], matrix[i][j - 1], matrix[i - 1][j - 1]) + 1
+        
+    #     return sum(sum(row) for row in matrix)
 
 def main():
     sol = Solution()
