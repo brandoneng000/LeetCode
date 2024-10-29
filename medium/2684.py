a5cc3feb3f5cff8f7fe087a6111634e9504e76c5
Brandon Eng <brandoneng000@gmail.com>
2024-10-29 07:05:42 -0700

    Updated 2684

--- medium/2684.py
+++ medium/2684.py
@@ -2,28 +2,57 @@ from typing import List
 
 class Solution:
     def maxMoves(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        res = 0
-        cur = set([(i, 0) for i in range(m)])
+        def dfs(row: int, col: int, grid: List[List[int]], dp: dict):
+            
+            if (row, col) in dp:
+                return dp[(row, col)]
 
-        while cur:
-            next = set()
+            res = 0
 
-            for row, col in cur:
+            for dir in directions:
+                r = row + dir
                 c = col + 1
 
-                for dr in [-1, 0, 1]:
-                    r = row + dr
-                    
-                    if 0 <= r < m and c < n and grid[row][col] < grid[r][c]:
-                        next.add((r, c))
+                if 0 <= r < m and 0 <= c < n and grid[row][col] < grid[r][c]:
+                    res = max(res, 1 + dfs(r, c, grid, dp))
 
-            if next:
-                res += 1
-            
-            cur = next
-            
+            dp[(row, col)] = res
+            return res
+
+        m, n = len(grid), len(grid[0])
+        directions = [-1, 0, 1]
+        dp = {}
+        res = 0
+
+        for i in range(m):
+            cur_moves = dfs(i, 0, grid, dp)
+            res = max(res, cur_moves)
+        
         return res
+
+    # def maxMoves(self, grid: List[List[int]]) -> int:
+    #     m, n = len(grid), len(grid[0])
+    #     res = 0
+    #     cur = set([(i, 0) for i in range(m)])
+
+    #     while cur:
+    #         next = set()
+
+    #         for row, col in cur:
+    #             c = col + 1
+
+    #             for dr in [-1, 0, 1]:
+    #                 r = row + dr
+                    
+    #                 if 0 <= r < m and c < n and grid[row][col] < grid[r][c]:
+    #                     next.add((r, c))
+
+    #         if next:
+    #             res += 1
+            
+    #         cur = next
+            
+    #     return res
 
 
         
