482d6648ca99cf520725577e40f84bed2d28c768
Brandon Eng <brandoneng000@gmail.com>
2024-10-31 05:59:41 -0700

    Finished 2900

---
+++ easy/2900.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        res = []
+        prev = ''
+
+        for word, bit in zip(words, groups):
+            if prev != bit:
+                res.append(word)
+                prev = bit
+
+        return res
+
+        
+def main():
+    sol = Solution()
+    print(sol.getLongestSubsequence(words = ["e","a","b"], groups = [0,0,1]))
+    print(sol.getLongestSubsequence(words = ["a","b","c","d"], groups = [1,0,1,1]))
+
+if __name__ == '__main__':
+    main()
