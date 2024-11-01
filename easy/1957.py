3c24f3650673f4b08d75d6c179506e927992607e
Brandon Eng <brandoneng000@gmail.com>
2024-11-01 02:15:13 -0700

    Updated 1957

--- easy/1957.py
+++ easy/1957.py
@@ -1,15 +1,28 @@
+from string import ascii_lowercase
+
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        from string import ascii_lowercase
-        triplets = []
-        for letter in ascii_lowercase:
-            triplets.append(letter * 3)
+        n = len(s)
+        res = list(s[:2])
 
-        for triple in triplets:
-            while triple in s:
-                s = s.replace(triple, triple[:2])
+        for i in range(2, n):
+            if res[-2] == res[-1] == s[i]:
+                continue
+            else:
+                res.append(s[i])
+
+        return ''.join(res)
+
+    # def makeFancyString(self, s: str) -> str:
+    #     triplets = []
+    #     for letter in ascii_lowercase:
+    #         triplets.append(letter * 3)
+
+    #     for triple in triplets:
+    #         while triple in s:
+    #             s = s.replace(triple, triple[:2])
         
-        return s
+    #     return s
 
 
 def main():
