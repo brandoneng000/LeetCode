5f8fd63cd112fabf6ae9bf384a34a6f6ab8fcb42
Brandon Eng <brandoneng000@gmail.com>
2024-11-02 05:30:07 -0700

    Finished 2490

--- easy/2490.py
+++ easy/2490.py
@@ -1,14 +1,36 @@
 class Solution:
     def isCircularSentence(self, sentence: str) -> bool:
         sentence = sentence.split()
-        cur_word = sentence[0]
+        n = len(sentence)
 
-        for i in range(len(sentence) - 1, -1, -1):
-            if cur_word[0] != sentence[i][-1]:
+        for i in range(n - 1, -1, -1):
+            if sentence[i][-1] != sentence[(i + 1) % n][0]:
                 return False
-            cur_word = sentence[i]
-
+        
         return True
+
+
+    # def isCircularSentence(self, sentence: str) -> bool:
+    #     n = len(sentence)
+
+    #     for i in range(n):
+    #         if sentence[i] == ' ' and sentence[i - 1] != sentence[i + 1]:
+    #             return False
+    #         elif i == n - 1 and sentence[i] != sentence[0]:
+    #             return False
+        
+    #     return True
+
+    # def isCircularSentence(self, sentence: str) -> bool:
+    #     sentence = sentence.split()
+    #     cur_word = sentence[0]
+
+    #     for i in range(len(sentence) - 1, -1, -1):
+    #         if cur_word[0] != sentence[i][-1]:
+    #             return False
+    #         cur_word = sentence[i]
+
+    #     return True
 
 def main():
     sol = Solution()
