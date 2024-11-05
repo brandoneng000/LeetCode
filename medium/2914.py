70def1fe0f993435cf9ae4e9de683a474bbc85b3
Brandon Eng <brandoneng000@gmail.com>
2024-11-05 03:27:43 -0800

    Updated 2914

--- medium/2914.py
+++ medium/2914.py
@@ -3,24 +3,34 @@ class Solution:
     def minChanges(self, s: str) -> int:
         n = len(s)
         res = 0
-
-        if s.count('1') == n or s.count('1') == 0:
-            return res
         
-        i = 0
-        j = i + 1
-
-        while j < n:
-            if s[i] != s[j]:
-                res += 1
-            
-            i += 2
-            j += 2
-                
+        for i in range(0, n - 1, 2):
+            res += s[i] != s[i + 1]
+        
         return res
+
+    # def minChanges(self, s: str) -> int:
+    #     n = len(s)
+    #     res = 0
+
+    #     if s.count('1') == n or s.count('1') == 0:
+    #         return res
+        
+    #     i = 0
+    #     j = i + 1
+
+    #     while j < n:
+    #         if s[i] != s[j]:
+    #             res += 1
+            
+    #         i += 2
+    #         j += 2
+                
+    #     return res
         
 def main():
     sol = Solution()
+    print(sol.minChanges("11000111"))
     print(sol.minChanges("1001"))
     print(sol.minChanges("10"))
     print(sol.minChanges("0000"))
