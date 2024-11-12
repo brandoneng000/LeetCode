f47ac0f75bc96a72cc26b867a793339044df261e
Brandon Eng <brandoneng000@gmail.com>
2024-11-12 06:16:24 -0800

    Updated 2070

--- medium/2070.py
+++ medium/2070.py
@@ -1,21 +1,42 @@
 from typing import List
 from bisect import bisect_right
+from collections import defaultdict
 
 class Solution:
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
-        items.append([0, 0])
+        items_dict = defaultdict(int)
         items.sort()
-        n = len(items)
         res = []
+        cur = 0
 
-        for i in range(1, n):
-            items[i][1] = max(items[i][1], items[i - 1][1])
+        for p, b in items:
+            cur = max(cur, b)
+            items_dict[p] = cur
+        
+        items_dict[0] = 0
+        keys = sorted(items_dict.keys())
 
         for q in queries:
-            index = bisect_right(items, q, key=lambda x: x[0])
-            res.append(items[index - 1][1])
-        
+            index = bisect_right(keys, q) - 1
+            res.append(items_dict[keys[index]])
+
         return res
+
+
+    # def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
+    #     items.append([0, 0])
+    #     items.sort()
+    #     n = len(items)
+    #     res = []
+
+    #     for i in range(1, n):
+    #         items[i][1] = max(items[i][1], items[i - 1][1])
+
+    #     for q in queries:
+    #         index = bisect_right(items, q, key=lambda x: x[0])
+    #         res.append(items[index - 1][1])
+        
+    #     return res
         
 
 
