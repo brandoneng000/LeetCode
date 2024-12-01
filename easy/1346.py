3319bd9eeb50e506d688083ab6bca94af4287863
Brandon Eng <brandoneng000@gmail.com>
2024-12-01 09:58:33 -0800

    Updated 1346

--- easy/1346.py
+++ easy/1346.py
@@ -1,16 +1,29 @@
 from typing import List
+from collections import Counter
 
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        if arr.count(0) >= 2:
-            return True
+        count = Counter(arr)
 
-        divide = [num // 2 for num in arr if num % 2 == 0 and num != 0]
+        if count[0] >= 2:
+            return True
         
-        for num in divide:
-            if num in arr:
+        for num in count:
+            if num != 0 and num * 2 in count:
                 return True
+        
         return False
+
+    # def checkIfExist(self, arr: List[int]) -> bool:
+    #     if arr.count(0) >= 2:
+    #         return True
+
+    #     divide = [num // 2 for num in arr if num % 2 == 0 and num != 0]
+        
+    #     for num in divide:
+    #         if num in arr:
+    #             return True
+    #     return False
 
 def main():
     sol = Solution()
