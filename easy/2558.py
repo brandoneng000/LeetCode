9dd94a33bfd87322dd87f5896de30edb3288e163
Brandon Eng <brandoneng000@gmail.com>
2024-12-12 00:49:34 -0800

    Updated 2558

--- easy/2558.py
+++ easy/2558.py
@@ -1,15 +1,26 @@
 from typing import List
-import heapq
+from heapq import heappush, heappop, heapify
+from math import isqrt
+# import heapq
 
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        gifts = [-gift for gift in gifts]
-        heapq.heapify(gifts)
+        heap = [-gift for gift in gifts]
+        heapify(heap)
+
         for _ in range(k):
-            temp = -heapq.heappop(gifts)
-            heapq.heappush(gifts, -int(temp ** 0.5))
+            heappush(heap, -isqrt(-heappop(heap)))
         
-        return -sum(gifts)
+        return -sum(heap)
+
+    # def pickGifts(self, gifts: List[int], k: int) -> int:
+    #     gifts = [-gift for gift in gifts]
+    #     heapq.heapify(gifts)
+    #     for _ in range(k):
+    #         temp = -heapq.heappop(gifts)
+    #         heapq.heappush(gifts, -int(temp ** 0.5))
+        
+    #     return -sum(gifts)
 
         # for _ in range(k):
         #     temp = max(gifts)
