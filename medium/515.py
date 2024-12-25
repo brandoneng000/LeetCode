106b3d3eef3875d16190ff1ed902f2a72f43ae92
Brandon Eng <brandoneng000@gmail.com>
2024-12-25 05:07:11 -0800

    Updated 515

--- medium/515.py
+++ medium/515.py
@@ -1,5 +1,5 @@
 from typing import Optional, List
-import collections
+from collections import defaultdict, deque
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -7,9 +7,25 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
+    # def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+    #     def dfs(root: TreeNode, level: int):
+    #         if not root:
+    #             return
+            
+    #         self.max_level = max(self.max_level, level)
+    #         max_val[level] = max(max_val[level], root.val)
+    #         dfs(root.left, level + 1)
+    #         dfs(root.right, level + 1)
+
+    #     max_val = defaultdict(lambda: -float('inf'))
+    #     self.max_level = -1
+    #     dfs(root, 0)
+
+    #     return [max_val[level] for level in range(self.max_level + 1)]
+
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        q = collections.deque([TreeNode(0, root)])
-        next = collections.deque([])
+        q = deque([TreeNode(0, root)])
+        next = deque([])
         res = []
         while q:
             node = q.popleft()
@@ -21,6 +37,6 @@ class Solution:
             if not q and next:
                 res.append(max([n.val for n in next]))
                 q = next
-                next = collections.deque([])
+                next = deque([])
         
         return res
