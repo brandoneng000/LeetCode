9de05095786f2d64a10ecfaea3c87fba2a32d2ea
Brandon Eng <brandoneng000@gmail.com>
2025-01-01 06:45:13 -0800

    Updated 1422

--- easy/1422.py
+++ easy/1422.py
@@ -1,24 +1,41 @@
 class Solution:
     def maxScore(self, s: str) -> int:
+        n = len(s)
         zeroes = 0
-        ones = 0
-        score = {}
+        ones = s.count('1')
+        res = 0
 
-        for index in range(len(s) - 1):
-            zeroes += 1 if s[index] == '0' else 0
-            score[index] = zeroes
+        for i in range(n - 1):
+            if s[i] == '0':
+                zeroes += 1
+            else:
+                ones -= 1
+            res = max(res, zeroes + ones)
+        
+        return res
 
-        for index in range(len(s) - 2, -1, -1):
-            ones += 1 if s[index + 1] == '1' else 0
-            score[index] += ones
 
-        return max(score.values())
+    # def maxScore(self, s: str) -> int:
+    #     zeroes = 0
+    #     ones = 0
+    #     score = {}
+
+    #     for index in range(len(s) - 1):
+    #         zeroes += 1 if s[index] == '0' else 0
+    #         score[index] = zeroes
+
+    #     for index in range(len(s) - 2, -1, -1):
+    #         ones += 1 if s[index + 1] == '1' else 0
+    #         score[index] += ones
+
+    #     return max(score.values())
 
 def main():
     sol = Solution()
+    print(sol.maxScore("00000"))
     print(sol.maxScore("011101"))
-    # print(sol.maxScore("00111"))
-    # print(sol.maxScore("1111"))
+    print(sol.maxScore("00111"))
+    print(sol.maxScore("1111"))
 
 if __name__ == '__main__':
     main()
