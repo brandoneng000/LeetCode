f37b126008f7c0cf8076510ea68917b9a7a3cee5
Brandon Eng <brandoneng000@gmail.com>
2025-01-05 03:21:49 -0800

    Updated 2381

--- medium/2381.py
+++ medium/2381.py
@@ -3,7 +3,6 @@ from typing import List
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         n = len(s)
-        s_list = [ord(letter) - ord('a') for letter in s]
         prefix = [0] * (n + 1)
 
         for start, end, direction in shifts:
@@ -14,7 +13,23 @@ class Solution:
         for i in range(1, n):
             prefix[i] += prefix[i - 1]
         
-        return ''.join([chr((s_list[i] + prefix[i]) % 26 + ord('a')) for i in range(n)])
+        return ''.join([chr(((ord(s[i]) - ord('a') + prefix[i]) % 26) + ord('a')) for i in range(n)])
+
+
+    # def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+    #     n = len(s)
+    #     s_list = [ord(letter) - ord('a') for letter in s]
+    #     prefix = [0] * (n + 1)
+
+    #     for start, end, direction in shifts:
+    #         direction = 1 if direction else -1
+    #         prefix[start] += direction
+    #         prefix[end + 1] -= direction
+
+    #     for i in range(1, n):
+    #         prefix[i] += prefix[i - 1]
+        
+    #     return ''.join([chr((s_list[i] + prefix[i]) % 26 + ord('a')) for i in range(n)])
         
 
         
