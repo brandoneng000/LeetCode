9ec7d7f7ae41e1eac56a6308568525181b20d0c8
Brandon Eng <brandoneng000@gmail.com>
2025-01-06 04:28:42 -0800

    Updated 1769

--- medium/1769.py
+++ medium/1769.py
@@ -3,13 +3,32 @@ from typing import List
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         n = len(boxes)
-        balls = [i for i in range(n) if boxes[i] == '1']
         res = [0] * n
 
+        balls_left = moves_left = 0
+        balls_right = moves_right = 0
+
         for i in range(n):
-            res[i] = sum(abs(i - j) for j in balls)
+            res[i] += moves_left
+            balls_left += int(boxes[i])
+            moves_left += balls_left
+
+            j = n - 1 - i
+            res[j] += moves_right
+            balls_right += int(boxes[j])
+            moves_right += balls_right
         
         return res
+
+    # def minOperations(self, boxes: str) -> List[int]:
+    #     n = len(boxes)
+    #     balls = [i for i in range(n) if boxes[i] == '1']
+    #     res = [0] * n
+
+    #     for i in range(n):
+    #         res[i] = sum(abs(i - j) for j in balls)
+        
+    #     return res
         
 def main():
     sol = Solution()
