261e184c6307ae4bd039e156b5aa6848f7841d18
Brandon Eng <brandoneng000@gmail.com>
2025-01-07 04:58:32 -0800

    Updated 1408

--- easy/1408.py
+++ easy/1408.py
@@ -2,19 +2,47 @@ from typing import List
 
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        words.sort(key=len)
+        sentence = ' '.join(words)
         res = []
-        
-        for index, word in enumerate(words):
-            largest = len(words) - 1
-            while index < largest:
-                if word in words[largest]:
-                    res.append(word)
-                    break
-                else:
-                    largest -= 1
 
+        for word in words:
+            if sentence.count(word) > 1:
+                res.append(word)
+        
         return res
+
+
+    # def stringMatching(self, words: List[str]) -> List[str]:
+    #     words.sort(key=len, reverse=True)
+    #     words_set = set()
+    #     res = []
+
+    #     for word in words:
+            
+    #         for large_word in words_set:
+    #             if word in large_word:
+    #                 res.append(word)
+    #                 break
+    #         else:
+    #             words_set.add(word)
+        
+    #     return res
+
+
+    # def stringMatching(self, words: List[str]) -> List[str]:
+    #     words.sort(key=len)
+    #     res = []
+        
+    #     for index, word in enumerate(words):
+    #         largest = len(words) - 1
+    #         while index < largest:
+    #             if word in words[largest]:
+    #                 res.append(word)
+    #                 break
+    #             else:
+    #                 largest -= 1
+
+    #     return res
 
 def main():
     sol = Solution()
