18cd2acb2c1a5c249066fd9451cc07e77bdfab4c
Brandon Eng <brandoneng000@gmail.com>
2025-01-16 07:55:19 -0800

    Updated 2425

--- medium/2425.py
+++ medium/2425.py
@@ -1,19 +1,29 @@
 from typing import List
+from operator import xor
+from functools import reduce
 
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
         m, n = len(nums1), len(nums2)
-        res = 0
+        
+        res = reduce(xor, nums1) if n % 2 else 0
+        res = reduce(xor, nums2, res) if m % 2 else res
 
-        if m % 2:
-            for num in nums2:
-                res ^= num
-        
-        if n % 2:
-            for num in nums1:
-                res ^= num
-        
         return res
+
+    # def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+    #     m, n = len(nums1), len(nums2)
+    #     res = 0
+
+    #     if m % 2:
+    #         for num in nums2:
+    #             res ^= num
+        
+    #     if n % 2:
+    #         for num in nums1:
+    #             res ^= num
+        
+    #     return res
         
 def main():
     sol = Solution()
