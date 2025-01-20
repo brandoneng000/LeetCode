9cc087ba7126b03e4e33ac6a9073aaf504bd50c4
Brandon Eng <brandoneng000@gmail.com>
2025-01-20 07:50:26 -0800

    Updated 2661

--- medium/2661.py
+++ medium/2661.py
@@ -4,9 +4,9 @@ from collections import Counter
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
         m, n = len(mat), len(mat[0])
-        coord = {}
-        row_vals = Counter()
-        col_vals = Counter()
+        coord = [[] for _ in range(m * n + 1)]
+        row_vals = [0] * m
+        col_vals = [0] * n
 
         for i in range(m):
             for j in range(n):
@@ -19,6 +19,49 @@ class Solution:
 
             if row_vals[r] == n or col_vals[c] == m:
                 return i
+        
+        return -1
+
+    # def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+    #     m, n = len(mat), len(mat[0])
+    #     indexes = {}
+    #     row_vals = [set() for _ in range(m)]
+    #     col_vals = [set() for _ in range(n)]
+
+    #     for i in range(m):
+    #         for j in range(n):
+    #             indexes[mat[i][j]] = [i, j]
+    #             row_vals[i].add(mat[i][j])
+    #             col_vals[j].add(mat[i][j])
+        
+    #     for i, num in enumerate(arr):
+    #         r, c = indexes[num]
+    #         row_vals[r].discard(num)
+    #         col_vals[c].discard(num)
+
+    #         if not row_vals[r] or not col_vals[c]:
+    #             return i
+        
+    #     return -1
+
+
+    # def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+    #     m, n = len(mat), len(mat[0])
+    #     coord = {}
+    #     row_vals = Counter()
+    #     col_vals = Counter()
+
+    #     for i in range(m):
+    #         for j in range(n):
+    #             coord[mat[i][j]] = [i, j]
+        
+    #     for i, num in enumerate(arr):
+    #         r, c = coord[num]
+    #         row_vals[r] += 1
+    #         col_vals[c] += 1
+
+    #         if row_vals[r] == n or col_vals[c] == m:
+    #             return i
 
 def main():
     sol = Solution()
