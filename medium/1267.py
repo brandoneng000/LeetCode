ee04c77b17e8b940577499613f8db42d373eef18
Brandon Eng <brandoneng000@gmail.com>
2025-01-23 12:12:00 -0800

    Updated 1267

--- medium/1267.py
+++ medium/1267.py
@@ -2,15 +2,39 @@ from typing import List
 
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
-        rows, cols = list(map(sum, grid)), list(map(sum, zip(*grid)))
-        res = 0
+        m, n = len(grid), len(grid[0])
+        res = set()
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(m):
+            cur = set()
+            for j in range(n):
                 if grid[i][j]:
-                    res += (rows[i] + cols[j]) > 2
+                    cur.add(i * n + j)
+            
+            if len(cur) > 1:
+                res.update(cur)
         
-        return res
+        for j in range(n):
+            cur = set()
+            for i in range(m):
+                if grid[i][j]:
+                    cur.add(i * n + j)
+            
+            if len(cur) > 1:
+                res.update(cur)
+
+        return len(res)
+
+    # def countServers(self, grid: List[List[int]]) -> int:
+    #     rows, cols = list(map(sum, grid)), list(map(sum, zip(*grid)))
+    #     res = 0
+
+    #     for i in range(len(grid)):
+    #         for j in range(len(grid[0])):
+    #             if grid[i][j]:
+    #                 res += (rows[i] + cols[j]) > 2
+        
+    #     return res
 
 def main():
     sol = Solution()
