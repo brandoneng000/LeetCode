379b0cd53982b91b0c4645ce68867070b17172f2
Brandon Eng <brandoneng000@gmail.com>
2025-02-11 07:05:50 -0800

    Updated 1910

--- medium/1910.py
+++ medium/1910.py
@@ -1,16 +1,25 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        part_list = list(part)
-        stack = []
-
-        for letter in s:
-            stack.append(letter)
-
-            if stack[-len(part):] == part_list:
-                for _ in range(len(part)):
-                    stack.pop()
+        while part in s:
+            s = s.replace(part, '', 1)
         
-        return ''.join(stack)
+        return s
+
+    # def removeOccurrences(self, s: str, part: str) -> str:
+    #     return self.removeOccurrences(s.replace(part, '', 1), part) if part in s else s
+
+    # def removeOccurrences(self, s: str, part: str) -> str:
+    #     part_list = list(part)
+    #     stack = []
+
+    #     for letter in s:
+    #         stack.append(letter)
+
+    #         if stack[-len(part):] == part_list:
+    #             for _ in range(len(part)):
+    #                 stack.pop()
+        
+    #     return ''.join(stack)
         
 def main():
     sol = Solution()
