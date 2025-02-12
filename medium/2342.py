9994eb3bb904516a232dc6758b908c1939bb1da9
Brandon Eng <brandoneng000@gmail.com>
2025-02-12 03:44:31 -0800

    Updated 2342

--- medium/2342.py
+++ medium/2342.py
@@ -6,19 +6,47 @@ class Solution:
         res = -1
 
         for num in nums:
-            temp = num
             digit_sum = 0
+            temp = num
 
             while temp:
                 digit_sum += temp % 10
                 temp //= 10
             
             if digit_sum in digits:
-                res = max(res, digits[digit_sum] + num)
-
-            digits[digit_sum] = max(digits.get(digit_sum, 0), num)
+                if digits[digit_sum][0] <= num:
+                    digits[digit_sum][1] = digits[digit_sum][0]
+                    digits[digit_sum][0] = num
+                elif digits[digit_sum][1] < num:
+                    digits[digit_sum][1] = num
+            else:
+                digits[digit_sum] = [num, -1]
+        
+        for digit_sum in digits:
+            if digits[digit_sum][1] != -1:
+                res = max(res, sum(digits[digit_sum]))
         
         return res
+
+
+    # def maximumSum(self, nums: List[int]) -> int:
+    #     digits = {}
+    #     res = -1
+
+    #     for num in nums:
+    #         temp = num
+    #         digit_sum = 0
+
+    #         while temp:
+    #             digit_sum += temp % 10
+    #             temp //= 10
+            
+    #         if digit_sum in digits:
+    #             res = max(res, digits[digit_sum] + num)
+
+    #         digits[digit_sum] = max(digits.get(digit_sum, 0), num)
+        
+    #     return res
 
         
 def main():
