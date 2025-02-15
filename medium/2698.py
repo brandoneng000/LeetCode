1ed0ff5f3b826186ec704b33bbf826cc407b102b
Brandon Eng <brandoneng000@gmail.com>
2025-02-15 05:07:01 -0800

    Updated 2698

--- medium/2698.py
+++ medium/2698.py
@@ -1,30 +1,55 @@
 class Solution:
     def punishmentNumber(self, n: int) -> int:
-        def check_punish(index: int, start: str, goal: int):
-            if index == len(start):
-                return goal == 0
-            if goal < 0:
+        def check_partition(num: int, target: int):
+            if target < 0 or num < target:
                 return False
             
-            for i in range(index, len(start)):
-                x = start[index: i + 1]
-                y = int(x)
-
-                if check_punish(i + 1, start, goal - y):
-                    return True
+            if num == target:
+                return True
             
-            return False
+            return (
+                check_partition(num // 10, target - num % 10) 
+                or check_partition(num // 100, target - num % 100)
+                or check_partition(num // 1000, target - num % 1000)
+            )
         
         res = 0
 
-        for i in range(1, n + 1):
-            x = i * i
-            num = str(x)
-            
-            if check_punish(0, num, i):
-                res += x
+        for num in range(1, n + 1):
+            square_num = num * num
+
+            if check_partition(square_num, num):
+                res += square_num
         
         return res
+
+
+    # def punishmentNumber(self, n: int) -> int:
+    #     def check_punish(index: int, start: str, goal: int):
+    #         if index == len(start):
+    #             return goal == 0
+    #         if goal < 0:
+    #             return False
+            
+    #         for i in range(index, len(start)):
+    #             x = start[index: i + 1]
+    #             y = int(x)
+
+    #             if check_punish(i + 1, start, goal - y):
+    #                 return True
+            
+    #         return False
+        
+    #     res = 0
+
+    #     for i in range(1, n + 1):
+    #         x = i * i
+    #         num = str(x)
+            
+    #         if check_punish(0, num, i):
+    #             res += x
+        
+    #     return res
 
         
 def main():
