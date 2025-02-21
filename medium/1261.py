5d861845c6aaa8945d0059c6ed563522edd328b6
Brandon Eng <brandoneng000@gmail.com>
2025-02-21 06:57:19 -0800

    Updated 1261

--- medium/1261.py
+++ medium/1261.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from collections import deque
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -7,23 +8,39 @@ class TreeNode:
         self.left = left
         self.right = right
 class FindElements:
-
     def __init__(self, root: Optional[TreeNode]):
-        def dfs(root: TreeNode, expected_val: int):
-            if not root:
-                return
-            
-            self.tree_vals.add(expected_val)
-            dfs(root.left, expected_val * 2 + 1)
-            dfs(root.right, expected_val * 2 + 2)
-
         self.tree_vals = set()
-        dfs(root, 0)
+        q = deque([[root, 0]])
+
+        while q:
+            node, val = q.popleft()
+            self.tree_vals.add(val)
+
+            if node.left:
+                q.append([node.left, val * 2 + 1])
+            
+            if node.right:
+                q.append([node.right, val * 2 + 2])
+
 
     def find(self, target: int) -> bool:
         return target in self.tree_vals
         
+# class FindElements:
+#     def __init__(self, root: Optional[TreeNode]):
+#         def dfs(root: TreeNode, expected_val: int):
+#             if not root:
+#                 return
+            
+#             self.tree_vals.add(expected_val)
+#             dfs(root.left, expected_val * 2 + 1)
+#             dfs(root.right, expected_val * 2 + 2)
 
+#         self.tree_vals = set()
+#         dfs(root, 0)
+
+#     def find(self, target: int) -> bool:
+#         return target in self.tree_vals
 
 # Your FindElements object will be instantiated and called as such:
 # obj = FindElements(root)
