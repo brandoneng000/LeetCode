e3f78dc3e8e9b26359fd5c101aacb19eeb372e45
Brandon Eng <brandoneng000@gmail.com>
2025-02-27 05:58:27 -0800

    Updated 873

--- medium/873.py
+++ medium/873.py
@@ -2,19 +2,37 @@ from typing import List
 
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
-        arr_set = set(arr)
+        n = len(arr)
         res = 0
-        for i in range(len(arr)):
-            for j in range(i + 1, len(arr)):
-                val = arr[j]
-                val2 = arr[i] + arr[j]
-                size = 2
-                while val2 in arr_set:
-                    val, val2 = val2, val + val2
-                    size += 1
-                res = max(res, size)
+        dp = [[0 for j in range(n)] for i in range(n)]
+
+        value_to_index = { num: index for index, num in enumerate(arr) }
+
+        for cur in range(n):
+            for prev in range(cur):
+                diff = arr[cur] - arr[prev]
+                prev_index = value_to_index.get(diff, -1)
+
+                dp[prev][cur] = dp[prev_index][prev] + 1 if diff < arr[prev] and prev_index >= 0 else 2
+                res = max(res, dp[prev][cur])
         
-        return res if res >= 3 else 0
+        return res if res > 2 else 0
+
+    # def lenLongestFibSubseq(self, arr: List[int]) -> int:
+    #     arr_set = set(arr)
+    #     res = 0
+    # 
+    #     for i in range(len(arr)):
+    #         for j in range(i + 1, len(arr)):
+    #             val = arr[j]
+    #             val2 = arr[i] + arr[j]
+    #             size = 2
+    #             while val2 in arr_set:
+    #                 val, val2 = val2, val + val2
+    #                 size += 1
+    #             res = max(res, size)
+        
+    #     return res if res >= 3 else 0
 
 def main():
     sol = Solution()
