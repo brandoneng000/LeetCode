90ea267a3005a5a8e709a3d82629b7045c304740
Brandon Eng <brandoneng000@gmail.com>
2025-03-01 02:52:59 -0800

    Updated 2460

--- easy/2460.py
+++ easy/2460.py
@@ -2,12 +2,42 @@ from typing import List
 
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        for index in range(len(nums) - 1):
-            if nums[index] == nums[index + 1]:
-                nums[index] *= 2
-                nums[index + 1] = 0
+        n = len(nums)
+        zero = 0
 
-        return [n for n in nums if n] + [0] * nums.count(0)
+        for i in range(n - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i] *= 2
+                nums[i + 1] = 0
+            zero += 1 if nums[i] == 0 else 0
+
+        zero += 1 if nums[-1] == 0 else 0
+        return [num for num in nums if num] + [0] * zero
+
+    # def applyOperations(self, nums: List[int]) -> List[int]:
+    #     n = len(nums)
+    #     zero = 0
+    #     res = []
+
+    #     for i in range(n):
+    #         if i + 1 < n and nums[i] == nums[i + 1]:
+    #             nums[i] *= 2
+    #             nums[i + 1] = 0
+    #         if nums[i] == 0:
+    #             zero += 1
+    #         else:
+    #             res.append(nums[i])
+
+    #     return res + [0] * zero
+
+
+    # def applyOperations(self, nums: List[int]) -> List[int]:
+    #     for index in range(len(nums) - 1):
+    #         if nums[index] == nums[index + 1]:
+    #             nums[index] *= 2
+    #             nums[index + 1] = 0
+
+    #     return [n for n in nums if n] + [0] * nums.count(0)
 
 def main():
     sol = Solution()
