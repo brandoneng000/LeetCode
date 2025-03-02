cbba6aa7690fe3cf0742d2b542930aadb2936e9f
Brandon Eng <brandoneng000@gmail.com>
2025-03-02 05:07:02 -0800

    Updated 2570

--- easy/2570.py
+++ easy/2570.py
@@ -2,39 +2,40 @@ from typing import List
 
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        res = []
-        index1, index2 = 0, 0
+        nums = {}
 
-        while index1 < len(nums1) or index2 < len(nums2):
-            if index1 == len(nums1):
-                res.extend(nums2[index2:])
-                index2 = len(nums2)
-            elif index2 == len(nums2):
-                res.extend(nums1[index1:])
-                index1 = len(nums1)
-            else:
-                if nums1[index1][0] == nums2[index2][0]:
-                    res.append([nums1[index1][0], nums1[index1][1] + nums2[index2][1]])
-                    index1 += 1
-                    index2 += 1
-                elif nums1[index1][0] < nums2[index2][0]:
-                    res.append(nums1[index1])
-                    index1 += 1
-                else:
-                    res.append(nums2[index2])
-                    index2 += 1
-
-        return res
-                    
-        # nums = {}
-
-        # for index, val in nums1:
-        #     nums[index] = val
+        for index, val in nums1:
+            nums[index] = val
         
-        # for index, val in nums2:
-        #     nums[index] = nums.get(index, 0) + val
+        for index, val in nums2:
+            nums[index] = nums.get(index, 0) + val
         
-        # return sorted(list(nums.items()))
+        return sorted(list(nums.items()))
+
+    # def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+    #     res = []
+    #     index1, index2 = 0, 0
+
+    #     while index1 < len(nums1) or index2 < len(nums2):
+    #         if index1 == len(nums1):
+    #             res.extend(nums2[index2:])
+    #             index2 = len(nums2)
+    #         elif index2 == len(nums2):
+    #             res.extend(nums1[index1:])
+    #             index1 = len(nums1)
+    #         else:
+    #             if nums1[index1][0] == nums2[index2][0]:
+    #                 res.append([nums1[index1][0], nums1[index1][1] + nums2[index2][1]])
+    #                 index1 += 1
+    #                 index2 += 1
+    #             elif nums1[index1][0] < nums2[index2][0]:
+    #                 res.append(nums1[index1])
+    #                 index1 += 1
+    #             else:
+    #                 res.append(nums2[index2])
+    #                 index2 += 1
+
+    #     return res
 
 
 def main():
