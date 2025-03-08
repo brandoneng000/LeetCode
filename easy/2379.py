8ec55bfba90bac65c89e325e174f731eb4531c47
Brandon Eng <brandoneng000@gmail.com>
2025-03-08 02:20:25 -0800

    Updated 2379

--- easy/2379.py
+++ easy/2379.py
@@ -1,11 +1,32 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
-        res = 1000
-        
-        for index in range(len(blocks) - k + 1):
-            res = min(res, blocks[index: index + k].count('W'))
+        n = len(blocks)
+        left = 0
+        num_whites = 0
+        res = float('inf')
 
+        for right in range(n):
+            if blocks[right] == 'W':
+                num_whites += 1
+            
+            if right - left + 1 == k:
+                res = min(res, num_whites)
+            
+                if blocks[left] == 'W':
+                    num_whites -= 1
+                
+                left += 1
+        
         return res
+
+
+    # def minimumRecolors(self, blocks: str, k: int) -> int:
+    #     res = 1000
+        
+    #     for index in range(len(blocks) - k + 1):
+    #         res = min(res, blocks[index: index + k].count('W'))
+
+    #     return res
 
         # return min(blocks[index: index + k].count('W') for index in range(len(blocks) - k + 1))
 
