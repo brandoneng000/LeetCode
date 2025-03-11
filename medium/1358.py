32737d2f1e8631f6f63b6b71913529aeeab41c9d
Brandon Eng <brandoneng000@gmail.com>
2025-03-11 05:27:09 -0700

    Finished 1358

--- medium/1358.py
+++ medium/1358.py
@@ -2,24 +2,36 @@ import collections
 
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        res = 0
         n = len(s)
-        count = collections.Counter()
-        left = 0
+        prev_index = [-1] * 3
+        res = 0
 
-        for right in range(n):
-            count[s[right]] += 1
-
-            if len(count) == 3:
-                while len(count) == 3:
-                    count[s[left]] -= 1
-                    if count[s[left]] == 0:
-                        count.pop(s[left])
-                    left += 1
-                
-                    res += n - right
+        for i in range(n):
+            prev_index[ord(s[i]) - ord('a')] = i
+            res += 1 + min(prev_index)
         
         return res
+
+
+    # def numberOfSubstrings(self, s: str) -> int:
+    #     res = 0
+    #     n = len(s)
+    #     count = collections.Counter()
+    #     left = 0
+
+    #     for right in range(n):
+    #         count[s[right]] += 1
+
+    #         if len(count) == 3:
+    #             while len(count) == 3:
+    #                 count[s[left]] -= 1
+    #                 if count[s[left]] == 0:
+    #                     count.pop(s[left])
+    #                 left += 1
+                
+    #                 res += n - right
+        
+    #     return res
         
 def main():
     sol = Solution()
