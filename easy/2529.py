d48e6c83b5cdb237a2a5a521d628cd1699c53643
Brandon Eng <brandoneng000@gmail.com>
2025-03-12 05:02:49 -0700

    Updated 2529

--- easy/2529.py
+++ easy/2529.py
@@ -2,30 +2,47 @@ from typing import List
 
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        if nums[0] == 0 and nums[-1] == 0: return 0
-        left, right = 0, len(nums) - 1
+        n = len(nums)
+        negative = 0
+        positive = n
 
-        while left <= right:
-            middle = (left + right) // 2
-
-            if nums[middle] == 0:
-                left = middle
-                break
-            elif nums[middle] > 0:
-                right = middle - 1
+        for i in range(n):
+            if nums[i] < 0:
+                negative += 1
+            
+            if nums[i] <= 0:
+                positive -= 1
             else:
-                left = middle + 1
+                break
         
-        positives = len(nums) - self.adjust_for_zero(nums, left, 1)
-        negatives = self.adjust_for_zero(nums, left, -1)
-        return max(positives, negatives)
+        return max(negative, positive)
+
+
+    # def maximumCount(self, nums: List[int]) -> int:
+    #     if nums[0] == 0 and nums[-1] == 0: return 0
+    #     left, right = 0, len(nums) - 1
+
+    #     while left <= right:
+    #         middle = (left + right) // 2
+
+    #         if nums[middle] == 0:
+    #             left = middle
+    #             break
+    #         elif nums[middle] > 0:
+    #             right = middle - 1
+    #         else:
+    #             left = middle + 1
         
-    def adjust_for_zero(self, nums: List[int], index: int, adjust: int):
-        while 0 <= index < len(nums) and nums[index] == 0:
-            index += adjust
-        if index == len(nums):
-            return index
-        return index if nums[index] > 0 else index + 1
+    #     positives = len(nums) - self.adjust_for_zero(nums, left, 1)
+    #     negatives = self.adjust_for_zero(nums, left, -1)
+    #     return max(positives, negatives)
+        
+    # def adjust_for_zero(self, nums: List[int], index: int, adjust: int):
+    #     while 0 <= index < len(nums) and nums[index] == 0:
+    #         index += adjust
+    #     if index == len(nums):
+    #         return index
+    #     return index if nums[index] > 0 else index + 1
 
 def main():
     sol = Solution()
