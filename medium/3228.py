fe14f01f238eb3aeed49325b6c4409b4723e48ad
Brandon Eng <brandoneng000@gmail.com>
2025-03-19 05:49:15 -0700

    Finished 3228

---
+++ medium/3228.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxOperations(self, s: str) -> int:
+        n = len(s)
+        res = 0
+        count = 0
+        i = 0
+
+        while i < n:
+            if s[i] == '1':
+                count += 1
+            else:
+                res += count
+                while i + 1 < n and s[i + 1] == '0':
+                    i += 1
+            
+            i += 1
+        
+        return res
+
+
+def main():
+    sol = Solution()
+    print(sol.maxOperations("1001101"))
+    print(sol.maxOperations("00111"))
+
+if __name__ == '__main__':
+    main()
