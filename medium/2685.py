42d2e78db3ca3115578ef7ad83dc4db0a88e6f9b
Brandon Eng <brandoneng000@gmail.com>
2025-03-22 00:52:24 -0700

    Updated 2685

--- medium/2685.py
+++ medium/2685.py
@@ -1,47 +1,82 @@
 from typing import List
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
-        def dfs(node: int, seen: set):
-            if node in seen:
-                return 0
+        def bfs(source: int, graph: defaultdict, visited: set):
+            q = deque([source])
+            visited[source] = True
+            nodes = []
+
+            while q:
+                node = q.popleft()
+                nodes.append(node)
+                
+                for nei in graph[node]:
+                    if not visited[nei]:
+                        q.append(nei)
+                        visited[nei] = True
             
-            res = 1
-            seen.add(node)
+            size = len(nodes) - 1
 
-            for nei in graph[node]:
-                res += dfs(nei, seen)
-            
-            return res
+            return all(len(graph[node]) == size for node in nodes)
 
 
-        def check(start: int):
-            size = dfs(start, set()) - 1
-            visited.add(start)
-            res = True
-
-            for nei in graph[start] + [start]:
-                visited.add(nei)
-                res = res and len(graph[nei]) == size
-            
-            return res
-
-        visited = set()
         graph = defaultdict(list)
+        visited = [False] * n
         res = 0
 
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
-        
-        for i in range(n):
-            if i in visited:
-                continue
 
-            res += check(i)
+        for node in range(n):
+            if not visited[node]:
+                if bfs(node, graph, visited):
+                    res += 1
         
         return res
+
+    # def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+    #     def dfs(node: int, seen: set):
+    #         if node in seen:
+    #             return 0
+            
+    #         res = 1
+    #         seen.add(node)
+
+    #         for nei in graph[node]:
+    #             res += dfs(nei, seen)
+            
+    #         return res
+
+
+    #     def check(start: int):
+    #         size = dfs(start, set()) - 1
+    #         visited.add(start)
+    #         res = True
+
+    #         for nei in graph[start] + [start]:
+    #             visited.add(nei)
+    #             res = res and len(graph[nei]) == size
+            
+    #         return res
+
+    #     visited = set()
+    #     graph = defaultdict(list)
+    #     res = 0
+
+    #     for u, v in edges:
+    #         graph[u].append(v)
+    #         graph[v].append(u)
+        
+    #     for i in range(n):
+    #         if i in visited:
+    #             continue
+
+    #         res += check(i)
+        
+    #     return res
         
 
 def main():
