95a8859f7d599ef72e4b537405c113b6d604d147
Brandon Eng <brandoneng000@gmail.com>
2025-04-02 07:36:43 -0700

    Updated 2873

--- easy/2873.py
+++ easy/2873.py
@@ -2,21 +2,46 @@ from typing import List
 
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        def helper(cur: List[int], index: int):
-            if len(cur) == 3:
-                self.res = max(self.res, (cur[0] - cur[1]) * cur[2])
-                return
+        res = high = diff = 0
+
+        for num in nums:
+            res = max(res, diff * num)
+            diff = max(diff, high - num)
+            high = max(high, num)
+        
+        return res
+
+    # def maximumTripletValue(self, nums: List[int]) -> int:
+    #     n = len(nums)
+    #     res = 0
+
+    #     for i in range(n - 2):
+    #         cur = 0
+
+    #         for j in range(i + 1, n - 1):
+    #             if cur < nums[i] - nums[j]:
+    #                 cur = nums[i] - nums[j]
+    #                 res = max(res, cur * max(nums[j + 1:]))
+        
+    #     return res
+                    
+
+    # def maximumTripletValue(self, nums: List[int]) -> int:
+    #     def helper(cur: List[int], index: int):
+    #         if len(cur) == 3:
+    #             self.res = max(self.res, (cur[0] - cur[1]) * cur[2])
+    #             return
             
-            for i in range(index, n):
-                cur.append(nums[i])
-                helper(cur, i + 1)
-                cur.pop()
+    #         for i in range(index, n):
+    #             cur.append(nums[i])
+    #             helper(cur, i + 1)
+    #             cur.pop()
 
-        n = len(nums)
-        self.res = 0
-        helper([], 0)
+    #     n = len(nums)
+    #     self.res = 0
+    #     helper([], 0)
 
-        return self.res
+    #     return self.res
         
 def main():
     sol = Solution()
