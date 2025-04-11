4850a8312be51264c7bc2099eb9ef5648fd8d153
Brandon Eng <brandoneng000@gmail.com>
2025-04-11 02:09:32 -0700

    Updated 2843

--- easy/2843.py
+++ easy/2843.py
@@ -3,15 +3,29 @@ class Solution:
         res = 0
 
         for num in range(low, high + 1):
-            num_str = str(num)
+            if num < 100 and num % 11 == 0:
+                res += 1
 
-            if len(num_str) % 2 == 1:
-                continue
-            first = sum(int(num_str[i]) for i in range(len(num_str) // 2))
-            second = sum(int(num_str[i]) for i in range(len(num_str) // 2, len(num_str)))
-            res += first == second
-        
+            if 1000 <= num < 10000:
+                left = num // 1000 + num % 1000 // 100
+                right = num % 100 // 10 + num % 10
+                res += (left == right)
+            
         return res
+
+    # def countSymmetricIntegers(self, low: int, high: int) -> int:
+    #     res = 0
+
+    #     for num in range(low, high + 1):
+    #         num_str = str(num)
+
+    #         if len(num_str) % 2 == 1:
+    #             continue
+    #         first = sum(int(num_str[i]) for i in range(len(num_str) // 2))
+    #         second = sum(int(num_str[i]) for i in range(len(num_str) // 2, len(num_str)))
+    #         res += first == second
+        
+    #     return res
         
 def main():
     sol = Solution()
