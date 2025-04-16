faa515657640c891afb9d6d0e34d1b186f2198f3
Brandon Eng <brandoneng000@gmail.com>
2025-04-16 02:41:40 -0700

    Updated 2537

--- medium/2537.py
+++ medium/2537.py
@@ -5,21 +5,40 @@ class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
         n = len(nums)
         res = 0
-        count = Counter()
+        count = {}
         left = 0
+        cur = 0
 
         for right in range(n):
-            k -= count[nums[right]]
-            count[nums[right]] += 1
-
-            while k <= 0:
+            count[nums[right]] = count.get(nums[right], 0) + 1
+            cur += count[nums[right]] - 1
+            
+            while cur >= k:
+                res += n - right
+                cur -= count[nums[left]] - 1
                 count[nums[left]] -= 1
-                k += count[nums[left]]
                 left += 1
-
-            res += left
         
         return res
+
+    # def countGood(self, nums: List[int], k: int) -> int:
+    #     n = len(nums)
+    #     res = 0
+    #     count = Counter()
+    #     left = 0
+
+    #     for right in range(n):
+    #         k -= count[nums[right]]
+    #         count[nums[right]] += 1
+
+    #         while k <= 0:
+    #             count[nums[left]] -= 1
+    #             k += count[nums[left]]
+    #             left += 1
+
+    #         res += left
+        
+    #     return res
 
         
 def main():
