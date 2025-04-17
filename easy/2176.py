11d7274adfd278835bb846b1e6a34a09258d3b9c
Brandon Eng <brandoneng000@gmail.com>
2025-04-17 01:52:29 -0700

    Updated 2176

--- easy/2176.py
+++ easy/2176.py
@@ -3,19 +3,30 @@ import collections
 
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
-        pairs = collections.defaultdict(list)
+        n = len(nums)
         res = 0
 
-        for index, num in enumerate(nums):
-            pairs[num].append(index)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] == nums[j] and (i * j % k) == 0:
+                    res += 1
         
-        for pair in pairs.values():
-            for i in range(len(pair) - 1, -1, -1):
-                for j in range(i - 1, -1, -1):
-                    if not (pair[i] * pair[j]) % k:
-                        res += 1
+        return res
 
-        return res                
+    # def countPairs(self, nums: List[int], k: int) -> int:
+    #     pairs = collections.defaultdict(list)
+    #     res = 0
+
+    #     for index, num in enumerate(nums):
+    #         pairs[num].append(index)
+        
+    #     for pair in pairs.values():
+    #         for i in range(len(pair) - 1, -1, -1):
+    #             for j in range(i - 1, -1, -1):
+    #                 if not (pair[i] * pair[j]) % k:
+    #                     res += 1
+
+    #     return res                
 
 
 
