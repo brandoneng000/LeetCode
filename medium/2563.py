ce6c8082dd1e5c0742ee422f555f7182564de9c4
Brandon Eng <brandoneng000@gmail.com>
2025-04-19 02:43:14 -0700

    Updated 2563

--- medium/2563.py
+++ medium/2563.py
@@ -3,19 +3,40 @@ from bisect import bisect_left, bisect_right
 
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
-        n = len(nums)
+        def lower_bound(nums: List[int], val: int):
+            left = 0
+            right = len(nums) - 1
+            res = 0
+
+            while left < right:
+                total = nums[left] + nums[right]
+
+                if total < val:
+                    res += right - left
+                    left += 1
+                else:
+                    right -= 1
+
+            return res
+
         nums.sort()
-        res = 0
-
-        for i in range(n):
-            lower_bound = lower - nums[i]
-            upper_bound = upper - nums[i]
-            lower_index = bisect_left(nums, lower_bound, i + 1)
-            upper_index = bisect_right(nums, upper_bound, i + 1)
-
-            res += upper_index - lower_index
+        return lower_bound(nums, upper + 1) - lower_bound(nums, lower)    
         
-        return res
+
+    # def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+    #     n = len(nums)
+    #     nums.sort()
+    #     res = 0
+
+    #     for i in range(n):
+    #         lower_bound = lower - nums[i]
+    #         upper_bound = upper - nums[i]
+    #         lower_index = bisect_left(nums, lower_bound, i + 1)
+    #         upper_index = bisect_right(nums, upper_bound, i + 1)
+
+    #         res += upper_index - lower_index
+        
+    #     return res
         
 def main():
     sol = Solution()
