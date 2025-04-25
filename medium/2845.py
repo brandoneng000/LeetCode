f1d74c0069d25c173dbeb8d3415196567362d877
Brandon Eng <brandoneng000@gmail.com>
2025-04-25 02:33:24 -0700

    Updated 2845

--- medium/2845.py
+++ medium/2845.py
@@ -4,19 +4,33 @@ from collections import Counter
 class Solution:
     def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
         n = len(nums)
-        prefix = [0] + [nums[i] % modulo == k for i in range(n)]
-        count = Counter()
+        count = Counter([0])
         res = 0
+        prefix = 0
 
-        for i in range(1, n + 1):
-            prefix[i] = (prefix[i] + prefix[i - 1]) % modulo
-        
-        for i in range(n + 1):
-            j = prefix[i] - k
-            res += count[j % modulo]
-            count[prefix[i] % modulo] += 1
+        for i in range(n):
+            prefix += 1 if nums[i] % modulo == k else 0
+            res += count[(prefix - k + modulo) % modulo]
+            count[prefix % modulo] += 1
         
         return res
+
+
+    # def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
+    #     n = len(nums)
+    #     prefix = [0] + [nums[i] % modulo == k for i in range(n)]
+    #     count = Counter()
+    #     res = 0
+
+    #     for i in range(1, n + 1):
+    #         prefix[i] = (prefix[i] + prefix[i - 1]) % modulo
+        
+    #     for i in range(n + 1):
+    #         j = prefix[i] - k
+    #         res += count[j % modulo]
+    #         count[prefix[i] % modulo] += 1
+        
+    #     return res
         
 def main():
     sol = Solution()
