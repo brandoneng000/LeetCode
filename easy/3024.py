4cc7523b310843db44193df7535d9e4640c510a7
Brandon Eng <brandoneng000@gmail.com>
2025-05-19 02:47:05 -0700

    Updated 3024

--- easy/3024.py
+++ easy/3024.py
@@ -6,11 +6,23 @@ class Solution:
         if nums[0] + nums[1] <= nums[2]:
             return "none"
 
-        if nums[0] == nums[1] == nums[2]:
+        if nums[0] == nums[2]:
             return "equilateral"
-        elif nums[0] == nums[1] or nums[0] == nums[2] or nums[1] == nums[2]:
+        elif nums[0] == nums[1] or nums[1] == nums[2]:
             return "isosceles"
+        
         return "scalene"
+    
+    # def triangleType(self, nums: List[int]) -> str:
+    #     nums.sort()
+    #     if nums[0] + nums[1] <= nums[2]:
+    #         return "none"
+
+    #     if nums[0] == nums[1] == nums[2]:
+    #         return "equilateral"
+    #     elif nums[0] == nums[1] or nums[0] == nums[2] or nums[1] == nums[2]:
+    #         return "isosceles"
+    #     return "scalene"
         
 def main():
     sol = Solution()
