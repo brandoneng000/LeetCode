9f0d7bc9892477179ec97f4cf1175211f4176470
Brandon Eng <brandoneng000@gmail.com>
2025-05-29 03:10:23 -0700

    Finished 3373

---
+++ hard/3373.py
@@ -0,0 +1,50 @@
+from typing import List
+
+class Solution:
+    def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
+        def dfs(node, parent, depth, children, color):
+            res = 1 - depth % 2
+            color[node] = depth % 2
+
+            for child in children[node]:
+                if child == parent:
+                    continue
+
+                res += dfs(child, node, depth + 1, children, color)
+            
+            return res
+
+        def build(edges, color):
+            n = len(edges) + 1
+            children = [[] for _ in range(n)]
+
+            for u, v in edges:
+                children[u].append(v)
+                children[v].append(u)
+            
+            res = dfs(0, -1, 0, children, color)
+            return [res, n - res]
+
+
+        n = len(edges1) + 1
+        m = len(edges2) + 1
+        color1 = [0] * n
+        color2 = [0] * m
+        count1 = build(edges1, color1)
+        count2 = build(edges2, color2)
+        res = [0] * n
+
+        for i in range(n):
+            res[i] = count1[color1[i]] + max(count2[0], count2[1])
+        
+        return res
+
+        
+def main():
+    sol = Solution()
+    print(sol.maxTargetNodes(edges1 = [[0,1]], edges2 = [[0,1]]))
+    print(sol.maxTargetNodes(edges1 = [[0,1],[0,2],[2,3],[2,4]], edges2 = [[0,1],[0,2],[0,3],[2,7],[1,4],[4,5],[4,6]]))
+    print(sol.maxTargetNodes(edges1 = [[0,1],[0,2],[0,3],[0,4]], edges2 = [[0,1],[1,2],[2,3]]))
+
+if __name__ == '__main__':
+    main()
