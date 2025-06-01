c420373676ada400864c0a9d69c9104724ebc58f
Brandon Eng <brandoneng000@gmail.com>
2025-06-01 02:37:10 -0700

    Updated 2929

--- medium/2929.py
+++ medium/2929.py
@@ -1,18 +1,28 @@
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        if n > limit * 3:
-            return 0
-
-        res = 0
-
-        for i in range(limit + 1):
-            if n - i > limit * 2:
-                continue
+        def calc(x: int):
+            if x < 0:
+                return 0
             
-            j = n - i
-            res += max(min(j, limit) - max(j - limit, 0) + 1, 0)
+            return x * (x - 1) // 2
         
-        return res
+        return calc(n + 2) - 3 * calc(n - limit + 1) + 3 * calc(n - (limit + 1) * 2 + 2) - calc(n - 3 * (limit + 1) + 2)
+
+
+    # def distributeCandies(self, n: int, limit: int) -> int:
+    #     if n > limit * 3:
+    #         return 0
+
+    #     res = 0
+
+    #     for i in range(limit + 1):
+    #         if n - i > limit * 2:
+    #             continue
+            
+    #         j = n - i
+    #         res += max(min(j, limit) - max(j - limit, 0) + 1, 0)
+        
+    #     return res
         
 def main():
     sol = Solution()
