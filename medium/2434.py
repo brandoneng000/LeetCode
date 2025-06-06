392879d9e15730af7aba485fa863c33f774c0b2f
Brandon Eng <brandoneng000@gmail.com>
2025-06-06 02:05:03 -0700

    Updated 2434

--- medium/2434.py
+++ medium/2434.py
@@ -3,21 +3,40 @@ from collections import deque, Counter
 class Solution:
     def robotWithString(self, s: str) -> str:
         count = Counter(s)
-        res = []
         stack = []
+        res = []
+        min_char = 'a'
 
         for c in s:
             stack.append(c)
+            count[c] -= 1
 
-            if count[c] == 1:
-                count.pop(c)
-            else:
-                count[c] -= 1
-
-            while count and stack and min(count) >= stack[-1]:
+            while min_char != 'z' and count[min_char] == 0:
+                min_char = chr(ord(min_char) + 1)
+            
+            while stack and stack[-1] <= min_char:
                 res.append(stack.pop())
+            
+        return ''.join(res)
+
+
+    # def robotWithString(self, s: str) -> str:
+    #     count = Counter(s)
+    #     res = []
+    #     stack = []
+
+    #     for c in s:
+    #         stack.append(c)
+
+    #         if count[c] == 1:
+    #             count.pop(c)
+    #         else:
+    #             count[c] -= 1
+
+    #         while count and stack and min(count) >= stack[-1]:
+    #             res.append(stack.pop())
         
-        return ''.join(res + stack[::-1])
+    #     return ''.join(res + stack[::-1])
         
     # def robotWithString(self, s: str) -> str:
     #     s_queue = deque(list(s))
