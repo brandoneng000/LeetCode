544ba06e63748ec2f98e188998927080f1a47689
Brandon Eng <brandoneng000@gmail.com>
2025-06-07 04:57:34 -0700

    Updated 3170

--- medium/3170.py
+++ medium/3170.py
@@ -4,21 +4,35 @@ from collections import defaultdict
 
 class Solution:
     def clearStars(self, s: str) -> str:
-        indexes = defaultdict(list)
         res = list(s)
+        heap = []
 
-        for i, letter in enumerate(s):
-            if letter == '*':
+        for i, c in enumerate(s):
+            if c == '*':
                 res[i] = ''
-                cur = min(indexes)
-                res[indexes[cur].pop()] = ''
-                
-                if not indexes[cur]:
-                    indexes.pop(cur)
+                char, index = heappop(heap)
+                res[-index] = ''
             else:
-                indexes[letter].append(i)
+                heappush(heap, (c, -i))
         
         return ''.join(res)
+
+    # def clearStars(self, s: str) -> str:
+    #     indexes = defaultdict(list)
+    #     res = list(s)
+
+    #     for i, letter in enumerate(s):
+    #         if letter == '*':
+    #             res[i] = ''
+    #             cur = min(indexes)
+    #             res[indexes[cur].pop()] = ''
+                
+    #             if not indexes[cur]:
+    #                 indexes.pop(cur)
+    #         else:
+    #             indexes[letter].append(i)
+        
+    #     return ''.join(res)
 
 
 def main():
