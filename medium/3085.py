7d1ee6c8fbc6242ceda0266a542c12ecbb66e643
Brandon Eng <brandoneng000@gmail.com>
2025-06-21 11:05:23 -0700

    Updated 3085

--- medium/3085.py
+++ medium/3085.py
@@ -5,18 +5,35 @@ class Solution:
         letter_count = Counter(word)
         res = float('inf')
 
-        for a in letter_count:
+        for a in letter_count.values():
             cur = 0
 
-            for b in letter_count:
-                if letter_count[a] > letter_count[b]:
-                    cur += letter_count[b]
-                else:
-                    cur += max(0, letter_count[b] - letter_count[a] - k)
+            for b in letter_count.values():
+                if a > b:
+                    cur += b
+                elif b > a + k:
+                    cur += b - a - k
             
             res = min(res, cur)
         
         return res
+    
+    # def minimumDeletions(self, word: str, k: int) -> int:
+    #     letter_count = Counter(word)
+    #     res = float('inf')
+
+    #     for a in letter_count:
+    #         cur = 0
+
+    #         for b in letter_count:
+    #             if letter_count[a] > letter_count[b]:
+    #                 cur += letter_count[b]
+    #             else:
+    #                 cur += max(0, letter_count[b] - letter_count[a] - k)
+            
+    #         res = min(res, cur)
+        
+    #     return res
 
         
 def main():
