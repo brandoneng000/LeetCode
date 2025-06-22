6481dfd565c5ea9d44567db863ebc467cf92277e
Brandon Eng <brandoneng000@gmail.com>
2025-06-22 02:42:33 -0700

    Updated 2138

--- easy/2138.py
+++ easy/2138.py
@@ -2,10 +2,15 @@ from typing import List
 
 class Solution:
     def divideString(self, s: str, k: int, fill: str) -> List[str]:
-        result = [s[i:i + k] for i in range(0, len(s), k)]
-        if len(result[-1]) != k:
-            result[-1] += fill * (k - len(result[-1]))
-        return result
+        s += fill * (k - len(s) % k if len(s) % k else 0)
+        res = [s[i:i + k] for i in range(0, len(s), k)]
+        return res
+    
+    # def divideString(self, s: str, k: int, fill: str) -> List[str]:
+    #     result = [s[i:i + k] for i in range(0, len(s), k)]
+    #     if len(result[-1]) != k:
+    #         result[-1] += fill * (k - len(result[-1]))
+    #     return result
     
 def main():
     sol = Solution()
