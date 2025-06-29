36c6a37bcdf1fbc2317431e2cbbc12a4a0103b44
Brandon Eng <brandoneng000@gmail.com>
2025-06-29 04:15:44 -0700

    Updated 1498

--- medium/1498.py
+++ medium/1498.py
@@ -1,20 +1,41 @@
 from typing import List
+from bisect import bisect_right
 
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
         mod = 10 ** 9 + 7
-        left, right = 0, len(nums) - 1
+        n = len(nums)
         res = 0
-
         nums.sort()
-        while left <= right:
-            if nums[left] + nums[right] <= target:
-                res = (res + 2 ** (right - left)) % mod
-                left += 1
-            else:
-                right -= 1
+
+        power = [1] * n
+        for i in range(1, n):
+            power[i] = (power[i - 1] * 2) % mod
         
+        for left in range(n):
+            remaining = target - nums[left]
+            right = bisect_right(nums, remaining) - 1
+
+            if left <= right:
+                res = (res + power[right - left]) % mod
+            
         return res
+
+
+    # def numSubseq(self, nums: List[int], target: int) -> int:
+    #     mod = 10 ** 9 + 7
+    #     left, right = 0, len(nums) - 1
+    #     res = 0
+
+    #     nums.sort()
+    #     while left <= right:
+    #         if nums[left] + nums[right] <= target:
+    #             res = (res + 2 ** (right - left)) % mod
+    #             left += 1
+    #         else:
+    #             right -= 1
+        
+    #     return res
 
 
 def main():
