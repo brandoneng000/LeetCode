132266ca18ea1895a86c0c80bec12469e05cd5fb
Brandon Eng <brandoneng000@gmail.com>
2025-06-30 04:06:57 -0700

    Updated 594

--- easy/594.py
+++ easy/594.py
@@ -1,17 +1,28 @@
 from typing import List
+from collections import Counter
 
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        from collections import Counter
-        result = 0
-        num_count = Counter(nums)
-        num_ordered = sorted(num_count.keys())
-        print(num_ordered)
-        for index in range(len(num_ordered) - 1):
-            if num_ordered[index + 1] - num_ordered[index] == 1:
-                result = max(result, num_count[num_ordered[index]] + num_count[num_ordered[index] + 1])
+        count = Counter(nums)
+        res = 0
 
-        return result
+        for num in count:
+            if num + 1 in count:
+                res = max(res, count[num] + count[num + 1])
+        
+        return res
+
+    # def findLHS(self, nums: List[int]) -> int:
+    #     from collections import Counter
+    #     result = 0
+    #     num_count = Counter(nums)
+    #     num_ordered = sorted(num_count.keys())
+
+    #     for index in range(len(num_ordered) - 1):
+    #         if num_ordered[index + 1] - num_ordered[index] == 1:
+    #             result = max(result, num_count[num_ordered[index]] + num_count[num_ordered[index] + 1])
+
+    #     return result
 
 
 
