9f6ba56b301613b69227167ec3815185a629509e
Brandon Eng <brandoneng000@gmail.com>
2025-07-17 06:02:43 -0700

    Updated 3202

--- medium/3202.py
+++ medium/3202.py
@@ -3,17 +3,39 @@ from typing import List
 class Solution:
     def maximumLength(self, nums: List[int], k: int) -> int:
         dp = [[0 for j in range(k)] for i in range(k)]
-        res = 1
 
         for num in nums:
-            cur = num % k
-
-            for i in range(k):
-                prev = (i - cur + k) % k
-                dp[cur][i] = max(dp[cur][i], dp[prev][i] + 1)
-                res = max(res, dp[cur][i])
+            for prev in range(k):
+                dp[num % k][prev] = dp[prev][num % k] + 1
         
-        return res
+        return max(map(max, dp))
+    
+    # def maximumLength(self, nums: List[int], k: int) -> int:
+    #     dp = [[0 for j in range(k)] for i in range(k)]
+    #     res = 0
+
+    #     for num in nums:
+    #         num %= k
+
+    #         for prev in range(k):
+    #             dp[prev][num] = dp[num][prev] + 1
+    #             res = max(res, dp[prev][num])
+        
+    #     return res
+
+    # def maximumLength(self, nums: List[int], k: int) -> int:
+    #     dp = [[0 for j in range(k)] for i in range(k)]
+    #     res = 1
+
+    #     for num in nums:
+    #         cur = num % k
+
+    #         for i in range(k):
+    #             prev = (i - cur + k) % k
+    #             dp[cur][i] = max(dp[cur][i], dp[prev][i] + 1)
+    #             res = max(res, dp[cur][i])
+        
+    #     return res
         
 def main():
     sol = Solution()
