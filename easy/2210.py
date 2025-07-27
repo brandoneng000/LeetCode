ff83c0cb9d0240e0afbd69f7d9f40c88f4a54fed
Brandon Eng <brandoneng000@gmail.com>
2025-07-27 02:32:53 -0700

    Updated 2210

--- easy/2210.py
+++ easy/2210.py
@@ -2,25 +2,41 @@ from typing import List
 
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
+        n = len(nums)
         res = 0
-        up = False
-        down = False
+        left = 0
 
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
-                pass
-            elif nums[i] < nums[i + 1]:
-                up = True
-                if down:
+        for right in range(1, n - 1):
+            if nums[right] != nums[right + 1]:
+                if (
+                    (nums[right] > nums[left] and nums[right] > nums[right + 1])
+                    or (nums[right] < nums[left] and nums[right] < nums[right + 1])
+                ):
                     res += 1
-                down = False
-            elif nums[i] > nums[i + 1]:
-                down = True
-                if up:
-                    res += 1
-                up = False
-        
+                left = right
+            
         return res
+
+    # def countHillValley(self, nums: List[int]) -> int:
+    #     res = 0
+    #     up = False
+    #     down = False
+
+    #     for i in range(len(nums) - 1):
+    #         if nums[i] == nums[i + 1]:
+    #             pass
+    #         elif nums[i] < nums[i + 1]:
+    #             up = True
+    #             if down:
+    #                 res += 1
+    #             down = False
+    #         elif nums[i] > nums[i + 1]:
+    #             down = True
+    #             if up:
+    #                 res += 1
+    #             up = False
+        
+    #     return res
 
 def main():
     sol = Solution()
