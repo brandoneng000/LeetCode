b62851c1b8908ae669b4d44d3cf67815bbdfc113
Brandon Eng <brandoneng000@gmail.com>
2025-07-31 02:09:07 -0700

    Updated 898

--- medium/898.py
+++ medium/898.py
@@ -2,18 +2,28 @@ from typing import List
 
 class Solution:
     def subarrayBitwiseORs(self, arr: List[int]) -> int:
-        res = set(arr)
-        prev = set()
-        prev.add(arr[0])
-        for n in arr[1:]:
-            temp = set()
-            for p in prev:
-                temp.add(n | p)
-                res.add(n | p)
-            prev = temp
-            prev.add(n)
+        res = set()
+        cur = set()
+
+        for x in arr:
+            cur = {x | y for y in cur} | {x}
+            res |= cur
         
         return len(res)
+
+    # def subarrayBitwiseORs(self, arr: List[int]) -> int:
+    #     res = set(arr)
+    #     prev = set()
+    #     prev.add(arr[0])
+    #     for n in arr[1:]:
+    #         temp = set()
+    #         for p in prev:
+    #             temp.add(n | p)
+    #             res.add(n | p)
+    #         prev = temp
+    #         prev.add(n)
+        
+    #     return len(res)
             
 def main():
     sol = Solution()
