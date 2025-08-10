fffd4c5fd2cad9de937089823752cd141c0ab9a1
Brandon Eng <brandoneng000@gmail.com>
2025-08-10 07:37:26 -0700

    Updated 869

--- medium/869.py
+++ medium/869.py
@@ -2,18 +2,38 @@ import collections
 
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        powers = ['1']
-        val = 1
-        size = len(str(n))
-        for i in range(32):
-            val *= 2
-            if len(str(val)) == size:
-                powers.append(str(val))
-            elif len(str(val)) > size:
-                break
+        def count_digits(num: int):
+            return ''.join(sorted(str(num)))
         
-        n_counter = collections.Counter(str(n))
-        return any(collections.Counter(p) == n_counter for p in powers)
+        target = count_digits(n)
+
+        for i in range(31):
+            if count_digits(1 << i) == target:
+                return True
+
+        return False
+    
+    # def reorderedPowerOf2(self, n: int) -> bool:
+    #     def count_digits(num: int):
+    #         return ''.join(sorted(str(num)))
+        
+    #     target = count_digits(n)
+
+    #     return any(count_digits(1 << i) == target for i in range(31))
+
+    # def reorderedPowerOf2(self, n: int) -> bool:
+    #     powers = ['1']
+    #     val = 1
+    #     size = len(str(n))
+    #     for i in range(32):
+    #         val *= 2
+    #         if len(str(val)) == size:
+    #             powers.append(str(val))
+    #         elif len(str(val)) > size:
+    #             break
+        
+    #     n_counter = collections.Counter(str(n))
+    #     return any(collections.Counter(p) == n_counter for p in powers)
 
 def main():
     sol = Solution()
