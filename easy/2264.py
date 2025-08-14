914ce2e1d236fe980cc6e241b739fc8432298689
Brandon Eng <brandoneng000@gmail.com>
2025-08-14 02:27:06 -0700

    Updated 2264

--- easy/2264.py
+++ easy/2264.py
@@ -1,13 +1,24 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
+        n = len(num)
         res = ""
 
-        for index in range(len(num) - 2):
-            triplet = num[index: index + 3]
-            if len(set(triplet)) == 1:
-                res = max(res, triplet)
-
+        for i in range(1, n - 1):
+            if num[i - 1] == num[i] == num[i + 1]:
+                res = max(res, num[i] * 3)
+        
         return res
+
+
+    # def largestGoodInteger(self, num: str) -> str:
+    #     res = ""
+
+    #     for index in range(len(num) - 2):
+    #         triplet = num[index: index + 3]
+    #         if len(set(triplet)) == 1:
+    #             res = max(res, triplet)
+
+    #     return res
 
 def main():
     sol = Solution()
