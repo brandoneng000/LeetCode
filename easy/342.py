3f13a191c23f4850d9f78c4253ad4aec277e9a44
Brandon Eng <brandoneng000@gmail.com>
2025-08-15 05:26:09 -0700

    Updated 342

--- easy/342.py
+++ easy/342.py
@@ -1,9 +1,12 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        # 01010101010101010101010101010101
-        check = n & 0b01010101010101010101010101010101 == n
-        only_one = n & n - 1 == 0
-        return n > 0 and check and only_one
+        return n > 0 and n == (n & -n) and (n - 1) % 3 == 0
+
+    # def isPowerOfFour(self, n: int) -> bool:
+    #     # 01010101010101010101010101010101
+    #     check = n & 0b01010101010101010101010101010101 == n
+    #     only_one = n & n - 1 == 0
+    #     return n > 0 and check and only_one
 
 def main():
     sol = Solution()
