a44d9a1406dbb1ca6c6e86cbe5ec838338c9085b
Brandon Eng <brandoneng000@gmail.com>
2025-08-22 01:52:18 -0700

    Updated 3195

--- medium/3195.py
+++ medium/3195.py
@@ -3,28 +3,43 @@ from typing import List
 class Solution:
     def minimumArea(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        left = top = float('inf')
-        right = bottom = 0
+        min_i, max_i = m, 0
+        min_j, max_j = n, 0
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j]:
-                    if i < top:
-                        top = i
-                    
-                    if j < left:
-                        left = j
+                if grid[i][j] == 1:
+                    min_i = min(min_i, i)
+                    max_i = max(max_i, i)
+                    min_j = min(min_j, j)
+                    max_j = max(max_j, j)
+        
+        return (max_i - min_i + 1) * (max_j - min_j + 1)
 
-                    if j > right:
-                        right = j
+    # def minimumArea(self, grid: List[List[int]]) -> int:
+    #     m, n = len(grid), len(grid[0])
+    #     left = top = float('inf')
+    #     right = bottom = 0
+
+    #     for i in range(m):
+    #         for j in range(n):
+    #             if grid[i][j]:
+    #                 if i < top:
+    #                     top = i
+                    
+    #                 if j < left:
+    #                     left = j
+
+    #                 if j > right:
+    #                     right = j
                         
-                    bottom = i
+    #                 bottom = i
                     
         
-        width = right - left + 1
-        height = bottom - top + 1
+    #     width = right - left + 1
+    #     height = bottom - top + 1
 
-        return width * height
+    #     return width * height
         
         
 def main():
