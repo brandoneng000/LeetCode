c287bf2e95f53747a6408e322a53efc57a0ee848
Brandon Eng <brandoneng000@gmail.com>
2025-08-25 02:49:19 -0700

    Updated 498

--- medium/498.py
+++ medium/498.py
@@ -5,28 +5,58 @@ class Solution:
         if not mat or not mat[0]:
             return []
         
-        row, col = 0, 0
-        direction = 1
+        m, n = len(mat), len(mat[0])
         res = []
-        while row < len(mat) and col < len(mat[0]):
-            res.append(mat[row][col])
-            next_row = row + (-1 if direction == 1 else 1)
-            next_col = col + (1 if direction == 1 else -1)
+        row = col = 0
 
-            if next_row < 0 or next_row == len(mat) or next_col < 0 or next_col == len(mat[0]):
-                if direction:
-                    row += (col == len(mat[0]) - 1)
-                    col += (col < len(mat[0]) - 1)
+        for _ in range(m * n):
+            res.append(mat[row][col])
+
+            if (row + col) % 2 == 0:
+                if col == n - 1:
+                    row += 1
+                elif row == 0:
+                    col += 1
                 else:
-                    col += (row == len(mat) - 1)
-                    row += (row < len(mat) - 1)
-                
-                direction = 1 - direction
+                    row -= 1
+                    col += 1
             else:
-                row = next_row
-                col = next_col
+                if row == m - 1:
+                    col += 1
+                elif col == 0:
+                    row += 1
+                else:
+                    row += 1
+                    col -= 1
         
         return res
+
+    # def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+    #     if not mat or not mat[0]:
+    #         return []
+        
+    #     row, col = 0, 0
+    #     direction = 1
+    #     res = []
+    #     while row < len(mat) and col < len(mat[0]):
+    #         res.append(mat[row][col])
+    #         next_row = row + (-1 if direction == 1 else 1)
+    #         next_col = col + (1 if direction == 1 else -1)
+
+    #         if next_row < 0 or next_row == len(mat) or next_col < 0 or next_col == len(mat[0]):
+    #             if direction:
+    #                 row += (col == len(mat[0]) - 1)
+    #                 col += (col < len(mat[0]) - 1)
+    #             else:
+    #                 col += (row == len(mat) - 1)
+    #                 row += (row < len(mat) - 1)
+                
+    #             direction = 1 - direction
+    #         else:
+    #             row = next_row
+    #             col = next_col
+        
+    #     return res
 
 def main():
     sol = Solution()
