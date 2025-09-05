c90e2163fce33c4d327a1759afde680d444f5d3f
Brandon Eng <brandoneng000@gmail.com>
2025-09-05 01:10:15 -0700

    Updated 2749

--- medium/2749.py
+++ medium/2749.py
@@ -2,17 +2,26 @@ from math import gcd
 
 class Solution:
     def makeTheIntegerZero(self, num1: int, num2: int) -> int:
-        if num1 < num2:
-            return -1
-        
-        for steps in range(61):
-            diff = num1 - num2 * steps
-            bits = bin(diff).count('1')
+        for step in range(61):
+            target = num1 - num2 * step
 
-            if bits <= steps and steps <= diff:
-                return steps
+            if target >= 0 and target.bit_count() <= step <= target:
+                return step
         
         return -1
+
+    # def makeTheIntegerZero(self, num1: int, num2: int) -> int:
+    #     if num1 < num2:
+    #         return -1
+        
+    #     for steps in range(61):
+    #         diff = num1 - num2 * steps
+    #         bits = bin(diff).count('1')
+
+    #         if bits <= steps and steps <= diff:
+    #             return steps
+        
+    #     return -1
 
         
 def main():
