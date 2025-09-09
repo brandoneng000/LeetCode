e4b69e62d31e84b55da338efb1323312a1deee9c
Brandon Eng <brandoneng000@gmail.com>
2025-09-09 02:00:48 -0700

    Updated 2327

--- medium/2327.py
+++ medium/2327.py
@@ -3,20 +3,39 @@ from collections import deque
 class Solution:
     def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
         mod = 1000000007
-        delay_q = deque([0] * delay)
-        forget_q = deque([0] * forget)
+        dp = [0] * (n + 1)
+        dp[1] = 1
         share = 0
-        delay_q[-1] = 1
-        forget_q[-1] = 1
 
-        for _ in range(1, n):
-            d = delay_q.popleft()
-            f = forget_q.popleft()
-            share = (share + d - f) % mod 
-            delay_q.append(share)
-            forget_q.append(share)
+        for time in range(2, n + 1):
+            if time - delay > 0:
+                share = (share + dp[time - delay]) % mod
+            if time - forget > 0:
+                share = (share - dp[time - forget]) % mod
+            dp[time] = share
+        
+        res = 0
+        for i in range(n - forget + 1, n + 1):
+            res = (res + dp[i]) % mod
+        
+        return res
 
-        return (share + sum(delay_q)) % mod
+    # def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+    #     mod = 1000000007
+    #     delay_q = deque([0] * delay)
+    #     forget_q = deque([0] * forget)
+    #     share = 0
+    #     delay_q[-1] = 1
+    #     forget_q[-1] = 1
+
+    #     for _ in range(1, n):
+    #         d = delay_q.popleft()
+    #         f = forget_q.popleft()
+    #         share = (share + d - f) % mod 
+    #         delay_q.append(share)
+    #         forget_q.append(share)
+
+    #     return (share + sum(delay_q)) % mod
         
 def main():
     sol = Solution()
