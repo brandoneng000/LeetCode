37ca4e9d01e79dcc7284c8737211dc0b163ebef9
Brandon Eng <brandoneng000@gmail.com>
2025-09-11 01:56:40 -0700

    Updated 2785

--- medium/2785.py
+++ medium/2785.py
@@ -1,4 +1,29 @@
+from collections import Counter
+
 class Solution:
+    # def sortVowels(self, s: str) -> str:
+    #     sorted_vowels = "AEIOUaeiou"
+    #     count_vowels = Counter()
+    #     res = []
+
+    #     for letter in s:
+    #         if letter in sorted_vowels:
+    #             count_vowels[letter] += 1
+        
+    #     j = 0
+
+    #     for letter in s:
+    #         if letter not in sorted_vowels:
+    #             res.append(letter)
+    #         else:
+    #             while j < len(sorted_vowels) and count_vowels[sorted_vowels[j]] == 0:
+    #                 j += 1
+
+    #             res.append(sorted_vowels[j])
+    #             count_vowels[sorted_vowels[j]] -= 1
+        
+    #     return ''.join(res)
+
     def sortVowels(self, s: str) -> str:
         res = list(s)
         vowels = []
