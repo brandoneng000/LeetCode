aa6fc0319883b53bf543553c7d28919c4e0f3835
Brandon Eng <brandoneng000@gmail.com>
2025-09-12 01:55:11 -0700

    Updated 3227

--- medium/3227.py
+++ medium/3227.py
@@ -2,13 +2,20 @@ from typing import List
 
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
-        vowels = set("aeiou")
-        vowel_count = 0
+        str_set = set(s)
+        return any(vowel in str_set for vowel in "aeiou")
 
-        for letter in s:
-            vowel_count += letter in vowels
+    # def doesAliceWin(self, s: str) -> bool:
+    #     return sum(letter in "aeiou" for letter in s) != 0
+
+    # def doesAliceWin(self, s: str) -> bool:
+    #     vowels = set("aeiou")
+    #     vowel_count = 0
+
+    #     for letter in s:
+    #         vowel_count += letter in vowels
         
-        return vowel_count != 0
+    #     return vowel_count != 0
         
         
 def main():
