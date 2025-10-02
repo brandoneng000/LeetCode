e2b2ef05fb0a5999849cc443a454d39b24c27974
Brandon Eng <brandoneng000@gmail.com>
2025-10-02 03:59:49 -0700

    Updated 3100

--- medium/3100.py
+++ medium/3100.py
@@ -1,19 +1,31 @@
+from math import sqrt, ceil
+
 class Solution:
     def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
-        res = 0
-        empty = 0
+        a = 1
+        b = 2 * numExchange - 3
+        c = -2 * numBottles
 
-        while numBottles:
-            res += numBottles
-            empty += numBottles
-            numBottles = 0
+        delta = b * b - 4 * a * c
+        t = ceil((-b + sqrt(delta)) / (2 * a))
+        
+        return numBottles + t - 1
 
-            while empty >= numExchange:
-                numBottles += 1
-                empty -= numExchange
-                numExchange += 1
+    # def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+    #     res = 0
+    #     empty = 0
+
+    #     while numBottles:
+    #         res += numBottles
+    #         empty += numBottles
+    #         numBottles = 0
+
+    #         while empty >= numExchange:
+    #             numBottles += 1
+    #             empty -= numExchange
+    #             numExchange += 1
             
-        return res
+    #     return res
 
         
 def main():
