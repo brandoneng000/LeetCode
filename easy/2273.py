c50842f1fcaa14fd40f851aca3de0c3fb6822385
Brandon Eng <brandoneng000@gmail.com>
2025-10-13 03:52:03 -0700

    Updated 2273

--- easy/2273.py
+++ easy/2273.py
@@ -2,21 +2,34 @@ from typing import List
 
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        result = []
-        prev = tuple()
+        n = len(words)
+        res = [words[0]]
 
-        for word in words:
-            key = tuple(sorted(word))
-            if key != prev:
-                prev = key
-                result.append(word)
+        for i in range(1, n):
+            if sorted(words[i]) == sorted(res[-1]):
+                continue
+            res.append(words[i])
+        
+        return res
+
+    # def removeAnagrams(self, words: List[str]) -> List[str]:
+    #     result = []
+    #     prev = tuple()
+
+    #     for word in words:
+    #         key = tuple(sorted(word))
+    #         if key != prev:
+    #             prev = key
+    #             result.append(word)
             
-        return result
+    #     return result
         
 def main():
     sol = Solution()
     print(sol.removeAnagrams(["abba","baba","bbaa","cd","cd"]))
     print(sol.removeAnagrams(["abba","baba","bbaa","cd","cd","abab"]))
+    print(sol.removeAnagrams(["abba","baba","bbaa","cd","cd","bbaa"]))
+    print(sol.removeAnagrams(["a","b","c","d","e"]))
 
 if __name__ == '__main__':
     main()
