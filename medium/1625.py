535fb0d31662f27afefdefe976a14c8fcadf14da
Brandon Eng <brandoneng000@gmail.com>
2025-10-19 01:46:35 -0700

    Updated 1625

--- medium/1625.py
+++ medium/1625.py
@@ -1,29 +1,82 @@
 import collections
+from math import gcd
 
 class Solution:
     def findLexSmallestString(self, s: str, a: int, b: int) -> str:
-        n = len(s)
-        q = collections.deque([s])
-        visited = set()
-        res = s
-
-        while q:
-            node = q.popleft()
-
-            if node in visited:
-                continue
-            
-            visited.add(node)
-            res = min(res, node)
-            q.append(node[b:] + node[:b])
-            cur = list(node)
+        def add_op(s):
+            res = list(s)
 
             for i in range(1, n, 2):
-                cur[i] = str((int(cur[i]) + a) % 10)
-            
-            q.append("".join(cur))
+                res[i] = incremented[s[i]]
+
+            return ''.join(res)
+
+        def rotate_ops(s):
+            return s[b:] + s[:b]
         
-        return res
+        def dfs(s):
+            if s in seen:
+                return 
+            
+            seen.add(s)
+            dfs(add_op(s))
+            dfs(rotate_ops(s))
+
+        n = len(s)
+        incremented = { str(n): str((n + a) % 10) for n in range(10) }
+        seen = set()
+        dfs(s)
+
+        return min(seen)
+
+    # def findLexSmallestString(self, s: str, a: int, b: int) -> str:
+    #     n = len(s)
+    #     res = s
+    #     s = s + s
+    #     g = gcd(b, n)
+
+    #     for i in range(0, n, g):
+    #         for j in range(10):
+    #             k_limit = 0 if b % 2 == 0 else 9
+
+    #             for k in range(k_limit + 1):
+    #                 t = list(s[i: i + n])
+
+    #                 for p in range(1, n, 2):
+    #                     t[p] = str((int(t[p]) + j * a) % 10)
+    #                 for p in range(0, n, 2):
+    #                     t[p] = str((int(t[p]) + k * a) % 10)
+    #                 t_str = ''.join(t)
+
+    #                 if t_str < res:
+    #                     res = t_str
+            
+    #     return res
+
+
+    # def findLexSmallestString(self, s: str, a: int, b: int) -> str:
+    #     n = len(s)
+    #     q = collections.deque([s])
+    #     visited = set()
+    #     res = s
+
+    #     while q:
+    #         node = q.popleft()
+
+    #         if node in visited:
+    #             continue
+            
+    #         visited.add(node)
+    #         res = min(res, node)
+    #         q.append(node[b:] + node[:b])
+    #         cur = list(node)
+
+    #         for i in range(1, n, 2):
+    #             cur[i] = str((int(cur[i]) + a) % 10)
+            
+    #         q.append("".join(cur))
+        
+    #     return res
 
 def main():
     sol = Solution()
