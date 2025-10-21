789f2b5fb0f13100c25c28cf132cbe97f4e3aeb1
Brandon Eng <brandoneng000@gmail.com>
2025-10-21 02:42:24 -0700

    Finished 3346

---
+++ medium/3346.py
@@ -0,0 +1,42 @@
+from typing import List
+from bisect import bisect_left, bisect_right
+
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
+        n = len(nums)
+        nums.sort()
+        res = 0
+        count = {}
+        last_num_index = 0
+
+        for i in range(n):
+            if nums[i] != nums[last_num_index]:
+                count[nums[last_num_index]] = i - last_num_index
+                res = max(res, i - last_num_index)
+                last_num_index = i
+        
+        count[nums[last_num_index]] = n - last_num_index
+        res = max(res, n - last_num_index)
+
+        for i in range(nums[0], nums[-1] + 1):
+            l = bisect_left(nums, i - k)
+            r = bisect_right(nums, i + k) - 1
+
+            if i in count:
+                temp = min(r - l + 1, count[i] + numOperations)
+            else:
+                temp = min(r - l + 1, numOperations)
+            
+            res = max(res, temp)
+        
+        return res
+            
+
+        
+def main():
+    sol = Solution()
+    print(sol.maxFrequency(nums = [1,4,5], k = 1, numOperations = 2))
+    print(sol.maxFrequency(nums = [5,11,20,20], k = 5, numOperations = 1))
+
+if __name__ == '__main__':
+    main()
