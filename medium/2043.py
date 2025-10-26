bd5f3fc6d75e9c0db8658b0e04d3d6b58cda65c4
Brandon Eng <brandoneng000@gmail.com>
2025-10-26 01:53:57 -0700

    Updated 2043

--- medium/2043.py
+++ medium/2043.py
@@ -3,36 +3,65 @@ from typing import List
 class Bank:
 
     def __init__(self, balance: List[int]):
-        self.n = len(balance) + 1
-        self.accounts = {}
-
-        for i, money in enumerate(balance, 1):
-            self.accounts[i] = money
-        
+        self.n = len(balance)
+        self.accounts = balance
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        if not (0 < account1 < self.n) or not (0 < account2 < self.n) or self.accounts[account1] < money:
+        if account1 > self.n or account2 > self.n or self.accounts[account1 - 1] < money:
             return False
-        self.accounts[account1] -= money
-        self.accounts[account2] += money
+        
+        self.accounts[account2 - 1] += money
+        self.accounts[account1 - 1] -= money
 
         return True
         
 
     def deposit(self, account: int, money: int) -> bool:
-        if not (0 < account < self.n):
+        if account > self.n:
             return False
         
-        self.accounts[account] += money
+        self.accounts[account - 1] += money
         return True
-        
 
     def withdraw(self, account: int, money: int) -> bool:
-        if not (0 < account < self.n) or self.accounts[account] < money:
+        if account > self.n or self.accounts[account - 1] < money:
             return False
         
-        self.accounts[account] -= money
+        self.accounts[account - 1] -= money
         return True
+
+# class Bank:
+#     def __init__(self, balance: List[int]):
+#         self.n = len(balance) + 1
+#         self.accounts = {}
+
+#         for i, money in enumerate(balance, 1):
+#             self.accounts[i] = money
+        
+
+#     def transfer(self, account1: int, account2: int, money: int) -> bool:
+#         if not (0 < account1 < self.n) or not (0 < account2 < self.n) or self.accounts[account1] < money:
+#             return False
+#         self.accounts[account1] -= money
+#         self.accounts[account2] += money
+
+#         return True
+        
+
+#     def deposit(self, account: int, money: int) -> bool:
+#         if not (0 < account < self.n):
+#             return False
+        
+#         self.accounts[account] += money
+#         return True
+        
+
+#     def withdraw(self, account: int, money: int) -> bool:
+#         if not (0 < account < self.n) or self.accounts[account] < money:
+#             return False
+        
+#         self.accounts[account] -= money
+#         return True
         
 
 
