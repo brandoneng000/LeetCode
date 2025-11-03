9b3038449d6fae3eee1acb785120c578d3d5e5e4
Brandon Eng <brandoneng000@gmail.com>
2025-11-03 05:44:07 -0800

    Updated 1578

--- medium/1578.py
+++ medium/1578.py
@@ -1,6 +1,17 @@
 from typing import List
 
 class Solution:
+    # def minCost(self, colors: str, neededTime: List[int]) -> int:
+    #     n = len(colors)
+    #     res = 0
+
+    #     for i in range(1, n):
+    #         if colors[i] == colors[i - 1]:
+    #             res += min(neededTime[i], neededTime[i - 1])
+    #             neededTime[i] = max(neededTime[i], neededTime[i - 1])
+        
+    #     return res
+
     def minCost(self, colors: str, neededTime: List[int]) -> int:
         n = len(colors)
         res = 0
