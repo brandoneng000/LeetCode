9b322890c960c4d5aa54b639aa37528f26f2bf64
Brandon Eng <brandoneng000@gmail.com>
2025-11-08 00:30:24 -0800

    Updated 1611

--- hard/1611.py
+++ hard/1611.py
@@ -1,16 +1,30 @@
 class Solution:
     def minimumOneBitOperations(self, n: int) -> int:
-        if n == 0:
-            return 0
+        binary = bin(n)[2:]
+        m = len(binary)
+        res = 0
+        sign = 1
+
+        for i in range(m):
+            bit = binary[i]
+            if bit == '1':
+                res += sign * (2 ** (m - i) - 1)
+                sign *= -1
         
-        k = 0
-        cur = 1
+        return res
+
+    # def minimumOneBitOperations(self, n: int) -> int:
+    #     if n == 0:
+    #         return 0
         
-        while (cur * 2) <= n:
-            cur *= 2
-            k += 1
+    #     k = 0
+    #     cur = 1
         
-        return 2 ** (k + 1) - 1 - self.minimumOneBitOperations(n ^ cur)
+    #     while (cur * 2) <= n:
+    #         cur *= 2
+    #         k += 1
+        
+    #     return 2 ** (k + 1) - 1 - self.minimumOneBitOperations(n ^ cur)
         
 def main():
     sol = Solution()
