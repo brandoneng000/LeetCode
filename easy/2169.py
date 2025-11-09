4f5fc1d5525544914fbb6eb81b573e58cef6ad8e
Brandon Eng <brandoneng000@gmail.com>
2025-11-09 03:05:37 -0800

    Updated 2169

--- easy/2169.py
+++ easy/2169.py
@@ -1,17 +1,27 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
-        if not num1 or not num2:
-            return 0
-        small = min(num1, num2)
-        large = max(num1, num2)
         res = 0
 
-        while large % small != 0:
-            large -= small
-            small, large =  min(large, small), max(large, small)
-            res += 1
+        while num1 and num2:
+            res += num1 // num2
+            num1 %= num2
+            num1, num2, = num2, num1
+        
+        return res
 
-        return res + large // small
+    # def countOperations(self, num1: int, num2: int) -> int:
+    #     if not num1 or not num2:
+    #         return 0
+    #     small = min(num1, num2)
+    #     large = max(num1, num2)
+    #     res = 0
+
+    #     while large % small != 0:
+    #         large -= small
+    #         small, large =  min(large, small), max(large, small)
+    #         res += 1
+
+    #     return res + large // small
 
 
 def main():
