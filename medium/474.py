6657344d04801193a62784cff672e8fb750e72f7
Brandon Eng <brandoneng000@gmail.com>
2025-11-11 02:41:49 -0800

    Updated 474

--- medium/474.py
+++ medium/474.py
@@ -3,17 +3,37 @@ import collections
 
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        dp = collections.defaultdict(lambda: collections.defaultdict(int))
-        
-        for bits in strs:
-            zero = bits.count('0')
-            one = len(bits) - zero
+        dp = { (0, 0): 0 }
 
-            for i in range(m, zero - 1, -1):
-                for j in range(n, one - 1, -1):
-                    dp[i][j] = max(dp[i][j], dp[i - zero][j - one] + 1)
+        for bits in strs:
+            ones = bits.count('1')
+            zeroes = len(bits) - ones
+            new_dp = {}
+
+            for (prev_zero, prev_one), val in dp.items():
+                new_zero = prev_zero + zeroes
+                new_one = prev_one + ones
+                
+                if new_zero <= m and new_one <= n:
+                    if (new_zero, new_one) not in dp or dp[(new_zero, new_one)] < val + 1:
+                        new_dp[(new_zero, new_one)] = val + 1
+                
+            dp.update(new_dp)
         
-        return dp[m][n]
+        return max(dp.values())
+
+    # def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+    #     dp = collections.defaultdict(lambda: collections.defaultdict(int))
+        
+    #     for bits in strs:
+    #         zero = bits.count('0')
+    #         one = len(bits) - zero
+
+    #         for i in range(m, zero - 1, -1):
+    #             for j in range(n, one - 1, -1):
+    #                 dp[i][j] = max(dp[i][j], dp[i - zero][j - one] + 1)
+        
+    #     return dp[m][n]
 
 def main():
     sol = Solution()
