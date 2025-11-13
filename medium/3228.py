dc620bf4badc183c7dc8fad03e043abd48279f3a
Brandon Eng <brandoneng000@gmail.com>
2025-11-13 00:24:46 -0800

    Updated 3228

--- medium/3228.py
+++ medium/3228.py
@@ -2,20 +2,34 @@ class Solution:
     def maxOperations(self, s: str) -> int:
         n = len(s)
         res = 0
-        count = 0
-        i = 0
+        ones = 0
 
-        while i < n:
+        for i in range(n - 1):
             if s[i] == '1':
-                count += 1
-            else:
-                res += count
-                while i + 1 < n and s[i + 1] == '0':
-                    i += 1
-            
-            i += 1
+                ones += 1
+
+                if s[i + 1] == '0':
+                    res += ones
         
         return res
+
+    # def maxOperations(self, s: str) -> int:
+    #     n = len(s)
+    #     res = 0
+    #     count = 0
+    #     i = 0
+
+    #     while i < n:
+    #         if s[i] == '1':
+    #             count += 1
+    #         else:
+    #             res += count
+    #             while i + 1 < n and s[i + 1] == '0':
+    #                 i += 1
+            
+    #         i += 1
+        
+    #     return res
 
 
 def main():
