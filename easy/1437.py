2fb6bc3552c3b80628d0d89dc555967705318b82
Brandon Eng <brandoneng000@gmail.com>
2025-11-17 00:13:07 -0800

    Updated 1437

--- easy/1437.py
+++ easy/1437.py
@@ -2,20 +2,33 @@ from typing import List
 
 class Solution:
     def kLengthApart(self, nums: List[int], k: int) -> bool:
-        count = 0
+        n = len(nums)
+        prev = -100000
 
-        for num in nums:
-            if count <= 0 and num == 1:
-                count = k
-            elif num == 1:
-                return False
-            else:
-                count -= 1
+        for i in range(n):
+            if nums[i]:
+                if i - prev <= k:
+                    return False
+                prev = i
         
         return True
 
+    # def kLengthApart(self, nums: List[int], k: int) -> bool:
+    #     count = 0
+
+    #     for num in nums:
+    #         if count <= 0 and num == 1:
+    #             count = k
+    #         elif num == 1:
+    #             return False
+    #         else:
+    #             count -= 1
+        
+    #     return True
+
 def main():
     sol = Solution()
+    print(sol.kLengthApart(nums = [0,0,0,1,0,1], k = 2))
     print(sol.kLengthApart(nums = [1,0,0,0,1,0,0,1], k = 2))
     print(sol.kLengthApart(nums = [1,0,0,1,0,1], k = 2))
 
