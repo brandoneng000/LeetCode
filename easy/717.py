720d70ec067099ac2231d6d251a27d4db3323289
Brandon Eng <brandoneng000@gmail.com>
2025-11-18 00:53:28 -0800

    Updated 717

--- easy/717.py
+++ easy/717.py
@@ -2,12 +2,25 @@ from typing import List
 
 class Solution:
     def isOneBitCharacter(self, bits: List[int]) -> bool:
-        bits_string = "".join(str(bit) for bit in bits)
+        n = len(bits)
+        i = 0
 
-        bits_string = bits_string.replace('11', '2')
-        bits_string = bits_string.replace('10', '2')
+        while i < n - 1:
+            if bits[i] == 1:
+                i += 2
+            else:
+                i += 1
         
-        return bits_string[-1] == '0'
+        return i == n - 1
+        
+
+    # def isOneBitCharacter(self, bits: List[int]) -> bool:
+    #     bits_string = "".join(str(bit) for bit in bits)
+
+    #     bits_string = bits_string.replace('11', '2')
+    #     bits_string = bits_string.replace('10', '2')
+        
+    #     return bits_string[-1] == '0'
 
 def main():
     sol = Solution()
