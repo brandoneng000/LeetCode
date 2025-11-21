e63d3d22cdd1bee92e59d35a689f175c9a28915c
Brandon Eng <brandoneng000@gmail.com>
2025-11-21 05:54:19 -0800

    Updated 1930

--- medium/1930.py
+++ medium/1930.py
@@ -1,18 +1,34 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        last_index = {}
-        used = set()
+        if len(s) <= 2:
+            return 0
+        
+        letters = set(s)
         res = 0
 
-        for index, letter in enumerate(s):
-            last_index[letter] = index
+        for letter in letters:
+            left = s.find(letter)
+            right = s.rfind(letter)
 
-        for index, letter in enumerate(s):
-            if index != last_index[letter] and letter not in used:
-                res += len(set(s[index + 1: last_index[letter]]))
-                used.add(letter)
+            if left != right:
+                res += len(set(s[left + 1: right]))
         
         return res
+
+    # def countPalindromicSubsequence(self, s: str) -> int:
+    #     last_index = {}
+    #     used = set()
+    #     res = 0
+
+    #     for index, letter in enumerate(s):
+    #         last_index[letter] = index
+
+    #     for index, letter in enumerate(s):
+    #         if index != last_index[letter] and letter not in used:
+    #             res += len(set(s[index + 1: last_index[letter]]))
+    #             used.add(letter)
+        
+    #     return res
         
         
 def main():
