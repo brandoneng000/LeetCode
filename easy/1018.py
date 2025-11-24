1397413bf62093ee929aa572f109f22a8d3cad1d
Brandon Eng <brandoneng000@gmail.com>
2025-11-24 05:50:54 -0800

    Updated 1018

--- easy/1018.py
+++ easy/1018.py
@@ -2,14 +2,24 @@ from typing import List
 
 class Solution:
     def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
-        value = 0
+        res = []
+        cur = 0
 
-        for index in range(len(nums)):
-            value <<= 1
-            value |= nums[index]
-            nums[index] = value % 5 == 0
+        for bit in nums:
+            cur = (cur * 2 + bit) % 5
+            res.append(cur == 0)
+        
+        return res
 
-        return nums
+    # def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+    #     value = 0
+
+    #     for index in range(len(nums)):
+    #         value <<= 1
+    #         value |= nums[index]
+    #         nums[index] = value % 5 == 0
+
+    #     return nums
     
 def main():
     sol = Solution()
