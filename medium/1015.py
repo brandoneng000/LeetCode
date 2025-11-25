3e2f22c875a45450696ab090e5d3c2b4848f2c8d
Brandon Eng <brandoneng000@gmail.com>
2025-11-25 02:25:47 -0800

    Updated 1015

--- medium/1015.py
+++ medium/1015.py
@@ -2,17 +2,28 @@ from typing import List
 
 class Solution:
     def smallestRepunitDivByK(self, k: int) -> int:
-        r = 0
-        size = 0
-        remainders = set()
-        while r not in remainders:
-            remainders.add(r)
-            r = ((r * 10) + 1) % k
-            size += 1
-            if r == 0:
+        remainder = 0
+
+        for size in range(1, k + 1):
+            remainder = (remainder * 10 + 1) % k
+
+            if remainder == 0:
                 return size
-            
+        
         return -1
+
+    # def smallestRepunitDivByK(self, k: int) -> int:
+    #     r = 0
+    #     size = 0
+    #     remainders = set()
+    #     while r not in remainders:
+    #         remainders.add(r)
+    #         r = ((r * 10) + 1) % k
+    #         size += 1
+    #         if r == 0:
+    #             return size
+            
+    #     return -1
 
 def main():
     sol = Solution()
