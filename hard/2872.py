c1602c88a39147cf1fc4e4e59ef63b7b72c2a54a
Brandon Eng <brandoneng000@gmail.com>
2025-11-28 03:25:10 -0800

    Updated 2872

--- hard/2872.py
+++ hard/2872.py
@@ -1,33 +1,64 @@
 from typing import List
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class Solution:
     def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
-        def dfs(root: int, parent: int, values: List[int], tree: defaultdict):
-            cur = values[root]
-
-            for nei in tree[root]:
-                if nei == parent:
-                    continue
-
-                cur += dfs(nei, root, values, tree)
-            
-            if cur % k == 0:
-                self.res += 1
-                return 0
-
-            return cur
+        if n < 2:
+            return 1
         
-        tree = defaultdict(list)
-        self.res = 0
+        res = 0
+        graph = defaultdict(set)
 
         for u, v in edges:
-            tree[u].append(v)
-            tree[v].append(u)
+            graph[u].add(v)
+            graph[v].add(u)
 
-        dfs(0, -1, values, tree)
+        q = deque(node for node, nei in graph.items() if len(nei) == 1)
 
-        return self.res
+        while q:
+            cur_node = q.popleft()
+            nei_node = next(iter(graph[cur_node])) if graph[cur_node] else -1
+
+            if nei_node >= 0:
+                graph[nei_node].remove(cur_node)
+
+            if values[cur_node] % k == 0:
+                res += 1
+            else:
+                values[nei_node] += values[cur_node]
+            
+            if nei_node >= 0 and len(graph[nei_node]) == 1:
+                q.append(nei_node)
+        
+        return res
+
+
+    # def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+    #     def dfs(root: int, parent: int, values: List[int], tree: defaultdict):
+    #         cur = values[root]
+
+    #         for nei in tree[root]:
+    #             if nei == parent:
+    #                 continue
+
+    #             cur += dfs(nei, root, values, tree)
+            
+    #         if cur % k == 0:
+    #             self.res += 1
+    #             return 0
+
+    #         return cur
+        
+    #     tree = defaultdict(list)
+    #     self.res = 0
+
+    #     for u, v in edges:
+    #         tree[u].append(v)
+    #         tree[v].append(u)
+
+    #     dfs(0, -1, values, tree)
+
+    #     return self.res
         
         
 def main():
