0c6f156c129c7ebc58f81f599d1cd23aed307fc0
Brandon Eng <brandoneng000@gmail.com>
2025-11-30 02:43:47 -0800

    Updated 1590

--- medium/1590.py
+++ medium/1590.py
@@ -3,19 +3,43 @@ from typing import List
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
         n = len(nums)
-        remove_remainder = sum(nums) % p
+        total = sum(nums) 
+        target = total % p
+
+        if target == 0:
+            return 0
+        
         dp = { 0: -1 }
-        cur = 0
+        cur_sum = 0
         res = n
 
-        for i, num in enumerate(nums):
-            cur = (cur + num) % p
-            dp[cur] = i
+        for i in range(n):
+            cur_sum = (cur_sum + nums[i]) % p
+            needed = (cur_sum - target + p) % p
 
-            if (cur - remove_remainder) % p in dp:
-                res = min(res, i - dp[(cur - remove_remainder) % p])
+            if needed in dp:
+                res = min(res, i - dp[needed])
+            
+            dp[cur_sum] = i
         
-        return res if res < n else -1
+        return -1 if res == n else res
+        
+
+    # def minSubarray(self, nums: List[int], p: int) -> int:
+    #     n = len(nums)
+    #     remove_remainder = sum(nums) % p
+    #     dp = { 0: -1 }
+    #     cur = 0
+    #     res = n
+
+    #     for i, num in enumerate(nums):
+    #         cur = (cur + num) % p
+    #         dp[cur] = i
+
+    #         if (cur - remove_remainder) % p in dp:
+    #             res = min(res, i - dp[(cur - remove_remainder) % p])
+        
+    #     return res if res < n else -1
         
 def main():
     sol = Solution()
