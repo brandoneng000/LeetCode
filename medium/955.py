7f160990a99dc4646b709690397e07b30fbdd1e5
Brandon Eng <brandoneng000@gmail.com>
2025-12-21 10:34:36 -0800

    Updated 955

--- medium/955.py
+++ medium/955.py
@@ -3,22 +3,38 @@ from typing import List
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:     
         m, n = len(strs), len(strs[0])
-        res = 0
         ordered = [False] * (m - 1)
+        res = 0
 
-        for j in range(n):
-            temp = ordered.copy()
-            for i in range(m - 1):
-                if not ordered[i] and strs[i][j] > strs[i + 1][j]:
-                    res += 1
-                    break
-                elif strs[i][j] < strs[i + 1][j] and not ordered[i]:
-                    temp[i] = True
+        for col in zip(*strs):
+            if all(ordered[i] or col[i] <= col[i + 1] for i in range(len(col) - 1)):
+                for i in range(len(col) - 1):
+                    if col[i] < col[i + 1]:
+                        ordered[i] = True
             else:
-                ordered = temp
-            if all(ordered):
-                return res
+                res += 1
+        
         return res
+
+
+    # def minDeletionSize(self, strs: List[str]) -> int:     
+    #     m, n = len(strs), len(strs[0])
+    #     res = 0
+    #     ordered = [False] * (m - 1)
+
+    #     for j in range(n):
+    #         temp = ordered.copy()
+    #         for i in range(m - 1):
+    #             if not ordered[i] and strs[i][j] > strs[i + 1][j]:
+    #                 res += 1
+    #                 break
+    #             elif strs[i][j] < strs[i + 1][j] and not ordered[i]:
+    #                 temp[i] = True
+    #         else:
+    #             ordered = temp
+    #         if all(ordered):
+    #             return res
+    #     return res
         
 
 def main():
