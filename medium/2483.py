a68319494c04c86915a8fa12b77ac8a841d516c3
Brandon Eng <brandoneng000@gmail.com>
2025-12-26 06:04:56 -0800

    Updated 2483

--- medium/2483.py
+++ medium/2483.py
@@ -1,19 +1,33 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        cur_pen = min_pen = customers.count('Y')
+        n = len(customers)
         res = 0
+        cur_score = max_score = 0
 
-        for i, c in enumerate(customers):
-            if c == 'Y':
-                cur_pen -= 1
-            else:
-                cur_pen += 1
-            
-            if cur_pen < min_pen:
+        for i in range(n):
+            cur_score += 1 if customers[i] == 'Y' else -1
+
+            if cur_score > max_score:
+                max_score = cur_score
                 res = i + 1
-                min_pen = cur_pen
         
         return res
+
+    # def bestClosingTime(self, customers: str) -> int:
+    #     cur_pen = min_pen = customers.count('Y')
+    #     res = 0
+
+    #     for i, c in enumerate(customers):
+    #         if c == 'Y':
+    #             cur_pen -= 1
+    #         else:
+    #             cur_pen += 1
+            
+    #         if cur_pen < min_pen:
+    #             res = i + 1
+    #             min_pen = cur_pen
+        
+    #     return res
 
 def main():
     sol = Solution()
