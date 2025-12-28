73eae02e719eaae8c604b0de3380324d8e4027ce
Brandon Eng <brandoneng000@gmail.com>
2025-12-28 11:11:51 -0800

    Updated 1351

--- easy/1351.py
+++ easy/1351.py
@@ -2,16 +2,28 @@ from typing import List
 
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
-        negatives = 0
-        col_end = len(grid[0])
+        n, m = len(grid), len(grid[0])
+        res = 0
 
-        for row in range(len(grid)):
-            for col in range(col_end):
-                if grid[row][col] < 0:
-                    col_end = min(col_end, col)
-                    negatives += (len(grid) - row)
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] < 0:
+                    res += m - j
+                    break
+        
+        return res
 
-        return negatives
+    # def countNegatives(self, grid: List[List[int]]) -> int:
+    #     negatives = 0
+    #     col_end = len(grid[0])
+
+    #     for row in range(len(grid)):
+    #         for col in range(col_end):
+    #             if grid[row][col] < 0:
+    #                 col_end = min(col_end, col)
+    #                 negatives += (len(grid) - row)
+
+    #     return negatives
 
 def main():
     sol = Solution()
