551b1380556ca7f5830e3fe6dc7c7514008b9685
Brandon Eng <brandoneng000@gmail.com>
2026-01-04 05:53:42 -0800

    Updated 1390

--- medium/1390.py
+++ medium/1390.py
@@ -6,17 +6,18 @@ class Solution:
             if num in cache:
                 return cache[num]
             
-            res = set([1, num])
+            res = 1 + num
+            count = 2
             
             for i in range(2, int(num ** 0.5) + 1):
                 if num % i == 0:
-                    res.add(i)
-                    res.add(num // i)
-                if len(res) > 4:
+                    count += 2 if i * i != num else 1
+                    res += i + num // i
+                if count > 4:
                     break
             
-            if len(res) == 4:
-                cache[num] = sum(res)
+            if count == 4:
+                cache[num] = res
             else:
                 cache[num] = 0
             
@@ -24,10 +25,35 @@ class Solution:
 
         cache = {}
         return sum(find_factors(n) for n in nums)
+    
+    # def sumFourDivisors(self, nums: List[int]) -> int:
+    #     def find_factors(num: int):
+    #         if num in cache:
+    #             return cache[num]
+            
+    #         res = set([1, num])
+            
+    #         for i in range(2, int(num ** 0.5) + 1):
+    #             if num % i == 0:
+    #                 res.add(i)
+    #                 res.add(num // i)
+    #             if len(res) > 4:
+    #                 break
+            
+    #         if len(res) == 4:
+    #             cache[num] = sum(res)
+    #         else:
+    #             cache[num] = 0
+            
+    #         return cache[num]
+
+    #     cache = {}
+    #     return sum(find_factors(n) for n in nums)
 
 
 def main():
     sol = Solution()
+    print(sol.sumFourDivisors([8]))
     print(sol.sumFourDivisors([21,4,7]))
     print(sol.sumFourDivisors([21,21]))
     print(sol.sumFourDivisors([1,2,3,4,5]))
