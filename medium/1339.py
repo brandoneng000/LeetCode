368e8efb8b4a8c2148500138c3bc80e4474157c6
Brandon Eng <brandoneng000@gmail.com>
2026-01-07 01:37:37 -0800

    Updated 1339

--- medium/1339.py
+++ medium/1339.py
@@ -11,16 +11,38 @@ class Solution:
         def sum_tree(root: TreeNode):
             if not root:
                 return 0
+            
             left = sum_tree(root.left)
             right = sum_tree(root.right)
-            self.res = max(self.res, left * (total_tree - left), right * (total_tree - right))
 
-            return root.val + left + right
+            total = left + right + root.val
+            sums.append(total)
+            return total
         
-        mod = 1000000007
-        total_tree = 0
-        self.res = 0
-        total_tree = sum_tree(root)
-        sum_tree(root)
+        mod = 1_000_000_007
+        sums = []
+        total = sum_tree(root)
+        res = 0
 
-        return self.res % mod
+        for s in sums:
+            res = max(res, s * (total - s))
+        
+        return res % mod
+
+    # def maxProduct(self, root: Optional[TreeNode]) -> int:
+    #     def sum_tree(root: TreeNode):
+    #         if not root:
+    #             return 0
+    #         left = sum_tree(root.left)
+    #         right = sum_tree(root.right)
+    #         self.res = max(self.res, left * (total_tree - left), right * (total_tree - right))
+
+    #         return root.val + left + right
+        
+    #     mod = 1000000007
+    #     total_tree = 0
+    #     self.res = 0
+    #     total_tree = sum_tree(root)
+    #     sum_tree(root)
+
+    #     return self.res % mod
