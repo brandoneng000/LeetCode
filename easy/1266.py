61c18bd9d1c6baf776b51c4d9fe4220ab4dbd0ef
Brandon Eng <brandoneng000@gmail.com>
2026-01-12 04:01:47 -0800

    Updated and cleaned up 1266

--- easy/1266.py
+++ easy/1266.py
@@ -2,27 +2,43 @@ from typing import List
 
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        # x, y = points.pop(0)
-        # result = 0
+        n = len(points)
+        res = 0
 
-        # for point in points:
-        #     large_x = max(point[0], x)
-        #     large_y = max(point[1], y)
-        #     small_x = min(point[0], x)
-        #     small_y = min(point[1], y)
-        #     diag = min(large_x - small_x, large_y - small_y)
-        #     straight = max(large_x - (small_x + diag), large_y - (small_y + diag))
-        #     result += diag + straight
-        #     x, y = point
-        
-        # return result
-        result = 0
+        for i in range(n - 1):
+            x1, y1 = points[i]
+            x2, y2 = points[i + 1]
+            time = max(abs(x2 - x1), abs(y2 - y1))
 
-        for index in range(1, len(points)):
-            straight = max(abs(points[index - 1][0] - points[index][0]), abs(points[index - 1][1] - points[index][1]))
-            result += straight
+            res += time
+
+        return res
+
+
+    # def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+    #     x, y = points.pop(0)
+    #     result = 0
+
+    #     for point in points:
+    #         large_x = max(point[0], x)
+    #         large_y = max(point[1], y)
+    #         small_x = min(point[0], x)
+    #         small_y = min(point[1], y)
+    #         diag = min(large_x - small_x, large_y - small_y)
+    #         straight = max(large_x - (small_x + diag), large_y - (small_y + diag))
+    #         result += diag + straight
+    #         x, y = point
         
-        return result
+    #     return result
+    
+    # def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+    #     result = 0
+
+    #     for index in range(1, len(points)):
+    #         straight = max(abs(points[index - 1][0] - points[index][0]), abs(points[index - 1][1] - points[index][1]))
+    #         result += straight
+        
+    #     return result
 
 def main():
     sol = Solution()
