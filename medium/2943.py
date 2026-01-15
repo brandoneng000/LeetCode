86bc520fd5a33819023a529f3cda499ef72a1244
Brandon Eng <brandoneng000@gmail.com>
2026-01-15 06:53:13 -0800

    Updated 2943

--- medium/2943.py
+++ medium/2943.py
@@ -3,24 +3,42 @@ from typing import List
 class Solution:
     def maximizeSquareHoleArea(self, n: int, m: int, hBars: List[int], vBars: List[int]) -> int:
         def count_consecutive(nums: List[int]):
-            if not nums:
-                return 0
-            
             nums.sort()
             n = len(nums)
-            stack = [1]
+            res = 1
+            cur = 1
 
-            for i in range(n - 1):
-                if nums[i] + 1 == nums[i + 1]:
-                    stack[-1] += 1
+            for i in range(1, n):
+                if nums[i - 1] + 1 == nums[i]:
+                    cur += 1
+                    res = max(res, cur)
                 else:
-                    stack.append(1)
+                    cur = 1
+            
+            return res + 1
+        
+        return min(count_consecutive(hBars), count_consecutive(vBars)) ** 2
 
-            return max(stack)
+    # def maximizeSquareHoleArea(self, n: int, m: int, hBars: List[int], vBars: List[int]) -> int:
+    #     def count_consecutive(nums: List[int]):
+    #         if not nums:
+    #             return 0
+            
+    #         nums.sort()
+    #         n = len(nums)
+    #         stack = [1]
 
-        size = min(count_consecutive(hBars), count_consecutive(vBars))
+    #         for i in range(n - 1):
+    #             if nums[i] + 1 == nums[i + 1]:
+    #                 stack[-1] += 1
+    #             else:
+    #                 stack.append(1)
 
-        return (1 + size) * (1 + size)
+    #         return max(stack)
+
+    #     size = min(count_consecutive(hBars), count_consecutive(vBars))
+
+    #     return (1 + size) * (1 + size)
         
 
         
