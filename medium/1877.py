c824fd798d930c923fcae9823ab4f0b0587c198b
Brandon Eng <brandoneng000@gmail.com>
2026-01-24 05:40:56 -0800

    Updated 1877

--- medium/1877.py
+++ medium/1877.py
@@ -2,15 +2,28 @@ from typing import List
 
 class Solution:
     def minPairSum(self, nums: List[int]) -> int:
-        n = len(nums)
         nums.sort()
         res = 0
+        left = 0
+        right = len(nums) - 1
 
-        for i in range(n // 2):
-            if nums[i] + nums[n - i - 1] > res:
-                res = nums[i] + nums[n - i - 1]
+        while left < right:
+            res = max(res, nums[left] + nums[right])
+            left += 1
+            right -= 1
         
         return res
+
+    # def minPairSum(self, nums: List[int]) -> int:
+    #     n = len(nums)
+    #     nums.sort()
+    #     res = 0
+
+    #     for i in range(n // 2):
+    #         if nums[i] + nums[n - i - 1] > res:
+    #             res = nums[i] + nums[n - i - 1]
+        
+    #     return res
 
         
 def main():
