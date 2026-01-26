7d5907aa2fd5cd920f39861381286588a1b6dc0e
Brandon Eng <brandoneng000@gmail.com>
2026-01-26 02:17:21 -0800

    Updated 1200

--- easy/1200.py
+++ easy/1200.py
@@ -2,25 +2,42 @@ from typing import List
 
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-        import heapq
-        heapq.heapify(arr)
-        result = []
-        min_distance = float('inf')
-        a = heapq.heappop(arr)
+        n = len(arr)
+        arr.sort()
+        res = []
+        min_diff = 1000000
+
+        for i in range(n - 1):
+            diff = arr[i + 1] - arr[i]
+            if diff < min_diff:
+                res = [[arr[i], arr[i + 1]]]
+                min_diff = diff
+            elif diff == min_diff:
+                res.append([arr[i], arr[i + 1]])
         
-        while arr:
-            b = heapq.heappop(arr)
-            distance = b - a
-            if distance < min_distance:
-                result.clear()
-                result.append([a, b])
-                min_distance = distance
-            elif distance == min_distance:
-                result.append([a,b])
+        return res
+
+
+    # def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+    #     import heapq
+    #     heapq.heapify(arr)
+    #     result = []
+    #     min_distance = float('inf')
+    #     a = heapq.heappop(arr)
+        
+    #     while arr:
+    #         b = heapq.heappop(arr)
+    #         distance = b - a
+    #         if distance < min_distance:
+    #             result.clear()
+    #             result.append([a, b])
+    #             min_distance = distance
+    #         elif distance == min_distance:
+    #             result.append([a,b])
             
-            a = b
+    #         a = b
         
-        return result
+    #     return result
 
 
 def main():
