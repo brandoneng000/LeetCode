1105d382bc088a17ece9d475c00de26ac6080ea9
Brandon Eng <brandoneng000@gmail.com>
2026-02-01 15:53:47 -0800

    Updated 3010

--- easy/3010.py
+++ easy/3010.py
@@ -2,7 +2,25 @@ from typing import List
 
 class Solution:
     def minimumCost(self, nums: List[int]) -> int:
-        return nums[0] + sum(sorted(nums[1:])[:2])
+        n = len(nums)
+        start = nums[0]
+        if nums[1] < nums[2]:
+            min_one = nums[1]
+            min_two = nums[2]
+        else:
+            min_two = nums[1]
+            min_one = nums[2]
+
+        for i in range(3, n):
+            if nums[i] < min_one:
+                min_one, min_two = nums[i], min_one
+            elif nums[i] < min_two:
+                min_two = nums[i]
+        
+        return start + min_one + min_two
+
+    # def minimumCost(self, nums: List[int]) -> int:
+    #     return nums[0] + sum(sorted(nums[1:])[:2])
         
 def main():
     sol = Solution()
