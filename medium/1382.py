5dfe95c97b029b6ea78117f279ba51d8bc58f9a6
Brandon Eng <brandoneng000@gmail.com>
2026-02-09 02:42:56 -0800

    Updated 1382

--- medium/1382.py
+++ medium/1382.py
@@ -15,17 +15,42 @@ class Solution:
             vals.append(root.val)
             in_order(root.right)
         
-        def generate_tree(vals: List[int]):
-            if not vals:
+        def generate_tree(left: int, right: int):
+            if left > right:
                 return None
             
-            middle_index = len(vals) // 2
+            middle_index = (left + right) // 2
             root = TreeNode(vals[middle_index])
-            root.left = generate_tree(vals[:middle_index - 1])
-            root.right = generate_tree(vals[middle_index + 1:])
+            root.left = generate_tree(left, middle_index - 1)
+            root.right = generate_tree(middle_index + 1, right)
 
             return root
 
         vals = []
         in_order(root)
-        return generate_tree(vals)
+        return generate_tree(0, len(vals) - 1)
+
+
+    # def balanceBST(self, root: TreeNode) -> TreeNode:
+    #     def in_order(root: TreeNode):
+    #         if not root:
+    #             return
+            
+    #         in_order(root.left)
+    #         vals.append(root.val)
+    #         in_order(root.right)
+        
+    #     def generate_tree(vals: List[int]):
+    #         if not vals:
+    #             return None
+            
+    #         middle_index = len(vals) // 2
+    #         root = TreeNode(vals[middle_index])
+    #         root.left = generate_tree(vals[:middle_index])
+    #         root.right = generate_tree(vals[middle_index + 1:])
+
+    #         return root
+
+    #     vals = []
+    #     in_order(root)
+    #     return generate_tree(vals)
