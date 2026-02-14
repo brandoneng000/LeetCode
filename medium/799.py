2dc797f64ccb559e500cafbbb4c4e5b777826b2d
Brandon Eng <brandoneng000@gmail.com>
2026-02-14 05:01:20 -0800

    Updated 799

--- medium/799.py
+++ medium/799.py
@@ -1,19 +1,34 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        cups = {}
+        dp = [[0] * (i + 1) for i in range(1, 102)]
+        dp[0][0] = poured
 
-        for i in range(query_row + 2):
-            cups[i] = [0] * (i + 1)
-        
-        cups[0][0] = poured
-        for r in range(query_row + 1):
+        for r in range(query_row):
             for c in range(r + 1):
-                q = (cups[r][c] - 1.0) / 2.0
-                if q > 0:
-                    cups[r + 1][c] += q
-                    cups[r + 1][c + 1] += q
+                if dp[r][c] > 1:
+                    excess = dp[r][c] - 1
+                    dp[r + 1][c] += excess / 2
+                    dp[r + 1][c + 1] += excess / 2
+                    dp[r][c] = 1
         
-        return min(1, cups[query_row][query_glass])
+        return min(1, dp[query_row][query_glass])
+
+
+    # def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+    #     cups = {}
+
+    #     for i in range(query_row + 2):
+    #         cups[i] = [0] * (i + 1)
+        
+    #     cups[0][0] = poured
+    #     for r in range(query_row + 1):
+    #         for c in range(r + 1):
+    #             q = (cups[r][c] - 1.0) / 2.0
+    #             if q > 0:
+    #                 cups[r + 1][c] += q
+    #                 cups[r + 1][c + 1] += q
+        
+    #     return min(1, cups[query_row][query_glass])
 
 def main():
     sol = Solution()
