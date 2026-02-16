b1f5278189adb6f0456f8ad18818c19125f8524e
Brandon Eng <brandoneng000@gmail.com>
2026-02-16 02:59:25 -0800

    Updated 190

--- easy/190.py
+++ easy/190.py
@@ -1,14 +1,21 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        res = 0
-        for bit in range(32):
-            res = (res << 1) | (n & 1)
-            n >>= 1
+        # binary = bin(n)[2:].zfill(32)
+        binary = f"{n:032b}"
+        return int(binary[::-1], 2)
+
+    # def reverseBits(self, n: int) -> int:
+    #     res = 0
+    #     for bit in range(32):
+    #         res = (res << 1) | (n & 1)
+    #         n >>= 1
         
-        return res
+    #     return res
 
 def main():
     sol = Solution()
+    print(sol.reverseBits(43261596))
+    print(sol.reverseBits(2147483644))
     print(sol.reverseBits(0b00000010100101000001111010011100))
 
 if __name__ == '__main__':
