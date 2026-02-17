704aed64e36b660fbc250ab1e221dedf4d516435
Brandon Eng <brandoneng000@gmail.com>
2026-02-17 04:30:09 -0800

    Updated 401

--- easy/401.py
+++ easy/401.py
@@ -1,15 +1,38 @@
 from typing import List
+from itertools import combinations
 
 class Solution:
     def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        leds = [
+            (8, 'h'), (4, 'h'), (2, 'h'), (1, 'h'),
+            (32, 'm'), (16, 'm'), (8, 'm'), (4, 'm'), (2, 'm'), (1, 'm')
+        ]
         res = []
 
-        for h in range(12):
-            for m in range(60):
-                if bin((h * 64) + m).count('1') == turnedOn:
-                    res.append(f'{h}:{m:02d}')
+        for time in combinations(leds, turnedOn):
+            hours = 0
+            minutes = 0
 
+            for val, unit in time:
+                if unit == 'h':
+                    hours += val
+                else:
+                    minutes += val
+
+            if hours < 12 and minutes < 60:
+                res.append(f'{hours}:{minutes:02d}')
+        
         return res
+
+    # def readBinaryWatch(self, turnedOn: int) -> List[str]:
+    #     res = []
+
+    #     for h in range(12):
+    #         for m in range(60):
+    #             if bin((h * 64) + m).count('1') == turnedOn:
+    #                 res.append(f'{h}:{m:02d}')
+
+    #     return res
 
 
 def main():
