5c407c97fafa9cb0861360bf276a831624661764
Brandon Eng <brandoneng000@gmail.com>
2026-02-22 03:21:38 -0800

    Updated 868

--- easy/868.py
+++ easy/868.py
@@ -1,17 +1,33 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
-        result = -1
-        count = -1
-        
-        while n:
-            if n & 1:
-                result = max(result, count, 0)
-                count = 1
-            elif count != -1:
-                count += 1
-            n >>= 1
+        binary = bin(n)[2:]
+        n = len(binary)
+        left = 0
+        res = 0
 
-        return result
+        while left < n and binary[left] == '0':
+            left += 1
+
+        for right in range(left + 1, n):
+            if binary[right] == '1':
+                res = max(res, right - left)
+                left = right
+        
+        return res
+
+    # def binaryGap(self, n: int) -> int:
+    #     result = -1
+    #     count = -1
+        
+    #     while n:
+    #         if n & 1:
+    #             result = max(result, count, 0)
+    #             count = 1
+    #         elif count != -1:
+    #             count += 1
+    #         n >>= 1
+
+    #     return result
 
 
 def main():
