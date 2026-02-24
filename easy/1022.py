220ea5802981684bcb569b7d263ec7966e8cfdcc
Brandon Eng <brandoneng000@gmail.com>
2026-02-24 02:49:46 -0800

    Updated 1022

--- easy/1022.py
+++ easy/1022.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from collections import deque
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -8,13 +9,35 @@ class TreeNode:
         self.right = right
 class Solution:
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
-        def dfs(root: TreeNode, val: int = 0):
-            if not root:
-                return 0
+        q = deque([(root, 0)])
+        res = 0
+
+        while q:
+            size = len(q)
+
+            for _ in range(size):
+                node, val = q.popleft()
+                val = val * 2 + node.val
+                
+                if not node.left and not node.right:
+                    res += val
+                
+                if node.left:
+                    q.append((node.left, val))
+                
+                if node.right:
+                    q.append((node.right, val))
+
+        return res
+
+    # def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+    #     def dfs(root: TreeNode, val: int = 0):
+    #         if not root:
+    #             return 0
             
-            val = val * 2 + root.val
-            if root.left == root.right:
-                return val
-            return dfs(root.left, val) + dfs(root.right, val)
+    #         val = val * 2 + root.val
+    #         if root.left == root.right:
+    #             return val
+    #         return dfs(root.left, val) + dfs(root.right, val)
         
-        return dfs(root, 0)
+    #     return dfs(root, 0)
