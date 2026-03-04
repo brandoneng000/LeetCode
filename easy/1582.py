019a762fd328fc891bc487f5e512ac9bbd7467b9
Brandon Eng <brandoneng000@gmail.com>
2026-03-04 02:54:35 -0800

    Updated 1582

--- easy/1582.py
+++ easy/1582.py
@@ -2,21 +2,41 @@ from typing import List
 
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
-        cols = []
-        result = 0
+        m, n = len(mat), len(mat[0])
+        row_count = [0] * m
+        col_count = [0] * n
+        res = 0
 
-        for index in range(len(mat[0])):
-            temp = []
-            for j in range(len(mat)):
-                temp.append(mat[j][index])
-            cols.append(temp)
+        for row in range(m):
+            for col in range(n):
+                if mat[row][col] == 1:
+                    row_count[row] += 1
+                    col_count[col] += 1
         
-        for row in mat:
-            if sum(row) == 1:
-                if sum(cols[row.index(1)]) == 1:
-                    result += 1
+        for row in range(m):
+            for col in range(n):
+                if mat[row][col] == 1:
+                    if row_count[row] == 1 and col_count[col] == 1:
+                        res += 1
         
-        return result
+        return res
+
+    # def numSpecial(self, mat: List[List[int]]) -> int:
+    #     cols = []
+    #     result = 0
+
+    #     for index in range(len(mat[0])):
+    #         temp = []
+    #         for j in range(len(mat)):
+    #             temp.append(mat[j][index])
+    #         cols.append(temp)
+        
+    #     for row in mat:
+    #         if sum(row) == 1:
+    #             if sum(cols[row.index(1)]) == 1:
+    #                 result += 1
+        
+    #     return result
 
 
 def main():
