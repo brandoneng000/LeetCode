0597189a2113f168323ee7ffbc9e8d390492cb07
Brandon Eng <brandoneng000@gmail.com>
2026-03-05 05:16:47 -0800

    Updated 1758

--- easy/1758.py
+++ easy/1758.py
@@ -1,16 +1,27 @@
 class Solution:
     def minOperations(self, s: str) -> int:
+        n = len(s)
         res = 0
-        temp = 0
-        alternate = bool(int(s[0]))
-        for bit in s:
-            if alternate != int(bit):
+
+        for i in range(n):
+            if s[i] != ('1' if i % 2 == 0 else '0'):
                 res += 1
-            if not alternate != int(bit):
-                temp += 1
-            alternate = not alternate
         
-        return min(temp, res)
+        return min(res, n - res)
+            
+
+    # def minOperations(self, s: str) -> int:
+    #     res = 0
+    #     temp = 0
+    #     alternate = bool(int(s[0]))
+    #     for bit in s:
+    #         if alternate != int(bit):
+    #             res += 1
+    #         if not alternate != int(bit):
+    #             temp += 1
+    #         alternate = not alternate
+        
+    #     return min(temp, res)
         
         
 def main():
