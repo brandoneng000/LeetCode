b384f357dbda8807a237287a9d0f6678baee8410
Brandon Eng <brandoneng000@gmail.com>
2026-03-29 01:40:09 -0700

    Updated 2839

--- easy/2839.py
+++ easy/2839.py
@@ -1,27 +1,36 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        if s1 == s2:
-            return True
-        
-        s1_list = list(s1)
-        s2_list = list(s2)
+        s1_first_set = { s1[0], s1[2] }
+        s1_second_set = { s1[1], s1[3] }
 
-        s1_list[0], s1_list[2] = s1_list[2], s1_list[0]
-        if s1_list == s2_list:
-            return True
-        
-        s1_list[1], s1_list[3] = s1_list[3], s1_list[1]
-        if s1_list == s2_list:
-            return True
-        
-        s1_list = list(s1)
-        s2_list = list(s2)
+        s2_first_set = { s2[0], s2[2] }
+        s2_second_set = { s2[1], s2[3] }
 
-        s1_list[1], s1_list[3] = s1_list[3], s1_list[1]
-        if s1_list == s2_list:
-            return True
+        return s1_first_set == s2_first_set and s1_second_set == s2_second_set
+
+    # def canBeEqual(self, s1: str, s2: str) -> bool:
+    #     if s1 == s2:
+    #         return True
         
-        return False
+    #     s1_list = list(s1)
+    #     s2_list = list(s2)
+
+    #     s1_list[0], s1_list[2] = s1_list[2], s1_list[0]
+    #     if s1_list == s2_list:
+    #         return True
+        
+    #     s1_list[1], s1_list[3] = s1_list[3], s1_list[1]
+    #     if s1_list == s2_list:
+    #         return True
+        
+    #     s1_list = list(s1)
+    #     s2_list = list(s2)
+
+    #     s1_list[1], s1_list[3] = s1_list[3], s1_list[1]
+    #     if s1_list == s2_list:
+    #         return True
+        
+    #     return False
         
         
 def main():
