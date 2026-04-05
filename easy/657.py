4907f0473197d254e2ada27035808b78d9e0fffd
Brandon Eng <brandoneng000@gmail.com>
2026-04-05 06:27:34 -0700

    Updated 657

--- easy/657.py
+++ easy/657.py
@@ -1,12 +1,16 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        from typing import Counter
-        counter = Counter(moves)
-        return counter['U'] == counter['D'] and counter['L'] == counter['R']
+        return (moves.count('U') == moves.count('D')) and (moves.count('L') == moves.count('R'))
+
+    # def judgeCircle(self, moves: str) -> bool:
+    #     from typing import Counter
+    #     counter = Counter(moves)
+    #     return counter['U'] == counter['D'] and counter['L'] == counter['R']
 
 def main():
     sol = Solution()
     print(sol.judgeCircle("UD"))
+    print(sol.judgeCircle("LL"))
 
 if __name__ == '__main__':
     main()
