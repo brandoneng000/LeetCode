e4d1e3da7cedb3453a9c410fd1401fb0222426ac
Brandon Eng <brandoneng000@gmail.com>
2026-04-15 02:36:19 -0700

    Updated 2515

--- easy/2515.py
+++ easy/2515.py
@@ -2,20 +2,32 @@ from typing import List
 
 class Solution:
     def closetTarget(self, words: List[str], target: str, startIndex: int) -> int:
-        if target not in words:
-            return -1
-        forward = backward = startIndex
-        count = 0
-        while words[forward] != target and words[backward] != target:
-            count += 1
-            forward += 1
-            backward -= 1
-            if forward == len(words):
-                forward = 0
-            if backward == -(len(words) + 1):
-                backward = len(words) - 1
+        INF = 10 ** 33
+        n = len(words)
+        res = INF
+
+        for i in range(n):
+            if words[i] == target:
+                res = min(res, abs(i - startIndex), n - abs(i - startIndex))
             
-        return count
+        return res if res != INF else -1
+
+
+    # def closetTarget(self, words: List[str], target: str, startIndex: int) -> int:
+    #     if target not in words:
+    #         return -1
+    #     forward = backward = startIndex
+    #     count = 0
+    #     while words[forward] != target and words[backward] != target:
+    #         count += 1
+    #         forward += 1
+    #         backward -= 1
+    #         if forward == len(words):
+    #             forward = 0
+    #         if backward == -(len(words) + 1):
+    #             backward = len(words) - 1
+            
+    #     return count
         
 
 def main():
