a4811464da20db666814752e43440cdb8560b6ca
Brandon Eng <brandoneng000@gmail.com>
2026-04-19 01:42:36 -0700

    Updated 1855

--- medium/1855.py
+++ medium/1855.py
@@ -2,18 +2,32 @@ from typing import List
 
 class Solution:
     def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        n, m = len(nums1), len(nums2)
+        i = j = 0
         res = 0
-        i = 0
 
-        for j, b in enumerate(nums2):
-            while i < len(nums1) and nums1[i] > b:
+        while i < n and j < m:
+            if nums1[i] <= nums2[j]:
+                res = max(res, j - i)
+                j += 1
+            else:
                 i += 1
-            if i == len(nums1):
-                break
-
-            res = max(res, j - i)
         
         return res
+
+    # def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+    #     res = 0
+    #     i = 0
+
+    #     for j, b in enumerate(nums2):
+    #         while i < len(nums1) and nums1[i] > b:
+    #             i += 1
+    #         if i == len(nums1):
+    #             break
+
+    #         res = max(res, j - i)
+        
+    #     return res
         
 def main():
     sol = Solution()
