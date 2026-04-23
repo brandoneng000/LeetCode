0c6873d465f85211239258317f6b3fd3535a0e3a
Brandon Eng <brandoneng000@gmail.com>
2026-04-23 03:36:15 -0700

    Updated 2615

--- medium/2615.py
+++ medium/2615.py
@@ -4,22 +4,42 @@ from collections import defaultdict
 class Solution:
     def distance(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        nums_indexes = defaultdict(list)
+        groups = defaultdict(list)
         res = [0] * n
 
         for i, num in enumerate(nums):
-            nums_indexes[num].append(i)
+            groups[num].append(i)
         
-        for num in nums_indexes:
-            m = len(nums_indexes[num])
-            left = 0
-            right = sum(nums_indexes[num])
-            for i in range(m):
-                res[nums_indexes[num][i]] = (nums_indexes[num][i] * i - left) + (right - nums_indexes[num][i] * (m - i))
-                left += nums_indexes[num][i]
-                right -= nums_indexes[num][i]
+        for group in groups.values():
+            total = sum(group)
+            prefix = 0
+            size = len(group)
+
+            for i, index in enumerate(group):
+                res[index] += total - prefix * 2 + index * (2 * i - size)
+                prefix += index
         
         return res
+
+
+    # def distance(self, nums: List[int]) -> List[int]:
+    #     n = len(nums)
+    #     nums_indexes = defaultdict(list)
+    #     res = [0] * n
+
+    #     for i, num in enumerate(nums):
+    #         nums_indexes[num].append(i)
+        
+    #     for num in nums_indexes:
+    #         m = len(nums_indexes[num])
+    #         left = 0
+    #         right = sum(nums_indexes[num])
+    #         for i in range(m):
+    #             res[nums_indexes[num][i]] = (nums_indexes[num][i] * i - left) + (right - nums_indexes[num][i] * (m - i))
+    #             left += nums_indexes[num][i]
+    #             right -= nums_indexes[num][i]
+        
+    #     return res
         
 def main():
     sol = Solution()
