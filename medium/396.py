d77cc8849b889a7b019872ed1310f34c6dc1d6bb
Brandon Eng <brandoneng000@gmail.com>
2026-05-01 02:45:42 -0700

    Updated 396

--- medium/396.py
+++ medium/396.py
@@ -3,16 +3,28 @@ import collections
 
 class Solution:
     def maxRotateFunction(self, nums: List[int]) -> int:
-        last_index = len(nums) - 1
-        total = sum(index * val for index, val in enumerate(nums))
-        total_nums_val = sum(nums)
-        res = total
+        n = len(nums)
+        total_nums = sum(nums)
+        f = sum(i * num for i, num in enumerate(nums))
+        res = f
 
-        for i in range(len(nums) - 1, -1, -1):
-            total = total + total_nums_val - nums[i] - (last_index * nums[i])
-            res = max(res, total)
+        for i in range(n - 1, 0, -1):
+            f = f + total_nums - n * nums[i]
+            res = max(res, f)
         
         return res
+
+    # def maxRotateFunction(self, nums: List[int]) -> int:
+    #     last_index = len(nums) - 1
+    #     total = sum(index * val for index, val in enumerate(nums))
+    #     total_nums_val = sum(nums)
+    #     res = total
+
+    #     for i in range(len(nums) - 1, -1, -1):
+    #         total = total + total_nums_val - nums[i] - (last_index * nums[i])
+    #         res = max(res, total)
+        
+    #     return res
             
 
 def main():
