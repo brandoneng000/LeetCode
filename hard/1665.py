9f792d9871fdee91bb363e539a32f2adc36fa64e
Brandon Eng <brandoneng000@gmail.com>
2026-05-12 03:48:24 -0700

    Finished 1665

---
+++ hard/1665.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        tasks.sort(key=lambda x: x[1] - x[0])
+        res = 0
+
+        for actual, minimum in tasks:
+            res = max(res + actual, minimum)
+
+        return res
+
+        
+def main():
+    sol = Solution()
+    print(sol.minimumEffort([[1,2],[2,4],[4,8]]))
+    print(sol.minimumEffort([[1,3],[2,4],[10,11],[10,12],[8,9]]))
+    print(sol.minimumEffort([[1,7],[2,8],[3,9],[4,10],[5,11],[6,12]]))
+
+if __name__ == '__main__':
+    main()
