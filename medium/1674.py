629afadd212d3be94a9764ee63343cae2508a66b
Brandon Eng <brandoneng000@gmail.com>
2026-05-13 04:04:13 -0700

    Updated 1674

--- medium/1674.py
+++ medium/1674.py
@@ -1,28 +1,57 @@
 from typing import List
 from collections import Counter
+from bisect import bisect_left
 
 class Solution:
     def minMoves(self, nums: List[int], limit: int) -> int:
         n = len(nums)
-        difference = Counter()
+        sum_count = Counter()
+        min_arr = []
+        max_arr = []
 
         for i in range(n // 2):
-            a, b = nums[i], nums[n - i - 1]
+            a = min(nums[i], nums[n - i - 1])
+            b = max(nums[i], nums[n - i - 1])
 
-            difference[2] += 2
-            difference[min(a, b) + 1] -= 1
-            difference[a + b] -= 1
-            difference[a + b + 1] += 1
-            difference[max(a, b) + limit + 1] += 1
+            sum_count[a + b] += 1
+            min_arr.append(a)
+            max_arr.append(b)
+
+        min_arr.sort()
+        max_arr.sort()
         
-        cur = 0
-        res = float('inf')
+        res = n
+        
+        for c in range(2, 2 * limit + 1):
+            left = n // 2 - bisect_left(min_arr, c)
+            right = bisect_left(max_arr, c - limit)
 
-        for i in range(2, 2 * limit + 1):
-            cur += difference[i]
+            cur = n // 2 + left + right - sum_count[c]
             res = min(res, cur)
         
         return res
+
+    # def minMoves(self, nums: List[int], limit: int) -> int:
+    #     n = len(nums)
+    #     difference = Counter()
+
+    #     for i in range(n // 2):
+    #         a, b = nums[i], nums[n - i - 1]
+
+    #         difference[2] += 2
+    #         difference[min(a, b) + 1] -= 1
+    #         difference[a + b] -= 1
+    #         difference[a + b + 1] += 1
+    #         difference[max(a, b) + limit + 1] += 1
+        
+    #     cur = 0
+    #     res = float('inf')
+
+    #     for i in range(2, 2 * limit + 1):
+    #         cur += difference[i]
+    #         res = min(res, cur)
+        
+    #     return res
 
 
 def main():
