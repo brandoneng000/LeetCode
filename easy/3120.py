179fbc86889f36be817232ac4eb9199b002a3271
Brandon Eng <brandoneng000@gmail.com>
2026-05-26 02:36:01 -0700

    Updated 3120

--- easy/3120.py
+++ easy/3120.py
@@ -2,14 +2,28 @@ from collections import Counter
 
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        count = Counter(word)
+        word_set = set(word)
+        visited = set()
         res = 0
 
-        for letter in set(word.lower()):
-            if count[letter] > 0 and count[letter.upper()] > 0:
+        for letter in word_set:
+            if letter.upper() in word_set and letter.lower() in word_set and letter not in visited:
                 res += 1
+                visited.add(letter.upper())
+                visited.add(letter.lower())
         
         return res
+        
+
+    # def numberOfSpecialChars(self, word: str) -> int:
+    #     count = Counter(word)
+    #     res = 0
+
+    #     for letter in set(word.lower()):
+    #         if count[letter] > 0 and count[letter.upper()] > 0:
+    #             res += 1
+        
+    #     return res
 
         
 def main():
