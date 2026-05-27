1b0069b923fbfe2991c655855958663fb1b7e39b
Brandon Eng <brandoneng000@gmail.com>
2026-05-27 02:42:34 -0700

    Updated 3121

--- medium/3121.py
+++ medium/3121.py
@@ -1,21 +1,43 @@
+from string import ascii_uppercase
+
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        n = len(word)
-        last_index = {}
-        first_index = {}
         res = 0
 
-        for i in range(n):
-            if word[i] not in first_index:
-                first_index[word[i]] = i
+        for letter in ascii_uppercase:
+            first_upper = 0
+            last_lower = 0
 
-            last_index[word[i]] = i
-        
-        for letter in set(word.lower()):
-            if last_index.get(letter, float('inf')) < first_index.get(letter.upper(), -1):
-                res += 1
+            if letter in word:
+                first_upper = word.find(letter)
+                lower_letter = letter.lower()
+
+                if lower_letter in word:
+                    last_lower = word.rfind(lower_letter)
+
+                    if first_upper > last_lower:
+                        res += 1
         
         return res
+
+
+    # def numberOfSpecialChars(self, word: str) -> int:
+    #     n = len(word)
+    #     last_index = {}
+    #     first_index = {}
+    #     res = 0
+
+    #     for i in range(n):
+    #         if word[i] not in first_index:
+    #             first_index[word[i]] = i
+
+    #         last_index[word[i]] = i
+        
+    #     for letter in set(word.lower()):
+    #         if last_index.get(letter, float('inf')) < first_index.get(letter.upper(), -1):
+    #             res += 1
+        
+    #     return res
         
 def main():
     sol = Solution()
