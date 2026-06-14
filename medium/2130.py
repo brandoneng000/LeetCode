12b914439fc636fd038bb409ac07a2dfa07872aa
Brandon Eng <brandoneng000@gmail.com>
2026-06-14 03:35:19 -0700

    Updated 2130

--- medium/2130.py
+++ medium/2130.py
@@ -7,13 +7,37 @@ class ListNode:
         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
+        res = []
         n = 0
         temp = head
-        nums = []
 
         while temp:
+            temp = temp.next
             n += 1
-            nums.append(temp.val)
+
+        temp = head
+        half = n // 2
+
+        for i in range(half):
+            res.append(temp.val)
             temp = temp.next
         
-        return max(nums[i] + nums[n - 1 - i] for i in range(n // 2))
+        for i in range(half):
+            res[half - i - 1] += temp.val
+            temp = temp.next
+        
+        return max(res)
+        
+
+
+    # def pairSum(self, head: Optional[ListNode]) -> int:
+    #     n = 0
+    #     temp = head
+    #     nums = []
+
+    #     while temp:
+    #         n += 1
+    #         nums.append(temp.val)
+    #         temp = temp.next
+        
+    #     return max(nums[i] + nums[n - 1 - i] for i in range(n // 2))
