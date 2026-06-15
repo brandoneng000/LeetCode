af210b703f79be201c85153e6541340eb112c6f2
Brandon Eng <brandoneng000@gmail.com>
2026-06-15 05:27:40 -0700

    Updated 2095

--- medium/2095.py
+++ medium/2095.py
@@ -7,17 +7,32 @@ class ListNode:
         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = fast = head
-        prev = None
+        if not head.next:
+            return None
+        
+        fast = head.next.next
+        slow = head
 
         while fast and fast.next:
-            fast = fast.next.next
-            prev = slow
             slow = slow.next
-            
-        if prev:
-            prev.next = slow.next
-        if not prev:
-            return None
+            fast = fast.next.next
+        
+        slow.next = slow.next.next
 
         return head
+
+    # def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     slow = fast = head
+    #     prev = None
+
+    #     while fast and fast.next:
+    #         fast = fast.next.next
+    #         prev = slow
+    #         slow = slow.next
+            
+    #     if prev:
+    #         prev.next = slow.next
+    #     if not prev:
+    #         return None
+
+    #     return head
