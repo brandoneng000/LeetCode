5a4b7128ec389c2e91ef237f895c60e1d8314c0b
Brandon Eng <brandoneng000@gmail.com>
2026-06-27 02:43:01 -0700

    Updated 3020

--- medium/3020.py
+++ medium/3020.py
@@ -4,32 +4,50 @@ from collections import Counter
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
         count = Counter(nums)
-        visited = set()
-        res = 1
+        one_count = count.get(1, 0)
+        res = one_count if one_count % 2 else one_count - 1
+        count.pop(1, None)
 
-        for num in sorted(count):
-            if num in visited:
-                continue
+        for x in count:
+            t = 0 
+
+            while count[x] > 1:
+                x *= x
+                t += 2
             
-            if num == 1:
-                res = max(res, count[1] if count[1] % 2 else count[1] - 1)
-                continue
-
-            if count[num] >= 2:
-                cur = num
-                cur_count = 1
-
-                while count[cur * cur] >= 2:
-                    cur_count += 1
-                    cur *= cur
-                    visited.add(cur)
-
-                if count[cur * cur] == 1:
-                    res = max(res, cur_count * 2 + 1)
-                else:
-                    res = max(res, (cur_count - 1) * 2 + 1)
-            
+            t += 1 if count[x] else -1
+            res = max(res, t)
+        
         return res
+
+    # def maximumLength(self, nums: List[int]) -> int:
+    #     count = Counter(nums)
+    #     visited = set()
+    #     res = 1
+
+    #     for num in sorted(count):
+    #         if num in visited:
+    #             continue
+            
+    #         if num == 1:
+    #             res = max(res, count[1] if count[1] % 2 else count[1] - 1)
+    #             continue
+
+    #         if count[num] >= 2:
+    #             cur = num
+    #             cur_count = 1
+
+    #             while count[cur * cur] >= 2:
+    #                 cur_count += 1
+    #                 cur *= cur
+    #                 visited.add(cur)
+
+    #             if count[cur * cur] == 1:
+    #                 res = max(res, cur_count * 2 + 1)
+    #             else:
+    #                 res = max(res, (cur_count - 1) * 2 + 1)
+            
+    #     return res
 
         
 def main():
