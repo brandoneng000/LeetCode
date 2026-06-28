726c23774c8328355e184ac6ef8f6c95c7a40874
Brandon Eng <brandoneng000@gmail.com>
2026-06-28 03:02:23 -0700

    Updated 1846

--- medium/1846.py
+++ medium/1846.py
@@ -1,6 +1,19 @@
 from typing import List
 
 class Solution:
+    # def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+    #     n = len(arr)
+    #     counts = [0] * (n + 1)
+    #     res = 1
+
+    #     for num in arr:
+    #         counts[min(num, n)] += 1
+
+    #     for num in range(2, n + 1):
+    #         res = min(res + counts[num], num)
+        
+    #     return res
+
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
         n = len(arr)
         arr.sort()
