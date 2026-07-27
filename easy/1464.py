1310c15b224780c4d3dce7c69402d5d05ad833b3
Brandon Eng <brandoneng000@gmail.com>
2026-07-27 12:12:50 -0700

    Updated 1464

--- easy/1464.py
+++ easy/1464.py
@@ -2,9 +2,21 @@ from typing import List
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        first = max(nums)
-        nums.remove(first)
-        return (first - 1) * (max(nums) - 1)
+        max1 = max2 = 0
+
+        for num in nums:
+            if num > max1:
+                max2 = max1
+                max1 = num
+            elif num > max2:
+                max2 = num
+
+        return (max1 - 1) * (max2 - 1)
+
+    # def maxProduct(self, nums: List[int]) -> int:
+    #     first = max(nums)
+    #     nums.remove(first)
+    #     return (first - 1) * (max(nums) - 1)
 
 def main():
     sol = Solution()
