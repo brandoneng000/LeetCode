d83a6cc025ba47cc4f6d530a784dbd09af82e1ca
Brandon Eng <brandoneng000@gmail.com>
2026-07-30 06:22:06 -0700

    Updated 3014

--- easy/3014.py
+++ easy/3014.py
@@ -2,20 +2,23 @@ from collections import Counter
 
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        count = Counter(word)
-        numbers = 8
-        pressses = 1
-        res = 0
+        return sum(i // 8 + 1 for i in range(len(word)))
 
-        for letter, c in count.most_common():
-            res += pressses * c
-            numbers -= 1
+    # def minimumPushes(self, word: str) -> int:
+    #     count = Counter(word)
+    #     numbers = 8
+    #     pressses = 1
+    #     res = 0
 
-            if numbers == 0:
-                pressses += 1
-                numbers = 8
+    #     for letter, c in count.most_common():
+    #         res += pressses * c
+    #         numbers -= 1
+
+    #         if numbers == 0:
+    #             pressses += 1
+    #             numbers = 8
         
-        return res
+    #     return res
             
 
         
