167a046bbe11d48722d649177d906b5e458d849c
Brandon Eng <brandoneng000@gmail.com>
2026-07-31 07:17:57 -0700

    Updated 3016

--- medium/3016.py
+++ medium/3016.py
@@ -3,19 +3,30 @@ from heapq import heappush, heappop
 
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        count = Counter(word)
-        heap = []
+        cnt = Counter(word)
+        sorted_cnt = sorted(cnt.values(), reverse=True)
+        res = 0
 
-        for i in range(2, 10):
-            heappush(heap, (0, 0, i))
+        for i, f in enumerate(sorted_cnt):
+            row = i // 8 + 1
+            res += f * row
+
+        return res
+
+    # def minimumPushes(self, word: str) -> int:
+    #     count = Counter(word)
+    #     heap = []
+
+    #     for i in range(2, 10):
+    #         heappush(heap, (0, 0, i))
         
-        for letter, c in count.most_common():
-            letter_count, presses, num = heappop(heap)
-            letter_count += 1
-            presses += letter_count * c
-            heappush(heap, (letter_count, presses, num))
+    #     for letter, c in count.most_common():
+    #         letter_count, presses, num = heappop(heap)
+    #         letter_count += 1
+    #         presses += letter_count * c
+    #         heappush(heap, (letter_count, presses, num))
         
-        return sum(presses for letter_count, presses, num in heap)
+    #     return sum(presses for letter_count, presses, num in heap)
         
 def main():
     sol = Solution()
