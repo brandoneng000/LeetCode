9d13401b04f75a846c7279d25c1dc084758ebfb3
Brandon Eng <brandoneng000@gmail.com>
2026-08-01 07:22:02 -0700

    Updated 486

--- medium/486.py
+++ medium/486.py
@@ -3,32 +3,50 @@ import collections
 
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        dp = collections.defaultdict(int)
+        def helper(l, r):
+            if dp[l][r] != -1:
+                return dp[l][r]
+            if l == r:
+                return nums[l]
 
-        def helper(left, right):
-            if left == right:
-                return nums[left]
-            if (left, right) in dp:
-                return dp[(left, right)]
+            left = nums[l] - helper(l + 1, r)
+            right = nums[r] - helper(l, r - 1)
+            dp[l][r] = max(left, right)
+
+            return dp[l][r]
+
+        n = len(nums)
+        dp = [[-1 for _ in range(n)] for _ in range(n)]
+        return helper(0, n - 1) >= 0
+
+    # def PredictTheWinner(self, nums: List[int]) -> bool:
+    #     dp = collections.defaultdict(int)
+
+    #     def helper(left, right):
+    #         if left == right:
+    #             return nums[left]
+    #         if (left, right) in dp:
+    #             return dp[(left, right)]
             
-            score_left = nums[left] - helper(left + 1, right)
-            score_right = nums[right] - helper(left, right - 1)
-            dp[(left, right)] = max(score_left, score_right)
+    #         score_left = nums[left] - helper(left + 1, right)
+    #         score_right = nums[right] - helper(left, right - 1)
+    #         dp[(left, right)] = max(score_left, score_right)
 
-            return dp[(left, right)]
+    #         return dp[(left, right)]
         
-        return helper(0, len(nums) - 1) >= 0
-    
-        # def helper(left, right):
-        #     if left == right:
-        #         return nums[left]
+    #     return helper(0, len(nums) - 1) >= 0
+
+    # def PredictTheWinner(self, nums: List[int]) -> bool:
+    #     def helper(left, right):
+    #         if left == right:
+    #             return nums[left]
             
-        #     score_left = nums[left] - helper(left + 1, right)
-        #     score_right = nums[right] - helper(left, right - 1)
+    #         score_left = nums[left] - helper(left + 1, right)
+    #         score_right = nums[right] - helper(left, right - 1)
 
-        #     return max(score_left, score_right)
+    #         return max(score_left, score_right)
 
-        # return helper(0, len(nums) - 1) >= 0
+    #     return helper(0, len(nums) - 1) >= 0
 
 def main():
     sol = Solution()
