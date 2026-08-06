112e83c45e88c66ba7e65adfffbb03e1d7130e17
Brandon Eng <brandoneng000@gmail.com>
2026-08-06 04:18:22 -0700

    Updated 3345

--- easy/3345.py
+++ easy/3345.py
@@ -1,20 +1,32 @@
 class Solution:
     def smallestNumber(self, n: int, t: int) -> int:
-        def helper(n: int):
-            res = 1
+        for i in range(n, n + 1000):
+            x = i
+            pr = 1
 
-            while n:
-                n, r = divmod(n, 10)
-                res *= r
+            while x > 0:
+                pr *= x % 10
+                x //= 10
+
+            if pr % t == 0:
+                return i
+
+    # def smallestNumber(self, n: int, t: int) -> int:
+    #     def helper(n: int):
+    #         res = 1
+
+    #         while n:
+    #             n, r = divmod(n, 10)
+    #             res *= r
             
-            return res
+    #         return res
         
-        res = n
+    #     res = n
 
-        while helper(res) % t != 0:
-            res += 1
+    #     while helper(res) % t != 0:
+    #         res += 1
         
-        return res
+    #     return res
         
 def main():
     sol = Solution()
