9c1021d47564ebd51eecc6fa58f8e11961b451b6
Brandon Eng <brandoneng000@gmail.com>
2026-08-11 06:07:37 -0700

    Updated 2996

--- easy/2996.py
+++ easy/2996.py
@@ -2,21 +2,38 @@ from typing import List
 
 class Solution:
     def missingInteger(self, nums: List[int]) -> int:
-        n = len(nums)
-        prefix = nums[0]
+        prefix_len = 1
+        nums_set = set(nums)
 
-        for i in range(1, n):
-            if nums[i - 1] + 1 == nums[i]:
-                prefix += nums[i]
+        for prev, curr in zip(nums, nums[1:]):
+            if curr == prev + 1:
+                prefix_len += 1
             else:
                 break
+
+        res = (nums[prefix_len - 1] + nums[0]) * prefix_len // 2
+
+        while res in nums_set:
+            res += 1
+
+        return res
+
+    # def missingInteger(self, nums: List[int]) -> int:
+    #     n = len(nums)
+    #     prefix = nums[0]
+
+    #     for i in range(1, n):
+    #         if nums[i - 1] + 1 == nums[i]:
+    #             prefix += nums[i]
+    #         else:
+    #             break
         
-        nums.sort()
-        for i in range(n):
-            if prefix == nums[i]:
-                prefix += 1
+    #     nums.sort()
+    #     for i in range(n):
+    #         if prefix == nums[i]:
+    #             prefix += 1
         
-        return prefix
+    #     return prefix
         
 def main():
     sol = Solution()
