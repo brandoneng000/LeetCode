c12846df8fafd69907ff08f6e41b9fb7355006a9
Brandon Eng <brandoneng000@gmail.com>
2026-08-12 07:09:34 -0700

    Updated 2958

--- medium/2958.py
+++ medium/2958.py
@@ -4,20 +4,41 @@ from collections import Counter
 class Solution:
     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        count = Counter()
+        freq = Counter()
         left = 0
-        res = 0
+        chars_over_k = 0
 
         for right in range(n):
-            count[nums[right]] += 1
+            freq[nums[right]] += 1
 
-            while left < right and count[nums[right]] > k:
-                count[nums[left]] -= 1
+            if freq[nums[right]] == k + 1:
+                chars_over_k += 1
+
+            if chars_over_k:
+                freq[nums[left]] -= 1
+
+                if freq[nums[left]] == k:
+                    chars_over_k -= 1
                 left += 1
+
+        return n - left
+
+    # def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+    #     n = len(nums)
+    #     count = Counter()
+    #     left = 0
+    #     res = 0
+
+    #     for right in range(n):
+    #         count[nums[right]] += 1
+
+    #         while count[nums[right]] > k:
+    #             count[nums[left]] -= 1
+    #             left += 1
             
-            res = max(res, right - left + 1)
+    #         res = max(res, right - left + 1)
         
-        return res
+    #     return res
             
 
 
