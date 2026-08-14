71a74eae02f32c18221cf27af5d4cb972ff1f8ca
Brandon Eng <brandoneng000@gmail.com>
2026-08-14 06:17:24 -0700

    Updated 3090

--- easy/3090.py
+++ easy/3090.py
@@ -1,21 +1,39 @@
+from collections import Counter
+
 class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
         n = len(s)
-        count = [0] * 26
-        left = 0
-        res = 0
+        freq = Counter()
+        left = res = 0
 
         for right in range(n):
-            index = ord(s[right]) - ord('a')
-            count[index] += 1
+            freq[s[right]] += 1
 
-            while left < right and count[index] > 2:
-                count[ord(s[left]) - ord('a')] -= 1
+            while freq[s[right]] > 2:
+                freq[s[left]] -= 1
                 left += 1
-            
+
             res = max(res, right - left + 1)
-        
+
         return res
+
+    # def maximumLengthSubstring(self, s: str) -> int:
+    #     n = len(s)
+    #     count = [0] * 26
+    #     left = 0
+    #     res = 0
+
+    #     for right in range(n):
+    #         index = ord(s[right]) - ord('a')
+    #         count[index] += 1
+
+    #         while left < right and count[index] > 2:
+    #             count[ord(s[left]) - ord('a')] -= 1
+    #             left += 1
+            
+    #         res = max(res, right - left + 1)
+        
+    #     return res
 
         
 def main():
