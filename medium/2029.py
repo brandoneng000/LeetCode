f163d492048d6eb3b60dd4084abed6d7ccf85377
Brandon Eng <brandoneng000@gmail.com>
2026-08-16 06:05:24 -0700

    Updated 2029

--- medium/2029.py
+++ medium/2029.py
@@ -3,11 +3,29 @@ from collections import Counter
 
 class Solution:
     def stoneGameIX(self, stones: List[int]) -> bool:
-        stones_count = Counter(s % 3 for s in stones)
-        if stones_count[0] % 2 == 0:
-            return stones_count[1] > 0 and stones_count[2] > 0
+        a = b = c = 0
+
+        for s in stones:
+            r = s % 3
+
+            if r == 0:
+                a += 1
+            elif r == 1:
+                b += 1
+            else:
+                c += 1
+
+        if a % 2 == 0:
+            return b > 0 and c > 0
+
+        return abs(b - c) > 2
+
+    # def stoneGameIX(self, stones: List[int]) -> bool:
+    #     stones_count = Counter(s % 3 for s in stones)
+    #     if stones_count[0] % 2 == 0:
+    #         return stones_count[1] > 0 and stones_count[2] > 0
         
-        return abs(stones_count[1] - stones_count[2]) > 2
+    #     return abs(stones_count[1] - stones_count[2]) > 2
 
 
 def main():
