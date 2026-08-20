400fa6d6342addcb3455220db29822fa15b1231d
Brandon Eng <brandoneng000@gmail.com>
2026-08-20 06:36:14 -0700

    Updated 3069

--- easy/3069.py
+++ easy/3069.py
@@ -3,16 +3,43 @@ from typing import List
 class Solution:
     def resultArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        arr1 = [nums[0]]
-        arr2 = [nums[1]]
+        res = [0] * n
+        res[0] = nums[0]
+        res[-1] = nums[1]
+        index = 0
+        reverse_index = n - 1
 
         for i in range(2, n):
-            if arr1[-1] > arr2[-1]:
-                arr1.append(nums[i])
+            if res[index] > res[reverse_index]:
+                index += 1
+                res[index] = nums[i]
             else:
-                arr2.append(nums[i])
+                reverse_index -= 1
+                res[reverse_index] = nums[i]
+
+        l = reverse_index
+        r = n - 1
+
+        while l < r:
+            res[l], res[r] = res[r], res[l]
+            l += 1
+            r -= 1
+
+        return res
         
-        return arr1 + arr2
+
+    # def resultArray(self, nums: List[int]) -> List[int]:
+    #     n = len(nums)
+    #     arr1 = [nums[0]]
+    #     arr2 = [nums[1]]
+
+    #     for i in range(2, n):
+    #         if arr1[-1] > arr2[-1]:
+    #             arr1.append(nums[i])
+    #         else:
+    #             arr2.append(nums[i])
+        
+    #     return arr1 + arr2
         
 def main():
     sol = Solution()
