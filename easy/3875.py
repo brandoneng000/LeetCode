1951208655daaf2234033487af77051da0d135d4
Brandon Eng <brandoneng000@gmail.com>
2026-09-02 01:57:07 -0700

    Finished 3875

---
+++ easy/3875.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
+
+def main():
+    sol = Solution()
+    print(sol.uniformArray([2,3]))
+    print(sol.uniformArray([4,6]))
+
+if __name__ == '__main__':
+    main()
