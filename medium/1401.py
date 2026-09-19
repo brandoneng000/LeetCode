a5ca3e04006ce403518291e0fb905a620c10c355
Brandon Eng <brandoneng000@gmail.com>
2026-09-19 02:08:07 -0700

    Updated 1401

--- medium/1401.py
+++ medium/1401.py
@@ -1,9 +1,15 @@
 class Solution:
     def checkOverlap(self, radius: int, xCenter: int, yCenter: int, x1: int, y1: int, x2: int, y2: int) -> bool:
-        x = 0 if x1 <= xCenter <= x2 else min(abs(x1 - xCenter), abs(x2 - xCenter))
-        y = 0 if y1 <= yCenter <= y2 else min(abs(y1 - yCenter), abs(y2 - yCenter))
+        x = max(x1, min(xCenter, x2))
+        y = max(y1, min(yCenter, y2))
+
+        return (xCenter - x) ** 2 + (yCenter - y) ** 2 <= radius * radius
+
+    # def checkOverlap(self, radius: int, xCenter: int, yCenter: int, x1: int, y1: int, x2: int, y2: int) -> bool:
+    #     x = 0 if x1 <= xCenter <= x2 else min(abs(x1 - xCenter), abs(x2 - xCenter))
+    #     y = 0 if y1 <= yCenter <= y2 else min(abs(y1 - yCenter), abs(y2 - yCenter))
         
-        return x * x + y * y <= radius * radius
+    #     return x * x + y * y <= radius * radius
 
         
 
